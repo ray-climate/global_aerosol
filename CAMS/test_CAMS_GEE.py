9a34fe57dbee3b5771005891ac57fcd61ca3289d
ruisong <rui.song@ucl.ac.uk>
2022-08-04 12:46:43 +0100

    Initial commit

---
+++ CAMS/test_CAMS_GEE.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    test_CAMS_GEE.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        04/08/2022 12:45
+
+import numpy as np
