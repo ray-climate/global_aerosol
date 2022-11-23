0a34297244a3a34ff0db434b221547eee6cdb984
ruisong <rui.song@ucl.ac.uk>
2022-11-23 13:15:22 +0000

    new global colocation database

---
+++ Colocation/generate_colocation_global.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    generate_colocation_global.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        23/11/2022 13:02
