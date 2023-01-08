b1f4231efcfed9039e104603bf6f4583f556ed43
ruisong <rui.song@ucl.ac.uk>
2023-01-08 22:47:58 +0000

    get aeolus from netcdf format data

--- Experiment/test_colocation_graphs.py
+++ Experiment/test_colocation_graphs.py
@@ -9,8 +9,8 @@ import os
 import sys
 sys.path.append('../')
 
+from getColocationData.get_aeolus import *
 from datetime import datetime, timedelta
-from get_aeolus import *
 import logging
 import csv
 
