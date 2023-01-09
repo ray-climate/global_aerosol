17ffe09c96f892c7076546922134c21020207443
ruisong <rui.song@ucl.ac.uk>
2023-01-09 15:58:33 +0000

    get aeolus from netcdf format data

--- getColocationData/get_reprojection.py
+++ getColocationData/get_reprojection.py
@@ -15,10 +15,10 @@ def reproject_observations(lat_colocation, lon_colocation, lat_aeolus, lon_aeolu
     lon_max = lon_min + 2 * interval
 
     lat_aeolus_cutoff = lat_aeolus[(lat_aeolus > lat_min) & (lat_aeolus < lat_max)]
-    lon_aeolus_cutoff = lat_aeolus[(lon_aeolus > lon_min) & (lon_aeolus < lon_max)]
+    lon_aeolus_cutoff = lon_aeolus[(lon_aeolus > lon_min) & (lon_aeolus < lon_max)]
 
     lat_caliop_cutoff = lat_caliop[(lat_caliop > lat_min) & (lat_caliop < lat_max)]
-    lon_caliop_cutoff = lat_caliop[(lon_caliop > lon_min) & (lon_caliop < lon_max)]
+    lon_caliop_cutoff = lon_caliop[(lon_caliop > lon_min) & (lon_caliop < lon_max)]
 
     print(lat_aeolus_cutoff)
     print(lon_aeolus_cutoff)
