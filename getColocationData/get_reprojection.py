12730307e35e0f29dda962279bbf40d673e87972
ruisong <rui.song@ucl.ac.uk>
2023-01-09 17:16:41 +0000

    get aeolus from netcdf format data

--- getColocationData/get_reprojection.py
+++ getColocationData/get_reprojection.py
@@ -41,7 +41,7 @@ def reproject_observations(lat_colocation, lon_colocation, time_colocation, lat_
     # Filter the lat_caliop array based on the latitude range and store the result in lat_caliop_cutoff
     lat_caliop_cutoff = lat_caliop[(lat_caliop > lat_min) & (lat_caliop < lat_max)]
     # Filter the lon_caliop array based on the latitude range and store the result in lon_caliop_cutoff
-    lon_caliop_cutoff = lon_caliop[(lon_caliop > lon_min) & (lon_caliop < lon_max)]
+    lon_caliop_cutoff = lon_caliop[(lat_caliop > lat_min) & (lat_caliop < lat_max)]
 
     return lat_aeolus_cutoff, lon_aeolus_cutoff, lat_caliop_cutoff, lon_caliop_cutoff
 
