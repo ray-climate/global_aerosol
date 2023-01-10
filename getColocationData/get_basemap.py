98d5841383e88ec99b31bf8c793cfcd68f7a2ae2
ruisong <rui.song@ucl.ac.uk>
2023-01-10 13:08:29 +0000

    get aeolus from netcdf format data

--- getColocationData/get_basemap.py
+++ getColocationData/get_basemap.py
@@ -97,11 +97,11 @@ def plot_grid_tiles(lat_colocation, lon_colocation, lat_aeolus, lon_aeolus, lat_
     cbar.set_label('[km$^{-1}$sr$^{-1}$]', fontsize=30, rotation=90)
     cbar.ax.tick_params(labelsize=20)
 
-    ax2.xlabel('Latitude', fontsize=30)
-    ax2.ylabel('Height [km]', fontsize=30)
+    ax2.set_xlabel('Latitude', fontsize=30)
+    ax2.set_ylabel('Height [km]', fontsize=30)
 
-    ax2.ylim([0., 25.])
-    ax2.title('CALIOP L2 Backscatter coeff.', fontsize=30)
+    ax2.set_ylim([0., 25.])
+    ax2.set_title('CALIOP L2 Backscatter coeff.', fontsize=30)
     for tick in ax2.xaxis.get_major_ticks():
         tick.label.set_fontsize(25)
     for tick in ax2.yaxis.get_major_ticks():
