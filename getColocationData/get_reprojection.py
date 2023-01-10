92d7be005921b65909e88e7b48e882799a46d0ea
ruisong <rui.song@ucl.ac.uk>
2023-01-10 17:29:30 +0000

    get aeolus from netcdf format data

--- getColocationData/get_reprojection.py
+++ getColocationData/get_reprojection.py
@@ -86,8 +86,7 @@ def resample_aeolus(lat_aeolus, alt_aeolus, data_aeolus, alt_caliop):
     # Iterate through rows and columns of alt_aeolus and data_aeolus
     for i in range(alt_aeolus.shape[0]):
         alt_aeolus_i = alt_aeolus[i, :]
-        print(data_aeolus[i, :])
-        print(alt_aeolus_i)
+
         for k in range(np.size(alt_aeolus_i)):
             if alt_aeolus_i[k] > 0:
                 if (k + 1) < len(alt_aeolus_i):
@@ -95,6 +94,8 @@ def resample_aeolus(lat_aeolus, alt_aeolus, data_aeolus, alt_caliop):
                     # Resample data based on nearest altitude value less than current value in alt_caliop
                     data_aeolus_resample[i, (alt_caliop < alt_aeolus_i[k]) & (alt_caliop > alt_aeolus_i[k + 1])] = \
                     data_aeolus[i, k]
+                    print(alt_aeolus_i[k])
+                    print(data_aeolus[i, k])
 
     return data_aeolus_resample
 
