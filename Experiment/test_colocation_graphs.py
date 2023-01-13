007a3f45f650380c523c3b89446b68a7a3f84ea5
ruisong <rui.song@ucl.ac.uk>
2023-01-13 22:52:20 +0000

    get aeolus from netcdf format data

--- Experiment/test_colocation_graphs.py
+++ Experiment/test_colocation_graphs.py
@@ -123,8 +123,9 @@ while start_date_datetime <= end_date_datetime:
                                footprint_lat_aeolus, footprint_lon_aeolus, altitude_aeolus, footprint_time_aeolus, beta_aeolus_mb, alpha_aeolus_mb,
                                footprint_lat_caliop, footprint_lon_caliop, beta_caliop,
                                interval=10)
-
+        print(alt_aeolus_cutoff, 88888)
         beta_aeolus_resample = resample_aeolus(lat_aeolus_cutoff, alt_aeolus_cutoff, beta_aeolus_cutoff, alt_caliop)
+        print(alt_aeolus_cutoff, 888889999)
         alpha_aeolus_resample = resample_aeolus(lat_aeolus_cutoff, alt_aeolus_cutoff, alpha_aeolus_cutoff, alt_caliop)
         print(alpha_aeolus_resample[alpha_aeolus_resample>0], 111)
         print(beta_aeolus_resample[beta_aeolus_resample > 0], 222)
