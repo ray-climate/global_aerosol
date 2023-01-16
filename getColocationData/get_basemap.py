780da64c65d77c47385936faf8159b1cef12fbca
ruisong <rui.song@ucl.ac.uk>
2023-01-16 22:51:44 +0000

    get aeolus from netcdf format data

--- getColocationData/get_basemap.py
+++ getColocationData/get_basemap.py
@@ -253,7 +253,7 @@ def plot_grid_tiles(lat_colocation, lon_colocation,
     divider = make_axes_locatable(ax6)
 
     # Add the colorbar to the divider
-    cax = divider.append_axes("bottom", size="1.5%", pad=0.1)
+    cax = divider.append_axes("bottom", pad=0.1)
 
     cbar = plt.colorbar(fig6, cax=cax, shrink=0.6)
     cbar.ax.tick_params(labelsize=18)
