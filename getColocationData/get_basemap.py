0b91d3f6c3f95495e32040648d1c5110fff8e164
ruisong <rui.song@ucl.ac.uk>
2023-01-17 11:42:20 +0000

    get aeolus from netcdf format data

--- getColocationData/get_basemap.py
+++ getColocationData/get_basemap.py
@@ -210,6 +210,7 @@ def plot_grid_tiles(lat_colocation, lon_colocation,
     z_grid_aeolus_alpha = alpha_aeolus.T
 
     fig5 = plt.pcolormesh(x_grid_aeolus, y_grid_aeolus, z_grid_aeolus_alpha, cmap='viridis', norm=colors.LogNorm(vmin=1.e-3, vmax=1.))
+    print(x_grid_aeolus)
     print(lat_colocation, savefigname)
     ax5.axvline(x=lat_colocation, color='red', linestyle='solid', alpha=0.3, lw=20)
     # Create an axes divider for the main plot
@@ -316,7 +317,7 @@ def plot_grid_tiles(lat_colocation, lon_colocation,
              color='black')
 
     plt.subplots_adjust(wspace=1., hspace=1.)
-    plt.suptitle("%s"%title, fontweight='bold', fontstyle='italic', fontsize=28)
+    plt.suptitle("%s"%title, fontweight='bold', fontstyle='italic', fontsize=28, pad=0.1)
     plt.tight_layout()
     # Show the map
     plt.savefig(savefigname)
