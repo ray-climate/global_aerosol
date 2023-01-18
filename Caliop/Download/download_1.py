200fce5d45e192437bbec927b382103ee0e0fa49
ruisong <rui.song@ucl.ac.uk>
2023-01-18 16:57:52 +0000

    get aeolus from netcdf format data

--- Caliop/Download/download_1.py
+++ Caliop/Download/download_1.py
@@ -12,7 +12,7 @@ import os
 data_dir = '/gws/nopw/j04/eo_shared_data_vol1/satellite/calipso/APro5km'
 
 start_date = '2020-05-16' # start data for downloading
-end_date   = '2020-07-31' # end date for downloading
+end_date   = '2020-06-30' # end date for downloading
 
 start_date_datetime = datetime.strptime(start_date, '%Y-%m-%d')
 end_date_datetime = datetime.strptime(end_date, '%Y-%m-%d')
@@ -25,12 +25,15 @@ while start_date_datetime <= end_date_datetime:
     month_i = '{:02d}'.format(start_date_datetime.month)
     day_i = '{:02d}'.format(start_date_datetime.day)
 
-    save_dir_i = data_dir + '/%s/%s_%s_%s'%(year_i, year_i, month_i, day_i)
+    save_dir_i = data_dir + '/%s/%s_%s_%s/'%(year_i, year_i, month_i, day_i)
 
     try:
         os.stat(save_dir_i)
     except:
         pathlib.Path(save_dir_i).mkdir(parents=True, exist_ok=True)
+
+    os.system(
+        "wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -P %s -A '*%s-%s-%s*' -r -nd -np -nc -nH --cut-dirs=4 -e robots=off https://asdc.larc.nasa.gov/data/CALIPSO/LID_L2_05kmAPro-Standard-V4-20/%s/%s/" %(save_dir_i, year_i, month_i, day_i, year_i, month_i))
 
     start_date_datetime = start_date_datetime + time_delta
 
