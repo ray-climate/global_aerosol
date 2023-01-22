615a08a2e8e79e3bdd599cb86dd587e6a225691f
ruisong <rui.song@ucl.ac.uk>
2023-01-22 11:58:26 +0000

    get aeolus from netcdf format data

--- getColocationData/save_colocated_data.py
+++ getColocationData/save_colocated_data.py
@@ -8,13 +8,16 @@
 from netCDF4 import Dataset
 
 def save_colocation_nc(saveFilename, lat_aeolus, lon_aeolus, alt_aeolus, beta_aeolus, alpha_aeolus):
-    print(lat_aeolus.shape)
-    print(lon_aeolus.shape)
-    print(alt_aeolus.shape)
-    print(beta_aeolus.shape)
-    print(alpha_aeolus.shape)
-    quit()
 
     ncfile = Dataset(saveFilename, mode='w', format='NETCDF4')
     ncfile.createDimension('x_aeolus', beta_aeolus.shape[0])
     ncfile.createDimension('y_aeolus', beta_aeolus.shape[1])
+
+    nc_lat_aeolus = ncfile.createVariable('aeolus_latitude', 'f4', ('x_aeolus'))
+    nc_lat_aeolus[:] = lat_aeolus
+
+    nc_lon_aeolus = ncfile.createVariable('aeolus_longitude', 'f4', ('x_aeolus'))
+    nc_lon_aeolus[:] = lon_aeolus
+
+    nc_beta_aeolus = ncfile.createVariable('aeolus_beta', 'f4', ('y_aeolus', 'x_aeolus'))
+    nc_beta_aeolus[:] = beta_aeolus
