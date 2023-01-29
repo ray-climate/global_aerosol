1353714bf2892989192aa77917b2422d86465e69
ruisong <rui.song@ucl.ac.uk>
2023-01-29 13:35:56 +0000

    get aeolus from netcdf format data

--- readColocationData/readColocationNetCDF.py
+++ readColocationData/readColocationNetCDF.py
@@ -5,6 +5,11 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        29/01/2023 13:24
 
+from netCDF4 import Dataset
+
 def extractColocationParameters(inputNetCDF):
 
     print(inputNetCDF)
+    with Dataset(inputNetCDF, 'r') as nc_data:
+        aeolus_beta = nc_data['aeolus_data']['aeolus_beta'][:]
+        print(aeolus_beta.shape)
