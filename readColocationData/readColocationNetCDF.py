2fda1dc7a6bbdce0c9f4a0c141f5819a67024957
ruisong <rui.song@ucl.ac.uk>
2023-01-30 18:03:57 +0000

    get aeolus from netcdf format data

--- readColocationData/readColocationNetCDF.py
+++ readColocationData/readColocationNetCDF.py
@@ -59,9 +59,9 @@ def extractColocationParameters(inputNetCDF):
 
                 beta_aeolus_stats.append(beta_aeolus_centre[k] * 1.e-6 * 1.e3) # scaling factor, and unit conversion
                 print(beta_aeolus_centre[k] * 1.e-6 * 1.e3)
-                print(caliop_index_x_min, caliop_index_x_max)
+
                 beta_caliop_filter = beta_caliop[(alt_caliop < alt_aeolus_centre[k]) & (alt_caliop > alt_aeolus_centre[k+1]), caliop_index_x_min : caliop_index_x_max]
-                print(beta_caliop.shape)
+                print(beta_caliop[(alt_caliop < alt_aeolus_centre[k]) & (alt_caliop > alt_aeolus_centre[k+1]), 399])
                 print(beta_caliop_filter)
                 beta_caliop_stats = np.nanmean(beta_caliop_filter)
                 print(beta_caliop_stats)
