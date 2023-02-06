035762bcedbc02e853569632d10ebb43e601923a
ruisong <rui.song@ucl.ac.uk>
2023-02-06 16:20:17 +0000

    get aeolus from netcdf format data

--- Experiment/global_20211001-20211101.py
+++ Experiment/global_20211001-20211101.py
@@ -138,7 +138,7 @@ y = beta_aeolus_all[(beta_caliop_all > 0) & (beta_aeolus_all > 0) & (beta_caliop
 # z = gaussian_kde(xy)(xy)
 
 fig, ax = plt.subplots(figsize=(10, 10))
-plt.hist2d(x, y, bins=(100, 100), cmap = "RdYlGn_r",
+plt.hist2d(x, y, bins=(70, 70), cmap = "RdYlGn_r",
            norm = colors.LogNorm())
 
 # ax.scatter(x, y, c=z, s=50, cmap=plt.cm.jet)
