d7c5bcf5eb68f1313ec83577e8509b134b2330cb
ruisong <rui.song@ucl.ac.uk>
2023-02-06 23:14:27 +0000

    get aeolus from netcdf format data

--- Experiment/global_20210601-20211231.py
+++ Experiment/global_20210601-20211231.py
@@ -161,6 +161,7 @@ qc_aeolus_all = [0 if ele =='--' else ele for ele in qc_aeolus_all]
 qc_aeolus_all = np.array(qc_aeolus_all, dtype=np.uint8)
 qc_aeolus_flag = np.unpackbits(qc_aeolus_all)
 print(qc_aeolus_flag)
+print(qc_aeolus_flag[0])
 quit()
 
 x = beta_caliop_all[(beta_caliop_all > 0) & (beta_aeolus_all > 0) & (beta_caliop_all < 0.02) & (beta_aeolus_all < 0.02)]
