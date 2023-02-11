78a1672aa14eb44637fb985ef6b1c70a3ab76094
ruisong <rui.song@ucl.ac.uk>
2023-02-11 23:59:43 +0000

    get aeolus from netcdf format data

--- Experiment/global_20210601-20211231.py
+++ Experiment/global_20210601-20211231.py
@@ -269,7 +269,7 @@ for i in range(2):
                 zi = k(np.vstack([xi.flatten(), yi.flatten()]))
 
                 ax[i, j].pcolormesh(xi, yi, zi.reshape(xi.shape), shading='auto', cmap='RdYlBu_r')
-                ax[i, j].scatter(x, y)
+                # ax[i, j].scatter(x, y)
                 ax[i, j].set_aspect(1)
                 ax[i, j].set_xlabel(r'$532\ nm\  \beta_{CALIPSO}\  [km^{-1}sr^{-1}]$', fontsize=12)
                 ax[i, j].set_ylabel(r'$355\ nm\  \beta_{AEOLUS}\  [km^{-1}sr^{-1}]$', fontsize=12)
