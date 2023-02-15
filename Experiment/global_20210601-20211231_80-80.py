378e5dbccbdcf4df952720c7098702195ea1dac5
ruisong <rui.song@ucl.ac.uk>
2023-02-15 16:50:31 +0000

    get aeolus from netcdf format data

--- Experiment/global_20210601-20211231_80-80.py
+++ Experiment/global_20210601-20211231_80-80.py
@@ -56,7 +56,22 @@ aeolus_layers_dic = {'layer-1': (0, 5),
 
 aeolus_layers_keys = list(aeolus_layers_dic.keys())
 
+aeolus_type_dic = {0: 'undefined',
+                   1: 'clean marine',
+                   2: 'dust',
+                   3: 'polluted continental / smoke',
+                   4: 'clean continental',
+                   5: 'polluted dust',
+                   6: 'elevated smoke',
+                   7: 'dusty marine'}
 
+aeolus_type_keys = list(aeolus_layers_dic.keys())
+
+for type_i in range(len(aeolus_type_keys)):
+
+    a = aeolus_type_dic[int(aeolus_type_keys[type_i])]
+
+quit()
 ##############################################################
 def get_script_name():
     return sys.modules['__main__'].__file__
@@ -146,11 +161,10 @@ else:
                         print('Processing file %s......'%file)
                         (beta_aeolus_i, beta_caliop_i, aerosol_type_caliop_i, alt_bottom_i, alt_top_i, time_str_i, qc_i, ber_i,
                          lod_i) = extractColocationParameters(colocationData_daily_dir + '/' + file)
-                        aerosol_type_caliop_all.extend(aerosol_type_caliop_i)
-                        print(aerosol_type_caliop_i)
-                        print(beta_caliop_i)
+
                         beta_aeolus_all.extend(beta_aeolus_i)
                         beta_caliop_all.extend(beta_caliop_i)
+                        aerosol_type_caliop_all.extend(aerosol_type_caliop_i)
                         alt_bottom_all.extend(alt_bottom_i)
                         alt_top_all.extend(alt_top_i)
                         time_str_all.extend(time_str_i)
@@ -168,16 +182,17 @@ else:
 
     beta_aeolus_all = np.asarray(beta_aeolus_all)
     beta_caliop_all = np.asarray(beta_caliop_all)
+    aerosol_type_caliop_all = np.asarray(aerosol_type_caliop_all)
 
     with open(output_dir + '/%s.csv' % script_base, "w") as output:
         writer = csv.writer(output, lineterminator='\n')
-        writer.writerow(('Colocation_Datetime', 'Aeolus_beta', 'Caliop_beta', 'alt_bottom', 'alt_top', 'Aeolus_QC', 'Aeolus_BER', 'Aeolus_LOD'))
+        writer.writerow(('Colocation_Datetime', 'Aeolus_beta', 'Caliop_beta', 'aerosol_type', 'alt_bottom', 'alt_top', 'Aeolus_QC', 'Aeolus_BER', 'Aeolus_LOD'))
 
         for j in range(np.size(beta_aeolus_all)):
 
             try:
                 if (float(beta_aeolus_all[j]) > 0) & (float(beta_caliop_all[j]) >0):
-                    writer.writerow((time_str_all[j], float(beta_aeolus_all[j]), float(beta_caliop_all[j]),
+                    writer.writerow((time_str_all[j], float(beta_aeolus_all[j]), float(beta_caliop_all[j]), int(aerosol_type_caliop_all[j]),
                                      alt_bottom_all[j], alt_top_all[j], qc_aeolus_all[j],
                                      ber_aeolus_all[j], lod_aeolus_all[j]))
             except:
@@ -236,85 +251,93 @@ plt.savefig(output_dir + '/%s_top_alt_hist1d.png' %script_base)
 fig, ax = plt.subplots(2, 3, figsize=(16, 10))
 
 # Loop through the axis array and plot random data
-plot_index = 0
-for i in range(2):
-    for j in range(3):
 
-        if plot_index <= len(aeolus_layers_keys):
+beta_aeolus_all_bk = np.copy(beta_aeolus_all)
+beta_caliop_all_bk = np.copy(beta_caliop_all)
+aerosol_type_caliop_all_bk = np.copy(aerosol_type_caliop_all)
 
-            nbins = 1000
+for type_i in range(len(aeolus_type_keys)):
 
-            if plot_index == 0:
+    beta_caliop_all = beta_caliop_all_bk[aerosol_type_caliop_all == [aeolus_type_dic[plot_index - 1]][0]]
+    plot_index = 0
+    for i in range(2):
+        for j in range(3):
 
-                x = beta_caliop_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0)]
-                y = beta_aeolus_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0)]
+            if plot_index <= len(aeolus_layers_keys):
 
-            else:
-
-                try :
-                    x = beta_caliop_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0) & (
-                                alt_top_all > float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0])) & (
-                                                    alt_top_all < float(
-                                                aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][1]))]
-
-                    y = beta_aeolus_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0) & (
-                                alt_top_all > float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0])) & (
-                                                    alt_top_all < float(
-                                                aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][1]))]
-
-                except:
-                    x = beta_caliop_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0) & (
-                                alt_top_all > float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0]))]
-
-                    y = beta_aeolus_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0) & (
-                                alt_top_all > float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0]))]
-
-            Colocation_number = np.size(x)
-
-            if Colocation_number > 0:
-
-                k = kde.gaussian_kde([x, y])
-                xi, yi = np.mgrid[x.min():x.max():nbins * 1j, y.min():y.max():nbins * 1j]
-                zi = k(np.vstack([xi.flatten(), yi.flatten()]))
-
-                ax[i, j].pcolormesh(xi, yi, zi.reshape(xi.shape), shading='auto', cmap='RdYlBu_r')
-                # ax[i, j].scatter(x, y)
-                ax[i, j].set_aspect(1)
-                ax[i, j].set_xlabel(r'$532\ nm\  \beta_{CALIPSO}\  [km^{-1}sr^{-1}]$', fontsize=12)
-                ax[i, j].set_ylabel(r'$355\ nm\  \beta_{AEOLUS}\  [km^{-1}sr^{-1}]$', fontsize=12)
+                nbins = 1000
 
                 if plot_index == 0:
-                    ax[i, j].set_title('Altitude bins: All \n Colocation Number = %s'%Colocation_number, fontsize=12)
+
+                    x = beta_caliop_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0)]
+                    y = beta_aeolus_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0)]
 
                 else:
 
-                    try:
-                        if float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][1]) > 0:
-                            ax[i, j].set_title('Altitude bins: %s - %s km \n Colocation Number = %s' % (
-                                float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0]),
-                                float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][1]), Colocation_number),
-                                               fontsize=12)
+                    try :
+                        x = beta_caliop_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0) & (
+                                    alt_top_all > float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0])) & (
+                                                        alt_top_all < float(
+                                                    aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][1]))]
+
+                        y = beta_aeolus_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0) & (
+                                    alt_top_all > float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0])) & (
+                                                        alt_top_all < float(
+                                                    aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][1]))]
+
                     except:
-                        ax[i, j].set_title('Altitude bins: %s km above \n Colocation Number = %s' % (
-                            float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0]), Colocation_number),
-                                           fontsize=12)
+                        x = beta_caliop_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0) & (
+                                    alt_top_all > float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0]))]
 
-                ax[i, j].set_xlim([0., np.nanmin([np.nanmax(x), np.nanmax(y)])])
-                ax[i, j].set_ylim([0., np.nanmin([np.nanmax(x), np.nanmax(y)])])
+                        y = beta_aeolus_all[(beta_caliop_all > 0) & (beta_aeolus_SNR_cloud_filtered > 0) & (
+                                    alt_top_all > float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0]))]
 
-                for tick in ax[i, j].xaxis.get_major_ticks():
-                    tick.label.set_fontsize(12)
-                for tick in ax[i, j].yaxis.get_major_ticks():
-                    tick.label.set_fontsize(12)
+                Colocation_number = np.size(x)
+
+                if Colocation_number > 0:
+
+                    k = kde.gaussian_kde([x, y])
+                    xi, yi = np.mgrid[x.min():x.max():nbins * 1j, y.min():y.max():nbins * 1j]
+                    zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+
+                    ax[i, j].pcolormesh(xi, yi, zi.reshape(xi.shape), shading='auto', cmap='RdYlBu_r')
+                    # ax[i, j].scatter(x, y)
+                    ax[i, j].set_aspect(1)
+                    ax[i, j].set_xlabel(r'$532\ nm\  \beta_{CALIPSO}\  [km^{-1}sr^{-1}]$', fontsize=12)
+                    ax[i, j].set_ylabel(r'$355\ nm\  \beta_{AEOLUS}\  [km^{-1}sr^{-1}]$', fontsize=12)
+
+                    if plot_index == 0:
+                        ax[i, j].set_title('Altitude bins: All \n Colocation Number = %s'%Colocation_number, fontsize=12)
+
+                    else:
+
+                        try:
+                            if float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][1]) > 0:
+                                ax[i, j].set_title('Altitude bins: %s - %s km \n Colocation Number = %s' % (
+                                    float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0]),
+                                    float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][1]), Colocation_number),
+                                                   fontsize=12)
+                        except:
+                            ax[i, j].set_title('Altitude bins: %s km above \n Colocation Number = %s' % (
+                                float(aeolus_layers_dic[aeolus_layers_keys[plot_index - 1]][0]), Colocation_number),
+                                               fontsize=12)
+
+                    ax[i, j].set_xlim([0., np.nanmin([np.nanmax(x), np.nanmax(y)])])
+                    ax[i, j].set_ylim([0., np.nanmin([np.nanmax(x), np.nanmax(y)])])
+
+                    for tick in ax[i, j].xaxis.get_major_ticks():
+                        tick.label.set_fontsize(12)
+                    for tick in ax[i, j].yaxis.get_major_ticks():
+                        tick.label.set_fontsize(12)
+
+                else:
+                    ax[i, j].set_visible(False)
+
+                plot_index = plot_index + 1
 
             else:
                 ax[i, j].set_visible(False)
 
-            plot_index = plot_index + 1
-
-        else:
-            ax[i, j].set_visible(False)
-
-plt.subplots_adjust(hspace=0.5)
-plt.savefig(output_dir + '/aerosol_20210601-20211231-80-80.png')
+    plt.subplots_adjust(hspace=0.5)
+    plt.savefig(output_dir + '/aerosol_20210601-20211231-80-80.png')
 
