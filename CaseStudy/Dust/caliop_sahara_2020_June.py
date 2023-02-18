a06452b0f49cf5116ad0073149d93cf231671264
ruisong <rui.song@ucl.ac.uk>
2023-02-18 14:27:23 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/caliop_sahara_2020_June.py
+++ CaseStudy/Dust/caliop_sahara_2020_June.py
@@ -141,8 +141,9 @@ beta_all_sort = beta_all[:, sort_index]
 beta_all_sort[beta_all_sort<0] = np.nan
 aerosol_type_all_sort = aerosol_type_all[:, sort_index]
 
-aerosol_type_all_sort_mask = np.mean(aerosol_type_all_sort, axis = 0)
-print(aerosol_type_all_sort)
+aerosol_type_all_sort_mask = np.zeros((aerosol_type_all_sort.shape))
+aerosol_type_all_sort_mask[(aerosol_type_all_sort == 2) | (aerosol_type_all_sort == 5) |(aerosol_type_all_sort == 6) ] = 1
+aerosol_type_all_sort_mask = np.sum(aerosol_type_all_sort, axis = 0)
 aerosol_type_all_sort_mask_index = np.where(aerosol_type_all_sort_mask > 0)[0]
 print(aerosol_type_all_sort_mask_index)
 
