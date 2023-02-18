1f563bd2f501946e1e85d40736c1426d004bf18e
ruisong <rui.song@ucl.ac.uk>
2023-02-18 12:51:58 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/caliop_sahara_2020_June.py
+++ CaseStudy/Dust/caliop_sahara_2020_June.py
@@ -84,7 +84,7 @@ def read_caliop_data(caliop_file_path, lat_down, lat_up, lon_left, lon_right):
     if len(spatial_mask) > 0:
 
         # logger.info('Data found within the spatial window: %s', caliop_file_path)
-        print('Data found within the spatial window: %s', caliop_file_path)
+        print('Data found within the spatial window: ' %caliop_file_path)
         return caliop_utc[spatial_mask], caliop_latitude[spatial_mask], \
                caliop_longitude[spatial_mask], caliop_altitude, caliop_beta[:, spatial_mask], \
                caliop_aerosol_type[:, spatial_mask], caliop_Depolarization_Ratio[:, spatial_mask]
@@ -119,7 +119,8 @@ while start_date_datetime <= end_date_datetime:
             if caliop_data:
                 caliop_utc, caliop_latitude, caliop_longitude, caliop_altitude, caliop_beta, \
                 caliop_aerosol_type_mask, caliop_Depolarization_Ratio = caliop_data
-
+                print(len(caliop_utc))
+                print(caliop_latitude.shape)
                 datatime_all.extend(caliop_utc)
                 latitude_all.extend(caliop_latitude)
                 longitude_all.extend(caliop_longitude)
