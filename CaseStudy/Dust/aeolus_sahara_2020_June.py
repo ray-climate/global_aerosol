8508f092c264731479b534b5411d36544484228b
ruisong <rui.song@ucl.ac.uk>
2023-02-21 16:52:01 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_sahara_2020_June.py
+++ CaseStudy/Dust/aeolus_sahara_2020_June.py
@@ -167,9 +167,8 @@ for day in range(14, 27):
                 if alt_aeolus_m[n] > 0:
                     if (n + 1) < len(alt_aeolus_m):
                         # Resample data based on nearest altitude value less than current value in alt_caliop
-                        print(alt_caliop, alt_aeolus_m[n])
                         backscatter_resample[m, (alt_caliop < alt_aeolus_m[n]) & (alt_caliop > alt_aeolus_m[n + 1])] = \
-                        backscatter_resample[m, n]
+                        sca_mb_backscatter[m, n]
 
         print(backscatter_resample[backscatter_resample>0])
         quit()
