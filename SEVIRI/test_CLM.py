aec03bc01b4dbe4e2125985a8e9355f6925081b0
ruisong <rui.song@ucl.ac.uk>
2023-03-04 15:48:12 +0000

    get aeolus from netcdf format data

--- SEVIRI/test_CLM.py
+++ SEVIRI/test_CLM.py
@@ -9,11 +9,11 @@ from satpy.scene import Scene
 
 def get_SEVIRI_CLM(file_path):
 
-    scn = Scene(reader='seviri_l2_grib', filenames=file_path)
+    scn = Scene(reader='seviri_l2_grib', filenames=[file_path])
     print(scn)
 
 
 if __name__ == '__main__':
     get_SEVIRI_CLM(
-        '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_CLM/20200622/')
+        '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_CLM/20200622/MSG4-SEVI-MSGCLMK-0100-0100-20200622181500.000000000Z-NA')
 
