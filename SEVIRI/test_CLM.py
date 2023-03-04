12d5a0197317e1a600d75baf45c774c4e72fa2f6
ruisong <rui.song@ucl.ac.uk>
2023-03-04 17:09:32 +0000

    get aeolus from netcdf format data

--- SEVIRI/test_CLM.py
+++ SEVIRI/test_CLM.py
@@ -5,24 +5,25 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        04/03/2023 15:05
 
+from satpy import Scene
 from osgeo import gdal
 
 
 def get_SEVIRI_CLM(file_path):
-    """Read the SEVIRI CLM data from the downloaded file"""
-    dataset = gdal.Open(file_path, gdal.GA_ReadOnly)
-    # Read the first band of the dataset
-    band = dataset.GetRasterBand(1)
+    # """Read the SEVIRI CLM data from the downloaded file"""
+    # dataset = gdal.Open(file_path, gdal.GA_ReadOnly)
+    # # Read the first band of the dataset
+    # band = dataset.GetRasterBand(1)
+    #
+    # # Read the data from the band as a NumPy array
+    # data = band.ReadAsArray()
+    # geotransform = dataset.GetGeoTransform()
+    # projection = dataset.GetProjection()
+    # print(data.shape)
+    # print(geotransform)
+    # print(projection)
 
-    # Read the data from the band as a NumPy array
-    data = band.ReadAsArray()
-    geotransform = dataset.GetGeoTransform()
-    projection = dataset.GetProjection()
-    print(data.shape)
-    print(geotransform)
-    print(projection)
-
-
+    scn = Scene(sensor='seviri_l2_grib', filenames=[file_path])
 
 if __name__ == '__main__':
     get_SEVIRI_CLM(
