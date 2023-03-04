9670d90d66ce9f87c4932b89e7226d2a1d162ddc
ruisong <rui.song@ucl.ac.uk>
2023-03-04 16:04:47 +0000

    get aeolus from netcdf format data

--- SEVIRI/test_CLM.py
+++ SEVIRI/test_CLM.py
@@ -5,15 +5,26 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        04/03/2023 15:05
 
-from satpy.scene import Scene
+from osgeo import gdal
+
 
 def get_SEVIRI_CLM(file_path):
+    """Read the SEVIRI CLM data from the downloaded file"""
+    dataset = gdal.Open(file_path, gdal.GA_ReadOnly)
 
-    scn = Scene(reader='seviri_l2_grib', filenames=[file_path])
-    print(scn)
+    # Get the metadata dictionary
+    metadata = dataset.GetMetadata()
+
+    # Print the variables
+    for key, value in metadata.items():
+        if key.startswith('NETCDF_VAR'):
+            print(value)
+
 
 
 if __name__ == '__main__':
     get_SEVIRI_CLM(
-        '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_CLM/20200622/MSG4-SEVI-MSGCLMK-0100-0100-20200622181500.000000000Z-NA/')
+        '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_CLM/20200622/MSG4-SEVI-MSGCLMK-0100-0100-20200622181500.000000000Z-NA/MSG4-SEVI-MSGCLMK-0100-0100-20200622181500.000000000Z-NA.grb')
+
+
 
