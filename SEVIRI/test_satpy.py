86cc0f6f70dcb543331e6d8d4901b7742b3683ee
ruisong <rui.song@ucl.ac.uk>
2023-03-05 00:51:33 +0000

    get aeolus from netcdf format data

--- SEVIRI/test_satpy.py
+++ SEVIRI/test_satpy.py
@@ -11,7 +11,8 @@ def get_SEVIRI_coordinates(file_path):
 
     scn = Scene(reader='seviri_l1b_native', filenames=[file_path])
     scn.load(["IR_108"], upper_right_corner="NE")
-    res= scn['IR_108'].attrs['area'].get_lonlat_from_array_coordinates(1000,2000)
+    # res= scn['IR_108'].attrs['area'].get_lonlat_from_array_coordinates(1000,2000)
+    res= scn['IR_108'].attrs['area'].get_lonlats(1000,2000)
 
     print(res)
 
