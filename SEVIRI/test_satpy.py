34829f81f80012e0c730309f43794e02beb1225b
ruisong <rui.song@ucl.ac.uk>
2023-03-05 00:20:10 +0000

    get aeolus from netcdf format data

---
+++ SEVIRI/test_satpy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    test_satpy.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        05/03/2023 00:14
+
+from satpy import Scene
+
+def get_SEVIRI_coordinates(file_path):
+
+    scn = Scene(reader='seviri_l1b_native', filenames=[file_path])
+
+if __name__ == '__main__':
+
+    filename = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_Natural/20200614/MSG4-SEVI-MSG15-0100-NA-20200614001241.789000000Z-NA'
+    get_SEVIRI_coordinates(filename)
