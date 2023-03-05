6464591def3558688816a3fc4dfcd6e7358d0ef0
ruisong <rui.song@ucl.ac.uk>
2023-03-05 22:17:07 +0000

    get aeolus from netcdf format data

--- SEVIRI/test_CLM_basemap.py
+++ SEVIRI/test_CLM_basemap.py
@@ -10,6 +10,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import numpy as np
+import xarray as xr
 
 lon = np.load('/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI/SEVIRI_lon.npy')
 lat = np.load('/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI/SEVIRI_lat.npy')
@@ -20,9 +21,9 @@ lat[(np.isinf(lon)) | (np.isinf(lat)) | (np.isinf(CLM))] = 0
 CLM[(np.isinf(lon)) | (np.isinf(lat)) | (np.isinf(CLM))] = 0
 
 CLM_valid = np.zeros((CLM.shape))
-CLM_valid[:] = np.nan
 CLM_valid[CLM == 2] = 1
-print(CLM_valid)
+CLM_valid = xr.where(CLM_valid < 0, np.NaN, CLM_valid)
+
 bbox = [-70.,0.,30.,40.] # map boundaries
 # figure setup
 fig,ax = plt.subplots(figsize=(9,4),dpi=200)
@@ -37,7 +38,7 @@ m.drawcoastlines()
 m.drawcountries()
 states = m.drawstates() # draw state boundaries
 
-m.pcolor(lon, lat, CLM_valid, latlon=True)
+m.pcolor(lon, lat, CLM_valid.to_masked_array(), latlon=True)
 
 # draw parallels and meridians by every 5 degrees
 parallels = np.arange(bbox[1],bbox[3],10.)
