2059a34da6f1923f3d7c26e4e4b6fadd790c2cb1
ruisong <rui.song@ucl.ac.uk>
2023-03-05 22:43:05 +0000

    get aeolus from netcdf format data

--- SEVIRI/download_SEVIRI_CLM.py
+++ SEVIRI/download_SEVIRI_CLM.py
@@ -84,8 +84,8 @@ if __name__ == '__main__':
     data_dir = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_CLM/'
 
     # Define the start and end dates
-    start_date = '20200614-0000'
-    end_date =   '20200627-0000'
+    start_date = '20230315-2210'
+    end_date =   '20230315-2220'
 
     # Create the output directory if it doesn't exist
     if not os.path.exists(data_dir):
