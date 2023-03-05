136bd6d28b873981c6a55d8663b8cfe3fc399e5e
ruisong <rui.song@ucl.ac.uk>
2023-03-05 12:31:17 +0000

    get aeolus from netcdf format data

--- SEVIRI/test_satpy.py
+++ SEVIRI/test_satpy.py
@@ -9,8 +9,8 @@ from satpy import Scene
 
 def get_SEVIRI_coordinates(file_path):
 
-    # scn = Scene(reader='seviri_l1b_native', filenames=[file_path])
-    scn = Scene(reader='seviri_l2_grib', filenames=[file_path])
+    scn = Scene(reader='seviri_l1b_native', filenames=[file_path])
+    # scn = Scene(reader='seviri_l2_grib', filenames=[file_path])
     scn.load(["IR_108"], upper_right_corner="NE")
 
     print(scn["IR_108"].attrs["area"].get_lonlat_from_array_coordinates(1000, 2000))
@@ -24,6 +24,6 @@ def get_SEVIRI_coordinates(file_path):
 
 if __name__ == '__main__':
 
-    # filename = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_Natural/20200614/MSG4-SEVI-MSG15-0100-NA-20200614001241.789000000Z-NA/MSG4-SEVI-MSG15-0100-NA-20200614001241.789000000Z-NA.nat'
-    filename = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_CLM/20200622/MSG4-SEVI-MSGCLMK-0100-0100-20200622181500.000000000Z-NA/MSG4-SEVI-MSGCLMK-0100-0100-20200622181500.000000000Z-NA.grb'
+    filename = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_Natural/20200614/MSG4-SEVI-MSG15-0100-NA-20200614001241.789000000Z-NA/MSG4-SEVI-MSG15-0100-NA-20200614001241.789000000Z-NA.nat'
+    # filename = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_CLM/20200622/MSG4-SEVI-MSGCLMK-0100-0100-20200622181500.000000000Z-NA/MSG4-SEVI-MSGCLMK-0100-0100-20200622181500.000000000Z-NA.grb'
     get_SEVIRI_coordinates(filename)
