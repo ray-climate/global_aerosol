d96eabb0d4868eaa586dafd45a9a63a6ac73bf73
ruisong <rui.song@ucl.ac.uk>
2023-03-06 15:43:00 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_sahara_2020_June_nightime.py
+++ CaseStudy/Dust/aeolus_sahara_2020_June_nightime.py
@@ -151,129 +151,118 @@ for day in range(14, 27):
     start_date = '2020-06-%d' % day
     end_date = '2020-06-%d' % day
 
-    fig = plt.figure(constrained_layout=True, figsize=(25, 10))
-    gs = GridSpec(1, 8, figure=fig)
+    lon_left = meridional_boundary[0]
+    lon_right = meridional_boundary[1]
 
-    for k in range(len(meridional_boundary) - 1):
+    latitude_all = []
+    longitude_all = []
+    altitude_all = []
+    beta_all = []
+    ber_all = []
 
-        lon_left = meridional_boundary[k]
-        lon_right = meridional_boundary[k + 1]
+    # Parse start and end dates
+    start_date_datetime = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date_datetime = datetime.strptime(end_date, '%Y-%m-%d')
 
-        latitude_all = []
-        longitude_all = []
-        altitude_all = []
-        beta_all = []
-        ber_all = []
+    while start_date_datetime <= end_date_datetime:
 
-        # Parse start and end dates
-        start_date_datetime = datetime.strptime(start_date, '%Y-%m-%d')
-        end_date_datetime = datetime.strptime(end_date, '%Y-%m-%d')
+        year_i = '{:04d}'.format(start_date_datetime.year)
+        month_i = '{:02d}'.format(start_date_datetime.month)
+        day_i = '{:02d}'.format(start_date_datetime.day)
 
-        while start_date_datetime <= end_date_datetime:
+        aeolus_fetch_dir = os.path.join(AEOLUS_JASMIN_dir, f'{year_i}-{month_i}')
 
-            year_i = '{:04d}'.format(start_date_datetime.year)
-            month_i = '{:02d}'.format(start_date_datetime.month)
-            day_i = '{:02d}'.format(start_date_datetime.day)
+        for aeolus_file_name in os.listdir(aeolus_fetch_dir):
+            if aeolus_file_name.endswith('%s-%s-%s.nc'%(year_i,  month_i, day_i)):
 
-            aeolus_fetch_dir = os.path.join(AEOLUS_JASMIN_dir, f'{year_i}-{month_i}')
+                aeolus_file_path = os.path.join(aeolus_fetch_dir, aeolus_file_name)
 
-            for aeolus_file_name in os.listdir(aeolus_fetch_dir):
-                if aeolus_file_name.endswith('%s-%s-%s.nc'%(year_i,  month_i, day_i)):
+                (latitude, longitude, sca_mb_altitude,
+                 footprint_time_aeolus, sca_mb_backscatter, alpha_aeolus_mb,
+                 qc_aeolus_mb, ber_aeolus_mb, lod_aeolus_mb) = \
+                    extract_variables_from_aeolus(aeolus_file_path, logger)
 
-                    aeolus_file_path = os.path.join(aeolus_fetch_dir, aeolus_file_name)
+                spatial_mask = np.where((latitude > lat_down) & (latitude < lat_up) &
+                                        (longitude > lon_left) & (longitude < lon_right))[0]
 
-                    (latitude, longitude, sca_mb_altitude,
-                     footprint_time_aeolus, sca_mb_backscatter, alpha_aeolus_mb,
-                     qc_aeolus_mb, ber_aeolus_mb, lod_aeolus_mb) = \
-                        extract_variables_from_aeolus(aeolus_file_path, logger)
+                latitude_i = latitude[spatial_mask]
+                longitude_i = longitude[spatial_mask]
+                sca_mb_altitude = sca_mb_altitude[spatial_mask, :]
+                sca_mb_backscatter = sca_mb_backscatter[spatial_mask, :]
+                sca_mb_ber = ber_aeolus_mb[spatial_mask, :]
 
-                    spatial_mask = np.where((latitude > lat_down) & (latitude < lat_up) &
-                                            (longitude > lon_left) & (longitude < lon_right))[0]
+                latitude_all.extend(latitude_i)
+                longitude_all.extend(longitude_i)
 
-                    latitude_i = latitude[spatial_mask]
-                    longitude_i = longitude[spatial_mask]
-                    sca_mb_altitude = sca_mb_altitude[spatial_mask, :]
-                    sca_mb_backscatter = sca_mb_backscatter[spatial_mask, :]
-                    sca_mb_ber = ber_aeolus_mb[spatial_mask, :]
+                # plot_aeolus_basemap(latitude_i, longitude_i, lat_SEVIRI, lon_SEVIRI, CLM_valid, './test_fig.png')
 
-                    latitude_all.extend(latitude_i)
-                    longitude_all.extend(longitude_i)
+                try:
+                    beta_all = np.concatenate([beta_all, sca_mb_backscatter], axis=0)
+                    ber_all = np.concatenate([ber_all, sca_mb_ber], axis=0)
+                    altitude_all = np.concatenate([altitude_all, sca_mb_altitude], axis=0)
+                except:
+                    beta_all = np.copy(sca_mb_backscatter)
+                    ber_all = np.copy(sca_mb_ber)
+                    altitude_all = np.copy(sca_mb_altitude)
 
-                    # plot_aeolus_basemap(latitude_i, longitude_i, lat_SEVIRI, lon_SEVIRI, CLM_valid, './test_fig.png')
+        start_date_datetime += time_delta
+    print(len(latitude_all))
+    quit()
+    altitude_all[altitude_all == -1] = np.nan
+    beta_all[beta_all == -1.e6] = 0
+    ber_all[ber_all < 0.] = np.nan
+    # Convert altitude values from meters to kilometers
+    altitude_all = altitude_all * 1e-3
 
-                    try:
-                        beta_all = np.concatenate([beta_all, sca_mb_backscatter], axis=0)
-                        ber_all = np.concatenate([ber_all, sca_mb_ber], axis=0)
-                        altitude_all = np.concatenate([altitude_all, sca_mb_altitude], axis=0)
-                    except:
-                        beta_all = np.copy(sca_mb_backscatter)
-                        ber_all = np.copy(sca_mb_ber)
-                        altitude_all = np.copy(sca_mb_altitude)
+    # convert aeolus data with the given scaling factor: convert to km-1.sr-1
+    beta_all = beta_all * 1.e-6 * 1.e3
 
-            start_date_datetime += time_delta
-        print(len(latitude_all))
-        quit()
-        altitude_all[altitude_all == -1] = np.nan
-        beta_all[beta_all == -1.e6] = 0
-        ber_all[ber_all < 0.] = np.nan
-        # Convert altitude values from meters to kilometers
-        altitude_all = altitude_all * 1e-3
+    # Create empty array for resampled data, with same shape as alt_aeolus
+    backscatter_resample = np.zeros((altitude_all.shape[0], np.size(alt_caliop)))
+    # backscatter_resample[:] = np.nan
 
-        # convert aeolus data with the given scaling factor: convert to km-1.sr-1
-        beta_all = beta_all * 1.e-6 * 1.e3
+    # Iterate through rows and columns of alt_aeolus and data_aeolus
+    for m in range(altitude_all.shape[0]):
+        alt_aeolus_m = altitude_all[m, :]
+        for n in range(np.size(alt_aeolus_m)):
+            if alt_aeolus_m[n] > 0:
+                if (n + 1) < len(alt_aeolus_m):
+                    # Resample data based on nearest altitude value less than current value in alt_caliop
+                    backscatter_resample[m, (alt_caliop < alt_aeolus_m[n]) & (alt_caliop > alt_aeolus_m[n + 1])] = \
+                    beta_all[m, n]
 
-        # Create empty array for resampled data, with same shape as alt_aeolus
-        backscatter_resample = np.zeros((altitude_all.shape[0], np.size(alt_caliop)))
-        # backscatter_resample[:] = np.nan
+    ber_all_mask = np.zeros((ber_all.shape))
+    ber_all_mask[ber_all < BER_threshold] = 1.
+    ber_volume_sum = np.sum(ber_all_mask, axis=1)
 
-        # Iterate through rows and columns of alt_aeolus and data_aeolus
-        for m in range(altitude_all.shape[0]):
-            alt_aeolus_m = altitude_all[m, :]
-            for n in range(np.size(alt_aeolus_m)):
-                if alt_aeolus_m[n] > 0:
-                    if (n + 1) < len(alt_aeolus_m):
-                        # Resample data based on nearest altitude value less than current value in alt_caliop
-                        backscatter_resample[m, (alt_caliop < alt_aeolus_m[n]) & (alt_caliop > alt_aeolus_m[n + 1])] = \
-                        beta_all[m, n]
+    beta_volume_sum = np.sum(backscatter_resample, axis=1)
 
-        ber_all_mask = np.zeros((ber_all.shape))
-        ber_all_mask[ber_all < BER_threshold] = 1.
-        ber_volume_sum = np.sum(ber_all_mask, axis=1)
+    axk = fig.add_subplot(gs[0, k])
 
-        beta_volume_sum = np.sum(backscatter_resample, axis=1)
+    figk = plt.plot(np.mean(backscatter_resample[beta_volume_sum > 0, :], axis=0), alt_caliop, 'r-*', lw=2,
+                    label='no filter')
+    plt.plot(np.mean(backscatter_resample[(beta_volume_sum > 0) & (ber_volume_sum < 1), :], axis=0),
+             alt_caliop, 'b-*', lw=2, label='cloud removed')
 
-        axk = fig.add_subplot(gs[0, k])
-
-        figk = plt.plot(np.mean(backscatter_resample[beta_volume_sum > 0, :], axis=0), alt_caliop, 'r-*', lw=2,
-                        label='no filter')
-        plt.plot(np.mean(backscatter_resample[(beta_volume_sum > 0) & (ber_volume_sum < 1), :], axis=0),
-                 alt_caliop, 'b-*', lw=2, label='cloud removed')
-
-        if meridional_boundary[k] < 0:
-            if meridional_boundary[k+1] < 0:
-                axk.set_xlabel('[%s$^{\circ}$ W - %s$^{\circ}$ W]' % (abs(meridional_boundary[k]), abs(meridional_boundary[k+1])), fontsize=15)
-            else:
-                axk.set_xlabel('[%s$^{\circ}$ W - %s$^{\circ}$ E]' % (abs(meridional_boundary[k]), abs(meridional_boundary[k+1])), fontsize=15)
+    if meridional_boundary[k] < 0:
+        if meridional_boundary[k+1] < 0:
+            axk.set_xlabel('[%s$^{\circ}$ W - %s$^{\circ}$ W]' % (abs(meridional_boundary[k]), abs(meridional_boundary[k+1])), fontsize=15)
         else:
-            if meridional_boundary[k+1] < 0:
-                axk.set_xlabel('[%s$^{\circ}$ E - %s$^{\circ}$ W]' % (abs(meridional_boundary[k]), abs(meridional_boundary[k+1])), fontsize=15)
-            else:
-                axk.set_xlabel('[%s$^{\circ}$ E - %s$^{\circ}$ E]' % (abs(meridional_boundary[k]), abs(meridional_boundary[k+1])), fontsize=15)
+            axk.set_xlabel('[%s$^{\circ}$ W - %s$^{\circ}$ E]' % (abs(meridional_boundary[k]), abs(meridional_boundary[k+1])), fontsize=15)
+    else:
+        if meridional_boundary[k+1] < 0:
+            axk.set_xlabel('[%s$^{\circ}$ E - %s$^{\circ}$ W]' % (abs(meridional_boundary[k]), abs(meridional_boundary[k+1])), fontsize=15)
+        else:
+            axk.set_xlabel('[%s$^{\circ}$ E - %s$^{\circ}$ E]' % (abs(meridional_boundary[k]), abs(meridional_boundary[k+1])), fontsize=15)
 
-        # axk.set_ylabel('Averaged photon counts', fontsize=15)
-        for tick in axk.xaxis.get_major_ticks():
-            tick.label.set_fontsize(15)
-        for tick in axk.yaxis.get_major_ticks():
-            tick.label.set_fontsize(15)
-        # axk.legend(loc='upper right', fontsize=15)
-        axk.set_xscale('log')
-        axk.set_xlim([1.e-4, 2.e-2])
-        axk.set_ylim([0., 8])
-        axk.grid()
+    # axk.set_ylabel('Averaged photon counts', fontsize=15)
+    for tick in axk.xaxis.get_major_ticks():
+        tick.label.set_fontsize(15)
+    for tick in axk.yaxis.get_major_ticks():
+        tick.label.set_fontsize(15)
+    # axk.legend(loc='upper right', fontsize=15)
+    axk.set_xscale('log')
+    axk.set_xlim([1.e-4, 2.e-2])
+    axk.set_ylim([0., 8])
+    axk.grid()
 
-    fig.suptitle("%s-%s-%s" % (year_i, month_i, day_i), fontsize=17)
-    fig.text(0.5, 0.02, 'Backscatter coefficient [km$^{-1}$sr$^{-1}$]', ha='center', va='center', fontsize=17)
-    fig.text(0.02, 0.5, 'Heights [km]', ha='center', va='center', rotation='vertical', fontsize=17)
-    fig.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.95, wspace=0.3, hspace=0.2)
-
-    plt.savefig(output_dir + '/aeolus_dust_backscatter_%s-%s-%s.png' % (year_i, month_i, day_i))
