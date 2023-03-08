c10dee306ec54e678d209ff842ab08489107dc14
ruisong <rui.song@ucl.ac.uk>
2023-03-08 17:18:25 +0000

    get aeolus from netcdf format data

--- SEVIRI/test_satpy.py
+++ SEVIRI/test_satpy.py
@@ -5,6 +5,8 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        05/03/2023 00:14
 
+from pyresample import create_area_def
+import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 from satpy import Scene
 import numpy as np
@@ -15,10 +17,38 @@ def plot_SEVIRI_images(file_path):
     scn.load([composite], upper_right_corner="NE")
     scn.save_dataset(composite, filename='./seviri_dust_rgb.png')
 
-    crs = scn[composite].attrs['area'].to_cartopy_crs()
-    ax = plt.axes(projection=crs)
-    plt.imshow(scn[composite].attrs['area'].values, transform=crs, extent=crs.bounds, origin='upper')
-    plt.savefig('./seviri_dust_rgb_local.png')
+    # Create area definition
+    ext = [-110.629439, 31.594328, -95.933875, 37.384890]
+    width = 3840
+    height = 2160
+    area_def = create_area_def('TX/OK/NM',
+                               {'proj': 'longlat', 'datum': 'WGS84'},
+                               area_extent=ext,
+                               shape=(height, width),
+                               units='degrees',
+                               description='TX/OK/NM border')
+
+    # Resample to AreaDefinition
+    new_scn = scn.resample(area_def)
+    new_scn.load([composite])
+
+    # Save without Cartopy
+    new_scn.save_dataset(composite, filename='seviri_dust_rgb_local.png')
+
+    # # Plot composite
+    # CRS = new_scn[composite].attrs['area'].to_cartopy_crs()
+    # fig = plt.figure(figsize=(30, 25))
+    # ax = fig.add_subplot(1, 1, 1, projection=CRS)
+    # new_scn[composite].plot.imshow(rgb='bands', transform=CRS, origin='upper')
+    # ax.add_feature(ccrs.cartopy.feature.STATES, linewidth=0.25)
+    #
+    # # Save plot
+    # plt.savefig('example.png')
+    #
+    # crs = scn[composite].attrs['area'].to_cartopy_crs()
+    # ax = plt.axes(projection=crs)
+    # plt.imshow(scn[composite].attrs['area'].values, transform=crs, extent=crs.bounds, origin='upper')
+    # plt.savefig('./seviri_dust_rgb_local.png')
 
 
 if __name__ == '__main__':
