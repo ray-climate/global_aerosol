8a1a158e836626ca2602e7ddc69860dff11ed7ca
ruisong <rui.song@ucl.ac.uk>
2023-03-08 16:14:32 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_sahara_2020_June_nightime.py
+++ CaseStudy/Dust/aeolus_sahara_2020_June_nightime.py
@@ -132,6 +132,38 @@ def plot_aeolus_basemap(lat_aeolus, lon_aeolus, lat_SEVIRI, lon_SEVIRI, CLM_SEVI
     plt.legend(fontsize=10)
     plt.savefig(save_fig, dpi=200)
 
+def plot_aeolus_basemap_dust(lat_aeolus, lon_aeolus, lat_SEVIRI, lon_SEVIRI, dust_RGB, save_fig):
+
+
+    bbox = [-60., 0., 30., 40.]  # map boundaries
+    fig, ax = plt.subplots(figsize=(9, 4), dpi=200)
+    # ax.set_axis_off()
+    # set basemap boundaries, cylindrical projection, 'i' = intermediate resolution
+    m = Basemap(llcrnrlon=bbox[0], llcrnrlat=bbox[1], urcrnrlon=bbox[2],
+                urcrnrlat=bbox[3], resolution='i', projection='cyl')
+
+    x_aeolus, y_aeolus = m(lon_aeolus, lat_aeolus)
+
+    # m.fillcontinents(color='#d9b38c',lake_color='#bdd5d5') # continent colors
+    # m.drawmapboundary(fill_color='#bdd5d5') # ocean color
+    m.drawcoastlines()
+    m.drawcountries()
+    states = m.drawstates()  # draw state boundaries
+
+    # m.pcolormesh(lon, lat, np.ma.masked_array(CLM_valid, mask), cmap='gray', latlon=True)
+
+    # draw parallels and meridians by every 5 degrees
+    parallels = np.arange(bbox[1], bbox[3], 10.)
+    m.drawparallels(parallels, labels=[1, 0, 0, 0], fontsize=10)
+    meridians = np.arange(bbox[0], bbox[2], 10.)
+    m.drawmeridians(meridians, labels=[0, 0, 0, 1], fontsize=10)
+
+    m.imshow(lon_SEVIRI, lat_SEVIRI, dust_RGB)
+
+    m.scatter(x_aeolus, y_aeolus, marker='o', color='blue', s=10, label='AEOLUS')
+    plt.legend(fontsize=10)
+    plt.savefig(save_fig, dpi=200)
+
 def plot_seviri(data, save_fig):
     fig, ax = plt.subplots(figsize=(9, 4), dpi=200)
     plt.imshow(data)
@@ -151,6 +183,7 @@ def get_SEVIRI_CLM(file_path):
 lon_SEVIRI = np.load('/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI/SEVIRI_lon.npy')
 lat_SEVIRI = np.load('/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI/SEVIRI_lat.npy')
 CLM_SEVIRI = np.load('/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI/SEVIRI_CLM.npy')
+dust_SEVIRI = plt.imread('/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI/seviri_dust_rgb_20200626194243.png')
 
 lon_SEVIRI[(np.isinf(lon_SEVIRI)) | (np.isinf(lat_SEVIRI)) | (np.isinf(CLM_SEVIRI))] = 0
 lat_SEVIRI[(np.isinf(lon_SEVIRI)) | (np.isinf(lat_SEVIRI)) | (np.isinf(CLM_SEVIRI))] = 0
@@ -282,7 +315,9 @@ for day in range(14, 27):
                 logger.warning('No SEVIRI CLM file found for the given time: %s' % central_time)
 
     # plot_aeolus_basemap(lat_ascending, lon_ascending, lat_SEVIRI, lon_SEVIRI, CLM_valid, './test_%s.png'%day)
-    plot_seviri(SEVIRI_CLM_data, './fulldisk_test_%s.png'%day)
+    plot_aeolus_basemap_dust(lat_ascending, lon_ascending, lat_SEVIRI, lon_SEVIRI, dust_SEVIRI, './dust_%s.png'%day)
+    quit()
+    # plot_seviri(SEVIRI_CLM_data, './fulldisk_test_%s.png'%day)
     #
     # beta_volume_sum = np.sum(backscatter_resample, axis=1)
     #
