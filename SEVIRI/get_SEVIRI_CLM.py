e45816bb88bcba73cd127b2a1cb15190c138abe2
ruisong <rui.song@ucl.ac.uk>
2023-03-09 21:44:03 +0000

    get aeolus from netcdf format data

--- SEVIRI/get_SEVIRI_CLM.py
+++ SEVIRI/get_SEVIRI_CLM.py
@@ -158,7 +158,9 @@ def get_SEVIRI_CLM_cartopy(SEVIRI_HR_file_path, SEVIRI_CLM_file_path, extent, ti
     scn = Scene(reader='seviri_l1b_native', filenames=[SEVIRI_HR_file_path])
     composite = 'dust'
     scn.load([composite], upper_right_corner="NE")
-    scn[composite].values[0,:,:] = data.T
+    print(scn[composite].values[0, :, :].shape)
+    print(data.T.shape)
+    scn[composite].values[0, :, :] = data.T
     scn[composite].values[1, :, :] = data.T
     scn[composite].values[2, :, :] = data.T
     width = 4000
