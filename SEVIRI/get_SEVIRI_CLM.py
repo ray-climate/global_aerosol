14b1cd13687583f4c4bf942d4f8621cc4baaf48f
ruisong <rui.song@ucl.ac.uk>
2023-03-09 14:28:28 +0000

    get aeolus from netcdf format data

--- SEVIRI/get_SEVIRI_CLM.py
+++ SEVIRI/get_SEVIRI_CLM.py
@@ -28,11 +28,34 @@ def get_SEVIRI_CLM_time(dt):
     formatted = datetime.strftime(rounded, '%Y%m%d%H%M%S')
     return formatted
 
+def get_HRSEVIRI_time(dt, interval=15):
+    """
+    Round a datetime object to the closest minute interval
+    specified by the 'interval' parameter.
+    """
+    minutes = dt.minute
+    # Calculate the number of minutes to add or subtract to get to the nearest interval
+    remainder = minutes % interval
+    if remainder < interval // 2:
+        rounded_minutes = minutes - remainder
+    else:
+        rounded_minutes = minutes + (interval - remainder)
+    # Handle cases where rounded_minutes is greater than or equal to 60
+    if rounded_minutes >= 60:
+        dt += timedelta(hours=1)
+        dt = dt.replace(minute=0)
+        rounded_minutes -= 60
+    # Round the datetime object to the nearest interval
+    rounded = dt.replace(minute=rounded_minutes, second=0, microsecond=0)
+    formatted = datetime.strftime(rounded, '%Y%m%d%H%M%S')
+    return formatted
+
+
 
 if __name__ == '__main__':
 
     now = datetime.now()
-    formatted = get_SEVIRI_CLM_time(now)
+    formatted = get_HRSEVIRI_time(now)
     print("Original datetime:", now)
     print("Formatted rounded datetime:", formatted)
 
