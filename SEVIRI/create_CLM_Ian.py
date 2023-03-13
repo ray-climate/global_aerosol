36d308436e9709bac85240cc891adca30d45b9c1
ruisong <rui.song@ucl.ac.uk>
2023-03-13 11:52:55 +0000

    get aeolus from netcdf format data

--- SEVIRI/create_CLM_Ian.py
+++ SEVIRI/create_CLM_Ian.py
@@ -10,7 +10,7 @@ import numpy as np
 
 # create SEVIRI cloud mask using modified method from Ian (https://doi.org/10.1029/2011JD016845)
 
-def create_108_087_ref(start_date_str, end_date_str, HRSEVIRI_dir):
+def create_108_087_ref(start_date_str, end_date_str, HRSEVIRI_dir, CLMSEVIRI_dir):
 
     # create the 108-087 BTD reference image using cloud-free images from specified input time range
     # input: start_date_str, end_date_str (datetime string object)
@@ -23,16 +23,26 @@ def create_108_087_ref(start_date_str, end_date_str, HRSEVIRI_dir):
     # a loop to read all days between start_date and end_date
     current_date = start_date
     while current_date <= end_date:
-        current_date_str = datetime.strftime(current_date, '%Y%m%d')
-        for sub_dir in os.listdir(HRSEVIRI_dir + '/%s/'%current_date_str):
+        current_HRSEVIRI_date_str = datetime.strftime(current_date, '%Y%m%d')
+        for sub_dir in os.listdir(HRSEVIRI_dir + '/%s/'%current_HRSEVIRI_date_str):
             if sub_dir.endswith('-NA'):
-                HRSEVIRI_path = HRSEVIRI_dir + '/%s/%s'%(current_date_str, sub_dir)
+                HRSEVIRI_path = HRSEVIRI_dir + '/%s/%s'%(current_HRSEVIRI_date_str, sub_dir)
                 HRSEVIRI_file = os.path.join(HRSEVIRI_path, sub_dir + '.nat')
-
                 HRSEVIRI_exact_time_str = sub_dir.split('-')[5][0:12]
                 CLMSEVIRI_exact_time_str = datetime.strftime(datetime.strptime(HRSEVIRI_exact_time_str, '%Y%m%d%H%M') + timedelta(minutes=3), '%Y%m%d%H%M')
-                print(HRSEVIRI_exact_time_str)
-                print(CLMSEVIRI_exact_time_str)
+
+                current_CLMSEVIRI_date_str = CLMSEVIRI_exact_time_str[0:8]
+                for sub_dir2 in os.listdir(CLMSEVIRI_dir + '/%s/' % current_CLMSEVIRI_date_str):
+                    if CLMSEVIRI_exact_time_str in sub_dir2:
+                        CLMSEVIRI_path = CLMSEVIRI_dir + '/%s/%s' % (current_CLMSEVIRI_date_str, sub_dir2)
+                        CLMSEVIRI_file = os.path.join(CLMSEVIRI_path, sub_dir2 + '.nat')
+                        print(HRSEVIRI_file)
+                        print(CLMSEVIRI_file)
+                        quit()
+        # check if CLMSEVIRI_exact_time_str exists in any subdirectory of CLMSEVIRI_dir
+
+
+
                 quit()
 
         # current_HRSEVIRI_file = HRSEVIRI_dir + 'HRSEVIRI_' + current_date_str + '.nc'
@@ -56,7 +66,8 @@ def create_108_087_ref(start_date_str, end_date_str, HRSEVIRI_dir):
 if __name__ == '__main__':
 
     HRSEVIRI_dir = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_data_collection/SEVIRI_HRSEVIRI'
+    CLMSEVIRI_dir = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/SEVIRI_data_collection/SEVIRI_CLM/'
     start_date_str = '20200614-0000'
     end_date_str = '20200627-2359'
-    create_108_087_ref(start_date_str, end_date_str, HRSEVIRI_dir)
+    create_108_087_ref(start_date_str, end_date_str, HRSEVIRI_dir, CLMSEVIRI_dir)
 
