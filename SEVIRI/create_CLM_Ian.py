920b5180a097c86c847d5982806fe263d06b3f6f
ruisong <rui.song@ucl.ac.uk>
2023-03-13 11:33:27 +0000

    get aeolus from netcdf format data

--- SEVIRI/create_CLM_Ian.py
+++ SEVIRI/create_CLM_Ian.py
@@ -26,8 +26,9 @@ def create_108_087_ref(start_date_str, end_date_str, HRSEVIRI_dir):
         current_date_str = datetime.strftime(current_date, '%Y%m%d')
         for sub_dir in os.listdir(HRSEVIRI_dir + '/%s/'%current_date_str):
             if sub_dir.endswith('-NA'):
-                SEVIRI_path = HRSEVIRI_dir + '/%s/%s'%(current_date_str, sub_dir)
-                print(SEVIRI_path)
+                HRSEVIRI_path = HRSEVIRI_dir + '/%s/%s'%(current_date_str, sub_dir)
+                HRSEVIRI_file = os.path.join(HRSEVIRI_path, sub_dir + '.nat')
+                print(HRSEVIRI_file)
         # current_HRSEVIRI_file = HRSEVIRI_dir + 'HRSEVIRI_' + current_date_str + '.nc'
         # print('Reading HRSEVIRI file: ', current_HRSEVIRI_file)
         # current_HRSEVIRI_data = xr.open_dataset(current_HRSEVIRI_file)
