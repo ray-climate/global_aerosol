0c2a7d1d6af5880445cf2a5fd23944694721f498
ruisong <rui.song@ucl.ac.uk>
2023-03-15 15:30:31 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_caliop_sahara2020_night.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_night.py
@@ -223,8 +223,7 @@ for i in range((end_date - start_date).days + 1):
             lat_ascending.append(aeolus_latitude_all[lat_sublists[m][0]:lat_sublists[m][-1]])
             lon_ascending.append(aeolus_longitude_all[lat_sublists[m][0]:lat_sublists[m][-1]])
             time_ascending.append(aeolus_time_all[lat_sublists[m][0]:lat_sublists[m][-1]])
-
-
+    print(time_ascending)
     time_ascending_array = np.array(time_ascending).reshape(1, np.array(time_ascending).size)
 
     central_time = time_ascending_array[0, int(np.size(time_ascending_array)/2)]
