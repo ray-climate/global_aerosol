e2b053898c9ab157cc9cd8fa218343bb69072f30
ruisong <rui.song@ucl.ac.uk>
2023-03-16 15:10:18 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_caliop_sahara2020_night.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_night.py
@@ -337,7 +337,7 @@ for i in range((end_date - start_date).days + 1):
                 caliop_time_ascending.append(caliop_time_all[lat_sublists[m][0]:lat_sublists[m][-1]])
         except:
             print('Only one data point in this orbit, ignore it')
-    print(caliop_lat_ascending)
+    print(caliop_lat_ascending[3][::20])
     print(caliop_time_ascending[3])
     ############# separate caliop data into different orbits ############################
 
