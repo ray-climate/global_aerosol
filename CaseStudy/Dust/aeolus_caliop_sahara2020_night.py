cc9e5a928422a7ceae87ca65caad457841e8cb3d
ruisong <rui.song@ucl.ac.uk>
2023-03-16 15:41:26 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_caliop_sahara2020_night.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_night.py
@@ -318,7 +318,7 @@ for i in range((end_date - start_date).days + 1):
 
     j = 1
     while j < len(caliop_latitude_all):
-        if (abs(caliop_latitude_all[j] - caliop_latitude_all[lat_sublists[-1][-1]]) >= lat_jump_threshold) & ((caliop_latitude_all[j] - caliop_latitude_all[lat_sublists[-1][-1]]) < 0.):
+        if (abs(caliop_latitude_all[j] - caliop_latitude_all[lat_sublists[-1][-1]]) >= lat_jump_threshold) & ((caliop_latitude_all[j] - caliop_latitude_all[lat_sublists[-1][-1]]) > 0.):
             lat_sublists.append([j])
         else:
             lat_sublists[-1].append(j)
