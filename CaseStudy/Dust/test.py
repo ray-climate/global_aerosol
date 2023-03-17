de3b0fff0e64ddc75f1a6d8af7bbc9d68c7eb6b9
ruisong <rui.song@ucl.ac.uk>
2023-03-17 18:10:28 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/test.py
+++ CaseStudy/Dust/test.py
@@ -5,19 +5,12 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        06/03/2023 15:17
 
-import numpy as np
-from scipy.spatial import cKDTree
+import matplotlib.pyplot as plt
 
-lat_grid = np.random.uniform(-90, 90, size=(5000, 5000))
-lon_grid = np.random.uniform(-180, 180, size=(5000, 5000))
-
-lat_search = np.random.uniform(-90, 90, size=(1000, 1))
-lon_search = np.random.uniform(-180, 180, size=(1000, 1))
-
-coords = np.stack((lat_grid.ravel(), lon_grid.ravel()), axis=-1)
-tree = cKDTree(coords)
-
-search_points = np.hstack((lat_search, lon_search))
-distances, indices = tree.query(search_points)
-
-print(distances, indices)
+circle_cross = plt.Circle((0,0), radius=1, fill=False, linestyle='--')
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+ax.add_artist(circle_cross)
+ax.axhline(y=0, color='k', linestyle='--')
+ax.axvline(x=0, color='k', linestyle='--')
+plt.show()
