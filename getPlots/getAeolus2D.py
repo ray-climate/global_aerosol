fb4e0820fb0d8f428b9d6678ec71c6d6d67f57d5
ruisong <rui.song@ucl.ac.uk>
2023-03-18 18:12:16 +0000

    get aeolus from netcdf format data

--- getPlots/getAeolus2D.py
+++ getPlots/getAeolus2D.py
@@ -36,17 +36,12 @@ def getAeolus2Dbeta(lon, alt, beta, aeolus_mask, extent, save_str):
 
     for i in range(len(index)):
         for j in range(len(alt[index[i]])-1):
-            # try:
+            try:
                 if (np.isnan(alt[index[i]][j]) == False) & (np.isnan(alt[index[i]][j+1]) == False):
                     lon_range = (lon[index[i]-1] + lon[index[i]])/2., (lon[index[i]+1] + lon[index[i]])/2.
-                    print(beta[index[i]][j])
-                    print(111, beta2D_proj[(altitude_grid_regular <= alt[index[i]][j]) & (altitude_grid_regular >= alt[index[i]][j+1]) & (longitude_grid_regular <= np.max(lon_range)) & (longitude_grid_regular >= np.min(lon_range))])
                     beta2D_proj[(altitude_grid_regular <= alt[index[i]][j]) & (altitude_grid_regular >= alt[index[i]][j+1]) & (longitude_grid_regular <= np.max(lon_range)) & (longitude_grid_regular >= np.min(lon_range))] = beta[index[i]][j]
-
-                    # beta2D_proj[index[i], np.where((altitude_grid_regular[index[i], :] <= alt[index[i]][j]) & (
-                    #             altitude_grid_regular[index[i], :] >= alt[index[i]][j+1]))] = beta[index[i]][j]
-            # except:
-            #     pass
+            except:
+                pass
 
     fig, ax = plt.subplots(figsize=(30, 15))
     mappable = plt.pcolormesh(longitude_grid_regular, altitude_grid_regular, beta2D_proj, norm=colors.LogNorm(vmin=1.e-5, vmax=1.e-2), cmap='viridis')
@@ -56,22 +51,18 @@ def getAeolus2Dbeta(lon, alt, beta, aeolus_mask, extent, save_str):
     # Add the colorbar to the divider
     cax = divider.append_axes("right", size="1.5%", pad=0.1)
     # Create the colorbar
-    cbar = plt.colorbar(mappable, cax=cax, extend='both', shrink=0.6)
-
-    # cbar = plt.colorbar( shrink=0.8, pad=0.002)
+    cbar = plt.colorbar(mappable, cax=cax, extend='both', shrink=0.3)
     cbar.set_label('[km$^{-1}$sr$^{-1}$]', fontsize=25, rotation=90)
     cbar.ax.tick_params(labelsize=20)
 
     ax.set_xlabel('Longitude', fontsize=25)
     ax.set_ylabel('Height [km]', fontsize=25)
 
-    # ax3.set_xlim([np.min(lat_caliop), np.max(lat_caliop)])
-    # ax3.set_ylim([0., 25.])
-    # ax3.set_title('AEOLUS L2 Backscatter coeff.', fontsize=30)
-    # for tick in ax3.xaxis.get_major_ticks():
-    #     tick.label.set_fontsize(25)
-    # for tick in ax3.yaxis.get_major_ticks():
-    #     tick.label.set_fontsize(25)
-    plt.savefig(save_str, dpi=300, bbox_inches='tight')
-    print(beta2D_proj)
-    # return beta2D
+    ax.set_xlim([np.min(longitude_range), np.max(longitude_range)])
+    ax.set_ylim([np.min(altitude_range), np.max(altitude_range)])
+    ax.set_title('AEOLUS L2 Backscatter coeff.', fontsize=30)
+    for tick in ax.xaxis.get_major_ticks():
+        tick.label.set_fontsize(25)
+    for tick in ax.yaxis.get_major_ticks():
+        tick.label.set_fontsize(25)
+    plt.savefig(save_str, dpi=300)
