30d07b8086a0c2c029a98048086905b71d88ec1d
ruisong <rui.song@ucl.ac.uk>
2023-03-18 17:43:34 +0000

    get aeolus from netcdf format data

--- getPlots/getAeolus2D.py
+++ getPlots/getAeolus2D.py
@@ -37,12 +37,16 @@ def getAeolus2Dbeta(lon, alt, beta, aeolus_mask, extent, save_str):
         for j in range(len(alt[index[i]])-1):
             try:
                 if (np.isnan(alt[index[i]][j]) == False) & (np.isnan(alt[index[i]][j+1]) == False):
-                    beta2D_proj[index[i], np.where((altitude_grid_regular[index[i], :] <= alt[index[i]][j]) & (
-                                altitude_grid_regular[index[i], :] >= alt[index[i]][j+1]))] = beta[index[i]][j]
+                    print(lon[index[i]-1], lon[index[i]+1])
+                    # beta2D_proj[(altitude_grid_regular <= alt[index[i]][j]) & (altitude_grid_regular >= alt[index[i]][j+1]), (longitude_grid_regular <=)]   = beta[index[i]][j]
+
+
+                    # beta2D_proj[index[i], np.where((altitude_grid_regular[index[i], :] <= alt[index[i]][j]) & (
+                    #             altitude_grid_regular[index[i], :] >= alt[index[i]][j+1]))] = beta[index[i]][j]
             except:
                 pass
-
-    fig, ax = plt.subplots()
+    quit()
+    fig, ax = plt.subplots(figsize=(30, 15))
     mappable = plt.pcolormesh(longitude_grid_regular, altitude_grid_regular, beta2D_proj, norm=colors.LogNorm(vmin=1.e-5, vmax=1.e-2), cmap='viridis')
     # Create an axes divider for the main plot
     divider = make_axes_locatable(ax)
@@ -53,11 +57,11 @@ def getAeolus2Dbeta(lon, alt, beta, aeolus_mask, extent, save_str):
     cbar = plt.colorbar(mappable, cax=cax, extend='both', shrink=0.6)
 
     # cbar = plt.colorbar( shrink=0.8, pad=0.002)
-    cbar.set_label('[km$^{-1}$sr$^{-1}$]', fontsize=30, rotation=90)
+    cbar.set_label('[km$^{-1}$sr$^{-1}$]', fontsize=25, rotation=90)
     cbar.ax.tick_params(labelsize=20)
 
-    ax.set_xlabel('Longitude', fontsize=30)
-    ax.set_ylabel('Height [km]', fontsize=30)
+    ax.set_xlabel('Longitude', fontsize=25)
+    ax.set_ylabel('Height [km]', fontsize=25)
 
     # ax3.set_xlim([np.min(lat_caliop), np.max(lat_caliop)])
     # ax3.set_ylim([0., 25.])
