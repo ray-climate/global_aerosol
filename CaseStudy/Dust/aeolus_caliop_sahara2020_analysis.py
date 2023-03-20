82dadbfd18865311790cefd7a290e3e92777c376
ruisong <rui.song@ucl.ac.uk>
2023-03-20 11:19:28 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
@@ -16,11 +16,14 @@ input_sat = str(sys.argv[1]) # input satellite, either 'aeolus' or 'caliop'
 # input_mode = str(sys.argv[2]) # input mode, either 'ascending' or 'descending'
 
 input_path = './aeolus_caliop_sahara2020_extraction_output/'
-
+beta_all = []
 for npz_file in os.listdir(input_path):
     if npz_file.endswith('.npz') & (input_sat in npz_file):
         # print the file name and variables in the file
         print(npz_file)
         alt = np.load(input_path + npz_file, allow_pickle=True)['alt']
         beta = np.load(input_path + npz_file, allow_pickle=True)['beta']
+        beta_all = np.append(beta_all, beta)
         print(beta.shape)
+beta_all = np.asarray(beta_all)
+print(beta_all).shape
