fb26f4defe648cfa79a32aad8fdcae057ad35210
ruisong <rui.song@ucl.ac.uk>
2023-03-20 14:26:49 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
@@ -28,7 +28,7 @@ for npz_file in os.listdir(input_path):
             beta_caliop_all = np.concatenate((beta_caliop_all, beta), axis=1)
         except:
             beta_caliop_all = np.copy(beta)
-quit()
+
 beta_caliop_mask = np.zeros((beta_caliop_all.shape))
 beta_caliop_mask[beta_caliop_all > 0.0] = 1.0
 
@@ -52,6 +52,7 @@ alt_aeolus_mean = (alt_aeolus_mean[1:] + alt_aeolus_mean[:-1]) / 2.0
 retrieval_numbers_caliop_all = np.sum(beta_caliop_mask, axis=1)
 retrieval_numbers_aeolus_all = np.sum(beta_aeolus_all, axis=0)
 
+############# retrieval number #############
 # Set font parameters
 font = {'family': 'serif',
         'weight': 'normal',
@@ -74,8 +75,6 @@ plt.xlabel('Retrieval numbers', fontsize=16)
 # Set title
 plt.title(f'Aerosol retrievals over the Sahara \n $14^{{th}}$ - $24^{{th}}$ June 2020', fontsize=18, y=1.05)
 
-
-
 # Set x-axis and y-axis ticks
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
@@ -89,3 +88,36 @@ output_path = input_path + f'retrieval_numbers.png'
 plt.savefig(output_path, dpi=300)
 plt.close()
 
+############# backscatter plot #############
+beta_caliop_all[beta_caliop_all<0] = np.nan
+beta_caliop_mean = np.nanmean(beta_caliop_all, axis=1)
+
+plt.figure(figsize=(8, 12))
+plt.plot(beta_caliop_mean, alt_caliop, 'r', label='Caliop')
+# plt.plot(retrieval_numbers_aeolus_all / np.max(retrieval_numbers_aeolus_all), alt_aeolus_mean, 'k', label='Aeolus Retrieval numbers')
+# retrieval_numbers_aeolus_all_norm = retrieval_numbers_aeolus_all / np.max(retrieval_numbers_aeolus_all)
+# for i in range(len(retrieval_numbers_aeolus_all_norm)-1):
+#     plt.plot([retrieval_numbers_aeolus_all_norm[i], retrieval_numbers_aeolus_all_norm[i]], [alt_aeolus_mean[i], alt_aeolus_mean[i+1]], 'k')
+# for i in range(len(retrieval_numbers_aeolus_all_norm)-1):
+#     plt.plot([retrieval_numbers_aeolus_all_norm[i], retrieval_numbers_aeolus_all_norm[i+1]], [alt_aeolus_mean[i+1], alt_aeolus_mean[i+1]], 'k')
+# plt.plot([], [], 'k', label='Aeolus Profiles (%d)'%beta_aeolus_all.shape[0])
+# set x to log scale
+plt.xscale('log')
+# Set x, y-axis label
+plt.ylabel('Altitude (km)', fontsize=16)
+plt.xlabel('Backscatter coeff.\n[km$^{-1}$sr$^{-1}$]', fontsize=16)
+# Set title
+plt.title(f'Aerosol retrievals over the Sahara [backscatter] \n $14^{{th}}$ - $24^{{th}}$ June 2020', fontsize=18, y=1.05)
+
+# Set x-axis and y-axis ticks
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+
+plt.ylim([0.,20.])
+# Display legend
+plt.legend(loc='best', fontsize=14, frameon=False)
+
+# Save the figure
+output_path = input_path + f'retrieval_backscatter.png'
+plt.savefig(output_path, dpi=300)
+plt.close()
