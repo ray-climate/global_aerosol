bd9ed9cb4eba20196d703377b4b5cf3a55889bb7
ruisong <rui.song@ucl.ac.uk>
2023-03-20 11:22:17 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
@@ -23,7 +23,9 @@ for npz_file in os.listdir(input_path):
         print(npz_file)
         alt = np.load(input_path + npz_file, allow_pickle=True)['alt']
         beta = np.load(input_path + npz_file, allow_pickle=True)['beta']
-        beta_all = np.append(beta_all, beta)
-        print(beta.shape)
-beta_all = np.asarray(beta_all)
-print(beta_all.shape)
+        try:
+            beta_all = np.concatenate((beta_all, beta), axis=1)
+        except:
+            beta_all = np.copy(beta)
+
+        print(beta_all.shape)
