f7613716f22b342c6b0c6bffb0e0c139d232b263
ruisong <rui.song@ucl.ac.uk>
2023-03-20 13:17:21 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
@@ -43,6 +43,7 @@ for npz_file in os.listdir(input_path):
         except:
             alt_aeolus_all = np.copy(alt)
             beta_aeolus_all = np.copy(beta)
+print(alt_aeolus_all.shape)
 alt_aeolus_mean = np.nanmean(alt_aeolus_all, axis=0)
 alt_aeolus_mean = (alt_aeolus_mean[1:] + alt_aeolus_mean[:-1]) / 2.0
 
