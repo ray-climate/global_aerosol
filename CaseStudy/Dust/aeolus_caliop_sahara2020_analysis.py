43707037d92ed76ed80a87bd693bad5eac564747
ruisong <rui.song@ucl.ac.uk>
2023-03-20 11:25:30 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
@@ -18,7 +18,7 @@ input_sat = str(sys.argv[1]) # input satellite, either 'aeolus' or 'caliop'
 input_path = './aeolus_caliop_sahara2020_extraction_output/'
 beta_all = []
 for npz_file in os.listdir(input_path):
-    if npz_file.endswith('.npz') & (input_sat in npz_file):
+    if npz_file.endswith('.npz') & ('caliop_2020' in npz_file):
         # print the file name and variables in the file
         print(npz_file)
         alt = np.load(input_path + npz_file, allow_pickle=True)['alt']
@@ -28,4 +28,17 @@ for npz_file in os.listdir(input_path):
         except:
             beta_all = np.copy(beta)
 
-        print(beta_all.shape)
+beta_mask = np.zeros((beta_all.shape))
+beta_mask[beta_all > 0.0] = 1.0
+
+retrieval_numbers_all = np.sum(beta_mask, axis=1)
+
+# plot the retrieval numbers
+plt.figure()
+plt.plot(alt, retrieval_numbers_all, 'k')
+plt.xlabel('Altitude (km)')
+plt.ylabel('Retrieval numbers')
+plt.title('Retrieval numbers of ' + input_sat + ' over the Sahara in 2020')
+plt.savefig('./aeolus_caliop_sahara2020_analysis_output/retrieval_numbers_' + input_sat + '.png', dpi=300)
+plt.close()
+
