a4ac90b2b4bb7beb55fb4c66ec2b2ca19f719553
ruisong <rui.song@ucl.ac.uk>
2023-03-20 12:05:42 +0000

    get aeolus from netcdf format data

--- CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_analysis.py
@@ -28,8 +28,18 @@ for npz_file in os.listdir(input_path):
 beta_caliop_mask = np.zeros((beta_caliop_all.shape))
 beta_caliop_mask[beta_caliop_all > 0.0] = 1.0
 
-
-
+for npz_file in os.listdir(input_path):
+    if npz_file.endswith('.npz') & ('aeolus' in npz_file):
+        # print the file name and variables in the file
+        print(npz_file)
+        alt = np.load(input_path + npz_file, allow_pickle=True)['alt']
+        beta = np.load(input_path + npz_file, allow_pickle=True)['beta']
+        print(beta.shape)
+        # try:
+        #     beta_caliop_all = np.concatenate((beta_caliop_all, beta), axis=1)
+        # except:
+        #     beta_caliop_all = np.copy(beta)
+quit()
 
 
 
