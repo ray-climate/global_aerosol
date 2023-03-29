3823c9166a1a2e3e26e2e4acd45660a67512ffe5
ruisong <rui.song@ucl.ac.uk>
2023-03-29 16:38:47 +0100

    get aeolus from netcdf format data

--- CaseStudy/Dust/test.py
+++ CaseStudy/Dust/test.py
@@ -6,7 +6,7 @@
 # @Time:        06/03/2023 15:17
 
 import numpy as np
-
+#
 # Create a base temperature profile
 altitudes = np.linspace(0, 30, 301)  # Altitudes from 0 to 30 km, assuming 100m intervals
 base_profile = 15 + 10 * np.sin((np.pi * altitudes) / 30)  # This is just an example function for the base profile
