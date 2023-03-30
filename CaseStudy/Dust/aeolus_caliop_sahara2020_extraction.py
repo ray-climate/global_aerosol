b4956e14801525d7f892cf2cfd459f51d00440ff
ruisong <rui.song@ucl.ac.uk>
2023-03-30 17:32:02 +0100

    plot

--- CaseStudy/Dust/aeolus_caliop_sahara2020_extraction.py
+++ CaseStudy/Dust/aeolus_caliop_sahara2020_extraction.py
@@ -128,9 +128,11 @@ def read_caliop_data(caliop_file_path, lat_down, lat_up, lon_left, lon_right):
     caliop_longitude = np.asarray(caliop_request._get_longitude(caliop_file_path))
     caliop_altitude = np.asarray(caliop_request.get_altitudes(caliop_file_path))
     caliop_AOD_532_trop = np.asarray(caliop_request._get_aod(caliop_file_path, 'Column_Optical_Depth_Tropospheric_Aerosols_532'))
-    print(caliop_AOD_532_trop)
-    print(caliop_AOD_532_trop.shape)
-    print(np.max(caliop_AOD_532_trop))
+    caliop_AOD_532_stra = np.asarray(caliop_request._get_aod(caliop_file_path, 'Column_Optical_Depth_Stratospheric_Aerosols_532'))
+    caliop_AOD_532_total = caliop_AOD_532_trop + caliop_AOD_532_stra
+    print(caliop_AOD_532_total)
+    print(caliop_AOD_532_total.shape)
+    print(np.max(caliop_AOD_532_total))
     quit()
     caliop_beta = np.asarray(
         caliop_request._get_calipso_data(filename=caliop_file_path, variable='Total_Backscatter_Coefficient_532'))
