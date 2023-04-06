4bc70d44ac1cca600907545e50f54789949f780f
ruisong <rui.song@ucl.ac.uk>
2023-04-06 18:54:50 +0100

    plot

--- CaseStudy/Dust/June-17-plot.py
+++ CaseStudy/Dust/June-17-plot.py
@@ -35,7 +35,7 @@ for npz_file in os.listdir(input_path):
         beta_aeolus = np.load(input_path + npz_file, allow_pickle=True)['beta'][0:-1, :]
         alpha_aeolus = np.load(input_path + npz_file, allow_pickle=True)['alpha']
 
-
+dp_caliop[dp_caliop < 0.01] = np.nan
 conversion_factor = (np.nanmean(dp_caliop) * 0.82 * 2) / (1. - np.nanmean(dp_caliop) * 0.82)
 conversion_factor = 1 / (1. + conversion_factor)
 print(np.nanmean(dp_caliop))
