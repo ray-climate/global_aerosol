b299ae29484e7f58bf0bd3617f1b48bda3408309
ruisong <rui.song@ucl.ac.uk>
2023-04-06 18:42:53 +0100

    plot

--- CaseStudy/Dust/June-17-plot.py
+++ CaseStudy/Dust/June-17-plot.py
@@ -22,16 +22,33 @@ for npz_file in os.listdir(input_path):
     if npz_file.endswith('.npz') & ('caliop_dbd_ascending_202006171527' in npz_file):
 
         alt_caliop = np.load(input_path + npz_file, allow_pickle=True)['alt']
-        beta = np.load(input_path + npz_file, allow_pickle=True)['beta']
-        alpha = np.load(input_path + npz_file, allow_pickle=True)['alpha']
-        dp = np.load(input_path + npz_file, allow_pickle=True)['dp']
-        aod = np.load(input_path + npz_file, allow_pickle=True)['aod']
+        beta_caliop = np.load(input_path + npz_file, allow_pickle=True)['beta']
+        alpha_caliop = np.load(input_path + npz_file, allow_pickle=True)['alpha']
+        dp_caliop = np.load(input_path + npz_file, allow_pickle=True)['dp']
+        aod_caliop = np.load(input_path + npz_file, allow_pickle=True)['aod']
 
-beta[beta < 0] = np.nan
+for npz_file in os.listdir(input_path):
+    if npz_file.endswith('.npz') & ('ascending_202006171912' in npz_file):
+        # print the file name and variables in the file
+        print(npz_file)
+        alt_aeolus = np.load(input_path + npz_file, allow_pickle=True)['alt']
+        beta_aeolus = np.load(input_path + npz_file, allow_pickle=True)['beta']
+        alpha_aeolus = np.load(input_path + npz_file, allow_pickle=True)['alpha']
+
+beta_caliop[beta_caliop < 0] = np.nan
+
+alt_aeolus_mean = np.nanmean(alt_aeolus, axis=0)
+alt_aeolus_mean = (alt_aeolus_mean[1:] + alt_aeolus_mean[:-1]) / 2.0
+beta_aeolus[beta_aeolus<0] = np.nan
+
 plt.figure(figsize=(8, 12))
-for k in range(beta.shape[1]):
-    plt.plot(beta[:, k], alt_caliop, 'k', alpha=0.1)
+for k in range(beta_caliop.shape[1]):
+    plt.plot(beta_caliop[:, k], alt_caliop, 'k', alpha=0.1)
 plt.plot([], [], 'k', label='Caliop')
+
+for k in range(beta_aeolus.shape[0]):
+    plt.plot(beta_aeolus[k, :], alt_aeolus_mean, 'r', alpha=0.1)
+plt.plot([], [], 'k', label='Aeolus')
 
 plt.xscale('log')
 plt.ylabel('Altitude (km)', fontsize=16)
