08ac91bd7aabb4f8ebf89aef5c5c90ee7a05dd17
ruisong <rui.song@ucl.ac.uk>
2023-04-06 19:14:20 +0100

    plot

--- CaseStudy/Dust/June-18-plot.py
+++ CaseStudy/Dust/June-18-plot.py
@@ -40,8 +40,7 @@ dp_caliop[dp_caliop > 1] = np.nan
 k_factor = 0.82
 conversion_factor = (np.nanmean(dp_caliop) * k_factor * 2) / (1. - np.nanmean(dp_caliop) * k_factor)
 conversion_factor = 1 / (1. + conversion_factor)
-print(np.nanmean(dp_caliop))
-print(conversion_factor)
+
 beta_caliop[beta_caliop < 1.e-4] = np.nan
 
 alt_aeolus_mean = np.nanmean(alt_aeolus, axis=0)
@@ -85,5 +84,34 @@ plt.xlim([0.,1.])
 plt.legend(loc='best', fontsize=14, frameon=False)
 # Save the figure
 output_path = save_path + f'caliop_depolarisation.png'
+plt.savefig(output_path, dpi=300)
+plt.close()
+
+
+####
+
+# alpha_caliop[beta_caliop < 1.e-4] = np.nan
+# beta_aeolus[beta_aeolus< 1.e-4] = np.nan
+
+plt.figure(figsize=(8, 12))
+for k in range(beta_caliop.shape[1]):
+    plt.plot(alpha_caliop[:, k], alt_caliop, 'k', alpha=0.1)
+plt.plot([], [], 'k', label='Caliop')
+# plt.plot(np.nanmean(beta_caliop, axis=1), alt_caliop, 'k', label='Caliop')
+# for k in range(beta_aeolus.shape[0]):
+#     plt.plot(beta_aeolus[k, :], alt_aeolus_mean, 'r', alpha=0.5)
+# plt.plot([], [], 'k', label='Aeolus')
+# plt.plot(np.nanmean(beta_aeolus, axis=0) / conversion_factor, alt_aeolus_mean, 'r', label='Aeolus')
+
+plt.xscale('log')
+plt.ylabel('Altitude (km)', fontsize=16)
+plt.xlabel('Extinction coeff.\n[km$^{-1}$]', fontsize=16)
+plt.title(f'Aerosol retrievals over the Sahara [extinction] \n $14^{{th}}$ - $24^{{th}}$ June 2020', fontsize=18, y=1.05)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.ylim([0.,20.])
+plt.legend(loc='best', fontsize=14, frameon=False)
+# Save the figure
+output_path = save_path + f'caliop_extinction.png'
 plt.savefig(output_path, dpi=300)
 plt.close()
