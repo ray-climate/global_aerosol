8aa2268b6af84000fb7210ba7f91645e74b38cef
ruisong <rui.song@ucl.ac.uk>
2023-04-12 17:29:10 +0100

    plot

--- CaseStudy/Dust/crosssection_pair_3.py
+++ CaseStudy/Dust/crosssection_pair_3.py
@@ -80,10 +80,12 @@ for npz_file in os.listdir(input_path):
     if npz_file.endswith('.npz') & ('aeolus_descending_202006190812' in npz_file):
         # print the file name and variables in the file
         print(npz_file)
+        lat_aeolus = np.load(input_path + npz_file, allow_pickle=True)['lat']
         alt_aeolus = np.load(input_path + npz_file, allow_pickle=True)['alt']
         beta_aeolus = np.load(input_path + npz_file, allow_pickle=True)['beta'][0:-1, :]
         alpha_aeolus = np.load(input_path + npz_file, allow_pickle=True)['alpha'][0:-1, :]
-
+print(lat_aeolus.shape)
+quit()
 for k in range(beta_aeolus.shape[0]):
     max_index = np.nanargmax(beta_aeolus[k, :])
     alt_value = (alt_aeolus[k, max_index] + alt_aeolus[k, max_index+1]) / 2.
@@ -91,9 +93,11 @@ for k in range(beta_aeolus.shape[0]):
 
 # integrate the alpha_aeolus to get the aod_aeolus
 
+aeolus_aod = np.zeros(alpha_aeolus.shape[0])
 for k in range(alpha_aeolus.shape[0]):
-    print(alt_aeolus[k, :])
-quit()
+    for kk in range(alpha_aeolus.shape[1]):
+        if (alpha_aeolus[k, kk] > 0) & (alt_aeolus[k, kk] > 0) & (alt_aeolus[k, kk+1] > 0):
+            aeolus_aod[k] = aeolus_aod[k] + alpha_aeolus[k, kk] * (alt_aeolus[k, kk] - alt_aeolus[k, kk+1])
 
 dp_caliop[dp_caliop < 0] = np.nan
 dp_caliop[dp_caliop > 1] = np.nan
