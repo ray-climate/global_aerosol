f23cfb091799f9b694d16599937c4505f664f123
ruisong <rui.song@ucl.ac.uk>
2023-04-12 12:48:32 +0100

    plot

--- CaseStudy/Dust/crosssection_pair_1.py
+++ CaseStudy/Dust/crosssection_pair_1.py
@@ -114,9 +114,9 @@ alpha_caliop[beta_caliop < 1.e-4] = np.nan
 alpha_aeolus[alpha_aeolus< 1.e-4] = np.nan
 
 plt.figure(figsize=(8, 12))
-# for k in range(beta_caliop.shape[1]):
-#     plt.plot(alpha_caliop[:, k], alt_caliop, 'k', alpha=0.1)
-# plt.plot([], [], 'k', label='Caliop')
+for k in range(beta_caliop.shape[1]):
+    plt.plot(alpha_caliop[:, k], alt_caliop, 'k', alpha=0.1)
+plt.plot([], [], 'k', label='Caliop')
 plt.plot(np.nanmean(alpha_caliop, axis=1), alt_caliop, 'k', label='Caliop')
 # for k in range(beta_aeolus.shape[0]):
 #     plt.plot(alpha_aeolus[k, :], alt_aeolus_mean, 'r', alpha=0.5)
