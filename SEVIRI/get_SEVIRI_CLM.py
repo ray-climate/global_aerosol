1bc713ab63d5121a3b890179a17cba3d5e1abfb1
ruisong <rui.song@ucl.ac.uk>
2023-04-16 17:29:22 +0100

    plot

--- SEVIRI/get_SEVIRI_CLM.py
+++ SEVIRI/get_SEVIRI_CLM.py
@@ -99,8 +99,8 @@ def get_SEVIRI_HR_cartopy(file_path, extent, title, save_str,
 
         # Add the text box
         for j in range(len(aeolus_time)):
-            text_str = aeolus_time[j][int(len(aeolus_time[j]) / 2)].strftime("%H:%M")
-            text_x, text_y = aeolus_lon[j][int(len(aeolus_time[j]) / 2)], aeolus_lat[j][int(len(aeolus_time[j]) / 2)]
+            text_str = aeolus_time[int(len(aeolus_time) / 2)].strftime("%H:%M")
+            text_x, text_y = aeolus_lon[int(len(aeolus_time) / 2)], aeolus_lat[int(len(aeolus_time) / 2)]
             text_x = text_x + 1.  # shift the text a bit to the right
             text_angle = -78.
             text_box = ax.text(text_x, text_y, text_str, ha='center', va='center', color='white',
