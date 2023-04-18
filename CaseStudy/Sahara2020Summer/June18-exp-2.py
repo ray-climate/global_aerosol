9c4af74528b54e78e4cb38bf550fa3e1e12d1f37
ruisong <rui.song@ucl.ac.uk>
2023-04-18 14:35:43 +0100

    plot

--- CaseStudy/Sahara2020Summer/June18-exp-2.py
+++ CaseStudy/Sahara2020Summer/June18-exp-2.py
@@ -76,8 +76,7 @@ for k in range(len(lat_aeolus)):
         rows_to_keep_aeolus.append(k)
         print(lat_aeolus[k])
         print(alpha_aeolus_qc[k, :])
-        print(beta_aeolus_qc[k, :])
-quit()
+
 beta_aeolus_qc = beta_aeolus_qc[rows_to_keep_aeolus, :]
 alpha_aeolus_qc = alpha_aeolus_qc[rows_to_keep_aeolus, :]
 lat_aeolus = lat_aeolus[rows_to_keep_aeolus]
