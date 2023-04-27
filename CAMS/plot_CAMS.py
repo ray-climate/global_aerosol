886f27c1f9223a0a3a8df843b7fec5ffd19521f5
ruisong <rui.song@ucl.ac.uk>
2023-04-27 16:46:54 +0100

    plot

--- CAMS/plot_CAMS.py
+++ CAMS/plot_CAMS.py
@@ -43,7 +43,7 @@ for i in range(88):
     fig, ax = plt.subplots(subplot_kw={'projection': projection}, figsize=(16, 8))
     ax.add_feature(coastline)
     cs = ax.pcolormesh(lons, lats, aod[i], cmap='viridis', transform=projection, vmin=0, vmax=3.)
-    cbar = plt.colorbar(cs, label='Aerosol Optical Depth')
+    cbar = plt.colorbar(cs, label='Aerosol Optical Depth', shrink=0.5, extend='both')
     cbar.set_label('Aerosol Optical Depth', fontsize=14)  # Change the font size of the colorbar label
     cbar.ax.tick_params(labelsize=12)  # Change the font size of the colorbar tick labels
     plt.title(f'Band {i + 1} - Time: {times[i]}', fontsize=16)
@@ -51,7 +51,6 @@ for i in range(88):
     plt.ylabel('Latitude', fontsize=14)
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
-    ax.set_extent([longitudes.min(), longitudes.max(), latitudes.min(), latitudes.max()])
 
     # Save the plot as an image file
     image_file = os.path.join(tmp_dir, f'band_{i + 1}.png')
