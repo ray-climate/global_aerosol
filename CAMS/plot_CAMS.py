e5bcaabb923efacda3987f8a6c323589e2b36937
ruisong <rui.song@ucl.ac.uk>
2023-04-27 15:53:21 +0100

    plot

---
+++ CAMS/plot_CAMS.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    plot_CAMS.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        27/04/2023 15:48
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+from netCDF4 import Dataset
+
+# Read the NetCDF file
+filename = 'CAMS_550AOD_20200614_20200624.nc'
+nc_file = Dataset(filename, 'r')
+
+# Extract the variable to plot (assuming 'aod' is the variable name)
+aod = nc_file.variables['Total Aerosol Optical Depth at 550nm'][:]
+
+print(aod.shape)
