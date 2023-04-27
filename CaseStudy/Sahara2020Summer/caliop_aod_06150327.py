68074a80da1bb1ea1b42ae3108967032fe537618
ruisong <rui.song@ucl.ac.uk>
2023-04-27 12:27:16 +0100

    plot

---
+++ CaseStudy/Sahara2020Summer/caliop_aod_06150327.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    caliop_aod_06150327.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        27/04/2023 12:24
+
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import pathlib
+import sys
+import csv
+import os
+
+lat1_caliop = 12.
+lat2_caliop = 20.
+
+input_path = './aeolus_caliop_sahara2020_extraction_output/'
+# Define output directory
+script_name = os.path.splitext(os.path.abspath(__file__))[0]
+save_path = f'{script_name}_output/'
+pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
+
+for npz_file in os.listdir(input_path):
+    if npz_file.endswith('.npz') & ('caliop_dbd_ascending_202006191642' in npz_file):
+
+        lat_caliop = np.load(input_path + npz_file, allow_pickle=True)['lat']
+        alt_caliop = np.load(input_path + npz_file, allow_pickle=True)['alt']
+        beta_caliop = np.load(input_path + npz_file, allow_pickle=True)['beta']
+        alpha_caliop = np.load(input_path + npz_file, allow_pickle=True)['alpha']
+        dp_caliop = np.load(input_path + npz_file, allow_pickle=True)['dp']
+        aod_caliop = np.load(input_path + npz_file, allow_pickle=True)['aod']
+
+cols_to_keep_caliop = []
+for k in range(len(lat_caliop)):
+    if lat_caliop[k] > lat1_caliop and lat_caliop[k] < lat2_caliop:
+        cols_to_keep_caliop.append(k)
+
+beta_caliop = beta_caliop[:, cols_to_keep_caliop]
+alpha_caliop = alpha_caliop[:, cols_to_keep_caliop]
+lat_caliop = lat_caliop[cols_to_keep_caliop]
+dp_caliop = dp_caliop[:, cols_to_keep_caliop]
+aod_caliop = aod_caliop[:, cols_to_keep_caliop]
+
+print(aod_caliop)
