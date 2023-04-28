84b5bf702da5f1645f00ead95e55befe1eae56f5
ruisong <rui.song@ucl.ac.uk>
2023-04-28 10:57:07 +0100

    plot

--- CaseStudy/Sahara2020Summer/compare_reanalysis.py
+++ CaseStudy/Sahara2020Summer/compare_reanalysis.py
@@ -7,6 +7,7 @@
 
 from netCDF4 import Dataset, num2date
 import numpy as np
+import datetime
 import os
 
 # Read the NetCDF file
@@ -19,3 +20,16 @@ latitudes = nc_file.variables['latitude'][:]
 longitudes = nc_file.variables['longitude'][:]
 time_var = nc_file.variables['time']
 times = num2date(time_var[:], time_var.units)
+
+# Create a dictionary mapping the datetime objects to the corresponding AOD data
+time_aod_dict = {times[i]: aod[i] for i in range(88)}
+
+# Example: Access AOD data for a specific time
+time_to_find = datetime.datetime(2020, 6, 14, 12, 0)
+aod_data = time_aod_dict.get(time_to_find)
+
+if aod_data is not None:
+    print(f"AOD data found for time {time_to_find}:")
+    print(aod_data)
+else:
+    print(f"No AOD data found for time {time_to_find}")
