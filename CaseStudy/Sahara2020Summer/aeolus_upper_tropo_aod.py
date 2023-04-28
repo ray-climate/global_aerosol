2bac13fb9ee0ba2c9caceb324d9aa19982199e38
ruisong <rui.song@ucl.ac.uk>
2023-04-28 14:10:46 +0100

    plot

--- CaseStudy/Sahara2020Summer/aeolus_upper_tropo_aod.py
+++ CaseStudy/Sahara2020Summer/aeolus_upper_tropo_aod.py
@@ -31,6 +31,11 @@ input_path = './aeolus_caliop_sahara2020_extraction_output/'
 alt_threshold = 7.
 ext_threshold = 0.1
 
+lat1_aeolus = 0.
+lat2_aeolus = 40.
+lat1_caliop = 0.
+lat2_caliop = 40.
+
 aeolus_upper_trop_aod_all = []
 for npz_file in os.listdir(input_path):
     if npz_file.endswith('.npz') & ('aeolus_qc_descending' in npz_file):
@@ -55,6 +60,15 @@ for npz_file in os.listdir(input_path):
         alpha_aeolus_qc = np.where(valid_mask_extinction, alpha_aeolus, np.nan)
         beta_aeolus_qc = np.where(valid_mask_backscatter, beta_aeolus, np.nan)
 
+        rows_to_keep_aeolus = []
+        for k in range(len(lat_aeolus)):
+            if lat_aeolus[k] > lat1_aeolus and lat_aeolus[k] < lat2_aeolus:
+                rows_to_keep_aeolus.append(k)
+
+        beta_aeolus_qc = beta_aeolus_qc[rows_to_keep_aeolus, :]
+        alpha_aeolus_qc = alpha_aeolus_qc[rows_to_keep_aeolus, :]
+        lat_aeolus = lat_aeolus[rows_to_keep_aeolus]
+
         aeolus_aod = np.zeros(len(lat_aeolus))
 
         for k in range(alpha_aeolus_qc.shape[0]):
@@ -72,4 +86,33 @@ plt.xlim(0, 0.5)
 ax.tick_params(axis='both', which='major', labelsize=20)
 plt.savefig('./compare_reanalysis_output/aeolus_upper_trop_aod.png', dpi=300)
 
+caliop_upper_trop_aod_all = []
+for npz_file in os.listdir(input_path):
+    if npz_file.endswith('.npz') & ('caliop_dbd_descending_' in npz_file):
+        print('processing file: ' + npz_file + '...')
+        lat_caliop = np.load(input_path + npz_file, allow_pickle=True)['lat']
+        lon_caliop = np.load(input_path + npz_file, allow_pickle=True)['lon']
+        alt_caliop = np.load(input_path + npz_file, allow_pickle=True)['alt']
+        beta_caliop = np.load(input_path + npz_file, allow_pickle=True)['beta']
+        alpha_caliop = np.load(input_path + npz_file, allow_pickle=True)['alpha']
+        dp_caliop = np.load(input_path + npz_file, allow_pickle=True)['dp']
+        aod_caliop = np.load(input_path + npz_file, allow_pickle=True)['aod']
+
+    cols_to_keep_caliop = []
+    for k in range(len(lat_caliop)):
+        if lat_caliop[k] > lat1_caliop and lat_caliop[k] < lat2_caliop:
+            cols_to_keep_caliop.append(k)
+
+    beta_caliop = beta_caliop[:, cols_to_keep_caliop]
+    alpha_caliop = alpha_caliop[:, cols_to_keep_caliop]
+    lat_caliop = lat_caliop[cols_to_keep_caliop]
+    dp_caliop = dp_caliop[:, cols_to_keep_caliop]
+    aod_caliop = aod_caliop[cols_to_keep_caliop]
+
+    caliop_aod = np.zeros(len(lat_caliop))
+    print(alt_caliop)
+    for k in range(alpha_caliop.shape[0]):
+        for kk in range(alpha_caliop.shape[1]):
+            if (alpha_caliop[k, kk] > 0) & (alt_caliop[k, kk] > alt_threshold) & (alt_caliop[k, kk+1] > alt_threshold) & (alpha_caliop[k, kk] < ext_threshold):
+                caliop_aod[k] = caliop_aod[k] + alpha_caliop[k, kk] * (alt_caliop[k, kk] - alt_caliop[k, kk+1])
 
