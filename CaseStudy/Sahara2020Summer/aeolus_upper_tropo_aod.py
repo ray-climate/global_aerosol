6b691493f7f39be37f442c22301b983ee47f4571
ruisong <rui.song@ucl.ac.uk>
2023-04-28 14:05:44 +0100

    plot

--- CaseStudy/Sahara2020Summer/aeolus_upper_tropo_aod.py
+++ CaseStudy/Sahara2020Summer/aeolus_upper_tropo_aod.py
@@ -68,6 +68,7 @@ fig, ax = plt.subplots(figsize=(10, 10))
 plt.hist(aeolus_upper_trop_aod_all, bins=50, edgecolor='black')
 plt.xlabel('AEOLUS Upper Tropospheric AOD', fontsize=20)
 plt.ylabel('Frequency', fontsize=20)
+plt.xlim(0, 0.5)
 ax.tick_params(axis='both', which='major', labelsize=20)
 plt.savefig('./compare_reanalysis_output/aeolus_upper_trop_aod.png', dpi=300)
 
