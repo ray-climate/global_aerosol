e17a2f5e1f8af1d2b7e126d896b0439e4f885cf3
ruisong <rui.song@ucl.ac.uk>
2023-05-03 12:47:37 +0100

    plot

---
+++ CaseStudy/Sahara2020Summer/temporal_evolution_graph.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    temporal_evolution_graph.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        03/05/2023 12:20
+
+# this route generates the temporal evolution of dust layer using combined CALIOP and Aeolus data
+
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import sys
+import csv
+import os
+
+lat1 = 10.
+lat2 = 30.
+
+alt_1 = 5.
+alt_2 = 6.
+
+input_path = './aeolus_caliop_sahara2020_extraction_output/'
+
+caliop_layer_aod_all = []
+for npz_file in os.listdir(input_path):
+    if npz_file.endswith('.npz') & ('caliop_dbd_descending_' in npz_file):
+
+        print('processing file: ' + npz_file + '...')
+        lat_caliop = np.load(input_path + npz_file, allow_pickle=True)['lat']
+        lon_caliop = np.load(input_path + npz_file, allow_pickle=True)['lon']
+        alt_caliop = np.load(input_path + npz_file, allow_pickle=True)['alt']
+        beta_caliop = np.load(input_path + npz_file, allow_pickle=True)['beta']
+        alpha_caliop = np.load(input_path + npz_file, allow_pickle=True)['alpha']
+        dp_caliop = np.load(input_path + npz_file, allow_pickle=True)['dp']
+        aod_caliop = np.load(input_path + npz_file, allow_pickle=True)['aod']
+
+        cols_to_keep_caliop = []
+        for k in range(len(lat_caliop)):
+            if lat_caliop[k] > lat1 and lat_caliop[k] < lat2:
+                cols_to_keep_caliop.append(k)
+
+        beta_caliop = beta_caliop[:, cols_to_keep_caliop]
+        alpha_caliop = alpha_caliop[:, cols_to_keep_caliop]
+        lat_caliop = lat_caliop[cols_to_keep_caliop]
+        dp_caliop = dp_caliop[:, cols_to_keep_caliop]
+        aod_caliop = aod_caliop[cols_to_keep_caliop]
+
+        caliop_layer_aod = np.zeros(len(lat_caliop))
+
+        for k in range(alpha_caliop.shape[1]):
+            for kk in range(alpha_caliop.shape[0]-1):
+                if (alpha_caliop[kk, k] > 0) & (alt_caliop[kk] < alt_2) & (alt_caliop[kk+1] > alt_1):
+                    caliop_layer_aod[k] = caliop_layer_aod[k] + alpha_caliop[kk, k] * (alt_caliop[kk] - alt_caliop[kk+1])
+
+        caliop_layer_aod_all.append(caliop_layer_aod)
+
+print(caliop_layer_aod_all)
