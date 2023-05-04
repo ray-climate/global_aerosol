10d561323593def3a114afabf7e11b0180bc913e
ruisong <rui.song@ucl.ac.uk>
2023-05-04 14:40:29 +0100

    plot

--- CaseStudy/Sahara2020Summer/temporal_evolution_both.py
+++ CaseStudy/Sahara2020Summer/temporal_evolution_both.py
@@ -239,9 +239,7 @@ cmap = mcolors.ListedColormap(['red', 'blue'])
 # Create an additional horizontal plot for the data source array
 ax2 = fig.add_axes([0.15, 0.1, 0.7, 0.05])
 
-mesh2 = ax2.pcolormesh(resampled_timestamps, [0, 1], np.repeat(resampled_data_sources[np.newaxis, :], 2, axis=0), cmap=cmap, shading='auto', vmin=0, vmax=1)
-print(np.repeat(resampled_data_sources[np.newaxis, :], 2, axis=0))
-quit()
+mesh2 = ax2.pcolormesh(resampled_timestamps, [0, 1], np.repeat(resampled_data_sources[np.newaxis, :], 2, axis=0), cmap=cmap, vmin=0, vmax=1)
 ax2.set_yticks([])
 ax2.set_xticks(np.arange(0, len(resampled_timestamps), 6))
 ax2.set_xticklabels([t.strftime('%Y-%m-%d %H:%M') for t in np.array(resampled_timestamps)[::6]], rotation=45)
