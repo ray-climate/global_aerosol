f93bfeb1d37e395e864715903e4886ec628fb163
ruisong <rui.song@ucl.ac.uk>
2023-05-04 15:00:30 +0100

    plot

--- CaseStudy/Sahara2020Summer/temporal_evolution_both.py
+++ CaseStudy/Sahara2020Summer/temporal_evolution_both.py
@@ -201,9 +201,7 @@ for i in range(len(resampled_timestamps)):
 fig, ax = plt.subplots()
 
 # Create an additional horizontal plot for the data source array
-colorbar_width = 0.02  # Set the width of the colorbar
 colorbar_pad = 0.02  # Set the padding between ax1 and the colorbar
-ax1_width = 0.7  # Set the width of ax1
 
 # mesh = ax.pcolormesh(timestamps, lat_grid, aod_grid, cmap='jet', vmin=0., vmax=0.3)
 mesh = ax.pcolormesh(resampled_timestamps, lat_grid, smoothed_aod_data, cmap='jet', vmin=0., vmax=0.4)
@@ -216,7 +214,7 @@ ax.set_ylabel('Latitude', fontsize=14)
 ax.set_title('AOD layer [%s - %s km]' % (alt_1, alt_2), fontsize=16)
 ax.tick_params(axis='both', labelsize=12)
 ax.set_xticks([])
-cbar = fig.colorbar(mesh, ax=ax, orientation='vertical', pad=colorbar_pad, shrink=0.8, extend='both', width=colorbar_width)
+cbar = fig.colorbar(mesh, ax=ax, orientation='vertical', pad=colorbar_pad, shrink=0.8, extend='both')
 cbar.ax.tick_params(labelsize=12)
 cbar.set_label('AOD', fontsize=14)
 
