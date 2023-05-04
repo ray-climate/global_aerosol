6a2671549c83e705c09826a05d78f92c3649ea1f
ruisong <rui.song@ucl.ac.uk>
2023-05-04 12:35:50 +0100

    plot

--- CaseStudy/Sahara2020Summer/temporal_evoluation_both.py
+++ CaseStudy/Sahara2020Summer/temporal_evoluation_both.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @Filename:    temporal_evoluation_both.py
+# @Filename:    temporal_evolution_graph_caliop.py
 # @Author:      Dr. Rui Song
 # @Email:       rui.song@physics.ox.ac.uk
-# @Time:        04/05/2023 12:07
+# @Time:        03/05/2023 12:20
 
 import matplotlib.ticker as ticker
 import matplotlib.dates as mdates
@@ -23,7 +23,7 @@ lon2 = -20.
 alt_1 = 4.5
 alt_2 = 6.5
 
-CALIOP_input_path = '../Dust/aeolus_caliop_sahara2020_extraction_output/'
+CALIOP_input_path = './aeolus_caliop_sahara2020_extraction_output/'
 AEOLUS_input_path = '../Dust/aeolus_caliop_sahara2020_extraction_output/'
 
 caliop_layer_aod_all = []
@@ -32,141 +32,63 @@ aeolus_layer_aod_all = []
 aeolus_layer_lat_all = []
 
 # Sort the npz_file list based on date and time
-npz_files = sorted([f for f in os.listdir(CALIOP_input_path) if f.endswith('.npz') and 'caliop_dbd_' in f],
+npz_files = sorted([f for f in os.listdir(CALIOP_input_path) if f.endswith('.npz') and (('caliop_dbd_' in f) or ('aeolus_qc_' in f))],
                    key=lambda x: datetime.strptime(x[-16:-4], '%Y%m%d%H%M'))
 timestamps = [datetime.strptime(f[-16:-4], '%Y%m%d%H%M') for f in npz_files]
 
-mode = 'aeolus'
+print(npz_files)
+quit()
+
+for npz_file in npz_files:
+
+    print('processing file: ' + npz_file + '...')
+    lat_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['lat']
+    lon_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['lon']
+    alt_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['alt']
+    beta_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['beta']
+    alpha_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['alpha']
+    dp_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['dp']
+    aod_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['aod']
+
+    cols_to_keep_caliop = [k for k in range(len(lat_caliop)) if
+                           lat1 < lat_caliop[k] < lat2 and lon1 < lon_caliop[k] < lon2]
+
+    beta_caliop = beta_caliop[:, cols_to_keep_caliop]
+    alpha_caliop = alpha_caliop[:, cols_to_keep_caliop]
+    lat_caliop = lat_caliop[cols_to_keep_caliop]
+    dp_caliop = dp_caliop[:, cols_to_keep_caliop]
+    aod_caliop = aod_caliop[cols_to_keep_caliop]
+
+    caliop_layer_aod = np.zeros(len(lat_caliop))
+
+    for k in range(alpha_caliop.shape[1]):
+        for kk in range(alpha_caliop.shape[0] - 1):
+            if (alpha_caliop[kk, k] > 0) & (alt_caliop[kk] < alt_2) & (alt_caliop[kk + 1] > alt_1):
+                caliop_layer_aod[k] += alpha_caliop[kk, k] * (alt_caliop[kk] - alt_caliop[kk + 1])
+
+    caliop_layer_aod_all.append(caliop_layer_aod)
+    caliop_layer_lat_all.append(lat_caliop)
+
 ############ extract caliop ################
-if mode == 'caliop':
-    for npz_file in npz_files:
-
-        print('processing file: ' + npz_file + '...')
-        lat_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['lat']
-        lon_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['lon']
-        alt_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['alt']
-        beta_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['beta']
-        alpha_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['alpha']
-        dp_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['dp']
-        aod_caliop = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['aod']
-
-        cols_to_keep_caliop = [k for k in range(len(lat_caliop)) if
-                               lat1 < lat_caliop[k] < lat2 and lon1 < lon_caliop[k] < lon2]
-
-        beta_caliop = beta_caliop[:, cols_to_keep_caliop]
-        alpha_caliop = alpha_caliop[:, cols_to_keep_caliop]
-        lat_caliop = lat_caliop[cols_to_keep_caliop]
-        dp_caliop = dp_caliop[:, cols_to_keep_caliop]
-        aod_caliop = aod_caliop[cols_to_keep_caliop]
-
-        caliop_layer_aod = np.zeros(len(lat_caliop))
-
-        for k in range(alpha_caliop.shape[1]):
-            for kk in range(alpha_caliop.shape[0] - 1):
-                if (alpha_caliop[kk, k] > 0) & (alt_caliop[kk] < alt_2) & (alt_caliop[kk + 1] > alt_1):
-                    caliop_layer_aod[k] += alpha_caliop[kk, k] * (alt_caliop[kk] - alt_caliop[kk + 1])
-
-        caliop_layer_aod_all.append(caliop_layer_aod)
-        caliop_layer_lat_all.append(lat_caliop)
-
-############ extract caliop ################
-
-############ extract aeolus ################
-
-# Sort the npz_file list based on date and time
-npz_files = sorted([f for f in os.listdir(CALIOP_input_path) if f.endswith('.npz') and 'aeolus_qc_' in f],
-                   key=lambda x: datetime.strptime(x[-16:-4], '%Y%m%d%H%M'))
-timestamps = [datetime.strptime(f[-16:-4], '%Y%m%d%H%M') for f in npz_files]
-
-if True:
-
-    def qc_to_bits(qc_array):
-        # Convert the quality control array to uint8
-        qc_uint8 = qc_array.astype(np.uint8)
-
-        # Unpack the uint8 array to bits
-        qc_bits = np.unpackbits(qc_uint8, axis=1)
-
-        # Reshape the bits array to match the original shape
-        qc_bits = qc_bits.reshape(*qc_array.shape, -1)
-
-        return qc_bits
-
-    for npz_file in os.listdir(CALIOP_input_path):
-        if npz_file.endswith('.npz') & ('aeolus_qc_' in npz_file):
-
-            print('processing file: ' + npz_file + '...')
-            # print the file name and variables in the file
-            lat_aeolus = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['lat']
-            lon_aeolus = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['lon']
-            alt_aeolus = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['alt']
-            beta_aeolus = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['beta']
-            alpha_aeolus = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['alpha']
-            qc_aeolus = np.load(CALIOP_input_path + npz_file, allow_pickle=True)['qc']
-
-            # Convert the quality control data to 8 bits
-            qc_bits = qc_to_bits(qc_aeolus)
-
-            first_bit = qc_bits[:, :, -1]
-            second_bit = qc_bits[:, :, -2]
-            # Create a boolean mask where the second bit equals 1 (valid data)
-            valid_mask_extinction = first_bit == 1
-            valid_mask_backscatter = second_bit == 1
-            # set invalid data to nan
-            # alpha_aeolus_qc = np.where(valid_mask_extinction, alpha_aeolus, np.nan)
-            alpha_aeolus_qc = np.copy(alpha_aeolus)
-            beta_aeolus_qc = np.where(valid_mask_backscatter, beta_aeolus, np.nan)
-
-            rows_to_keep_aeolus = []
-            for k in range(len(lat_aeolus)):
-                if lat_aeolus[k] > lat1 and lat_aeolus[k] < lat2 and lon_aeolus[k] > lon1 and lon_aeolus[k] < lon2 :
-                    rows_to_keep_aeolus.append(k)
-
-            beta_aeolus_qc = beta_aeolus_qc[rows_to_keep_aeolus, :]
-            alpha_aeolus_qc = alpha_aeolus_qc[rows_to_keep_aeolus, :]
-            lat_aeolus = lat_aeolus[rows_to_keep_aeolus]
-
-            aeolus_aod = np.zeros(len(lat_aeolus))
-
-            for k in range(alpha_aeolus_qc.shape[0]):
-
-                alt_top = []
-                alt_bot = []
-                for kk in range(alpha_aeolus_qc.shape[1]):
-
-                    if (alt_aeolus[k, kk] < np.ceil(alt_2)) & (alt_aeolus[k, kk + 1] > np.floor(alt_1)):
-                        alt_top.append(alt_aeolus[k, kk])
-                        alt_bot.append(alt_aeolus[k, kk + 1])
-                        aeolus_aod[k] = aeolus_aod[k] + alpha_aeolus_qc[k, kk] * (alt_aeolus[k, kk] - alt_aeolus[k, kk + 1])
-                aeolus_aod[k] = aeolus_aod[k] / (alt_top[0] - alt_bot[-1]) * (alt_2 - alt_1)
-
-            aeolus_layer_aod_all.append(aeolus_aod)
-            aeolus_layer_lat_all.append(lat_aeolus)
-
-layer_lat_all = []
-layer_aod_all = []
-layer_aod_all = aeolus_layer_aod_all
-layer_lat_all = aeolus_layer_lat_all
 
 lat_grid = np.arange(lat1, lat2, 0.01)
 
 # Create a 2D grid for AOD values using the lat_grid
-aod_grid = np.zeros((len(lat_grid), len(layer_aod_all)))
+aod_grid = np.zeros((len(lat_grid), len(caliop_layer_aod_all)))
 aod_grid[:] = np.nan
 
-for k in range(len(layer_aod_all)):
-    if np.size(layer_aod_all[k]) > 0:
-        lat_centre = layer_lat_all[k]
-        print(timestamps[k], lat_centre)
-        for kk in range(len(lat_centre)):
-            aod_grid[(lat_grid > (lat_centre[kk] - 0.4)) & (lat_grid < (lat_centre[kk] + 0.4)), k] = layer_aod_all[k][kk]
-        print(aod_grid[:, k])
+for k in range(len(caliop_layer_aod_all)):
+    if np.size(caliop_layer_aod_all[k]) > 0:
+        lat_centre = (caliop_layer_lat_all[k][1:] + caliop_layer_lat_all[k][0:-1]) / 2.
+        for kk in range(len(lat_centre) - 2):
+            aod_grid[(lat_grid > min(lat_centre[kk], lat_centre[kk + 1])) & (
+                        lat_grid < max(lat_centre[kk], lat_centre[kk + 1])), k] = caliop_layer_aod_all[k][kk + 1]
 
 # Only keep columns with mean AOD larger than 0
 cols_to_keep = [k for k in range(aod_grid.shape[1]) if np.nanmean(aod_grid[:, k]) > 0]
 
 aod_grid = aod_grid[:, cols_to_keep]
-layer_aod_all = [layer_aod_all[k] for k in cols_to_keep]
+caliop_layer_aod_all = [caliop_layer_aod_all[k] for k in cols_to_keep]
 timestamps = [timestamps[k] for k in cols_to_keep]
 
 # Create the 2D pcolormesh plot
@@ -192,6 +114,5 @@ fig.autofmt_xdate()
 plt.xticks(fontsize=10, rotation=60)
 
 # Save the figure with an appropriate size
-plt.savefig('./figures/temporal_evolution_aod_aeolus.png', dpi=300, bbox_inches='tight')
-
+plt.savefig('./figures/temporal_evolution_caliop_aod.png', dpi=300, bbox_inches='tight')
 
