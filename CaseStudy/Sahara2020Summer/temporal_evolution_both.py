29bb68d4e715bf737873105f85985fc560c8ede8
ruisong <rui.song@ucl.ac.uk>
2023-05-04 13:27:18 +0100

    plot

--- CaseStudy/Sahara2020Summer/temporal_evolution_both.py
+++ CaseStudy/Sahara2020Summer/temporal_evolution_both.py
@@ -198,7 +198,7 @@ for i in range(len(resampled_timestamps)):
 fig, ax = plt.subplots()
 
 # mesh = ax.pcolormesh(timestamps, lat_grid, aod_grid, cmap='jet', vmin=0., vmax=0.3)
-mesh = ax.pcolormesh(resampled_timestamps, lat_grid, smoothed_aod_data, cmap='jet', vmin=0., vmax=0.3)
+mesh = ax.pcolormesh(resampled_timestamps, lat_grid, smoothed_aod_data, cmap='jet', vmin=0., vmax=0.4)
 
 # Adjust figure size, font size, label, and tick size
 fig.set_size_inches(12, 6)
