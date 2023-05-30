199ece52218b561b29f86dea95106e4b9b9c6cde
ruisong <rui.song@ucl.ac.uk>
2023-05-30 13:52:50 +0100

    plot

--- Caliop/SOVCC/caliop_extraction.py
+++ Caliop/SOVCC/caliop_extraction.py
@@ -9,6 +9,7 @@ import sys
 sys.path.append('../../')
 
 from Caliop.caliop import Caliop_hdf_reader
+import numpy as np
 import sys
 import os
 
@@ -36,20 +37,22 @@ for caliop_sub_folder in os.listdir(caliop_location + '/' + year):
             # extract ash layer only
             request = Caliop_hdf_reader()
 
-            (caliop_v4_aerosol_type, feature_type) = request._get_feature_classification(
-                filename=caliop_file,
-                variable='Atmospheric_Volume_Description')
-
+            (caliop_v4_aerosol_type, feature_type) = request._get_feature_classification(filename=caliop_file, variable='Atmospheric_Volume_Description')
+            extinction = request._get_calipso_data(filename=caliop_file, variable='Extinction_Coefficient_532')
             orbit_l2_altitude = request.get_altitudes(filename=caliop_file)
             orbit_l2_latitude = request._get_latitude(filename=caliop_file)
             orbit_l2_longitude = request._get_longitude(filename=caliop_file)
             orbit_l2_tropopause_height = request._get_tropopause_height(filename=caliop_file)
 
-            print(caliop_v4_aerosol_type, feature_type)
-            print(orbit_l2_altitude)
-            print(orbit_l2_latitude)
-            print(orbit_l2_longitude)
-            print(orbit_l2_tropopause_height)
+            save_sub_location = save_location + '/' + year + '/' + caliop_sub_folder
+            try:
+                os.stat(save_sub_location)
+            except:
+                os.mkdir(save_sub_location)
+
+            # save all the variables as a numpy file
+            save_file_name = save_sub_location + files[:-4] + '.npy'
+            np.save(save_file_name, [caliop_v4_aerosol_type, feature_type, extinction, orbit_l2_altitude, orbit_l2_latitude, orbit_l2_longitude, orbit_l2_tropopause_height])
             quit()
         else:
             pass
