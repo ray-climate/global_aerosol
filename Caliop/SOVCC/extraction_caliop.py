2758fef2204d1abe0afac684c430fbcd693d46ff
ruisong <rui.song@ucl.ac.uk>
2023-05-30 12:54:12 +0100

    plot

---
+++ Caliop/SOVCC/extraction_caliop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    extraction_caliop.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        30/05/2023 12:51
+
+import os
+
+# caliop location on CEDA
+caliop_location = '/gws/nopw/j04/eo_shared_data_vol1/satellite/calipso/APro5km'
+# location to save ash only data
+save_location = './caliop_ash_data_extraction/'
+
+# create save_location folder if not exist
+try:
+    os.stat(save_location)
+except:
+    os.mkdir(save_location)
+
+
+
