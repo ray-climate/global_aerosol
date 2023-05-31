90bfce8f5745ea0736aea3d4939dcf108c159333
ruisong <rui.song@ucl.ac.uk>
2023-05-31 10:33:26 +0100

    plot

---
+++ Caliop/SOVCC/thickness_extraction.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    thickness_extraction.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        31/05/2023 10:28
+
+import sys
+sys.path.append('../../')
+
+from Caliop.caliop import Caliop_hdf_reader
+import numpy as np
+import sys
+import os
+
+# set year from command line arugment
+year = sys.argv[1]
+
+# caliop location on CEDA
+caliop_extracted_location = './caliop_ash_data_extraction'
+
+# loop through all the sub year folder in caliop_location
+for caliop_sub_folder in os.listdir(caliop_extracted_location + '/' + year):
+
+    print('---------> Reading caliop file: %s' %caliop_sub_folder)
