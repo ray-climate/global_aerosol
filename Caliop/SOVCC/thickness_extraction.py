84bf2bed719f7d988d5978e24e433ad3805bf2dc
ruisong <rui.song@ucl.ac.uk>
2023-05-31 11:39:54 +0100

    plot

--- Caliop/SOVCC/thickness_extraction.py
+++ Caliop/SOVCC/thickness_extraction.py
@@ -86,7 +86,7 @@ for caliop_sub_folder in os.listdir(caliop_extracted_location + '/' + year):
 
             for i in range(ash_mask.shape[1]):
                 print(ash_mask[:, i].shape)
-                print(altitude[i].shape)
+                print(altitude.shape)
                 print(latitude[i])
                 print(longitude[i])
 
