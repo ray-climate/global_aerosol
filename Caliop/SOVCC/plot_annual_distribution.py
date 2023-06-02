9ae2291088609f3b9f04de01a77878a76840f47f
ruisong <rui.song@ucl.ac.uk>
2023-06-02 17:22:40 +0100

    plot

--- Caliop/SOVCC/plot_annual_distribution.py
+++ Caliop/SOVCC/plot_annual_distribution.py
@@ -56,8 +56,13 @@ for ax, file in zip(axs.flatten(), files):
 
     # Now we plot two histograms for the 'thickness' variable for this file,
     # one for ash_height < 20 and another for ash_height >= 20
-    ax.hist(below_20['thickness'].dropna(), bins=100, color='blue', alpha=0.7, label='ash_height < 20 km')
-    ax.hist(above_20['thickness'].dropna(), bins=100, color='red', alpha=0.7, label='ash_height >= 20 km')
+    ax.hist(below_20['thickness'].dropna(), bins=100, histtype='step', color='blue', alpha=0.7,
+            label='ash_height < 20 km')
+    ax.hist(above_20['thickness'].dropna(), bins=100, histtype='step', color='red', alpha=0.7,
+            label='ash_height >= 20 km')
+
+    # ax.hist(below_20['thickness'].dropna(), bins=100, color='blue', alpha=0.7, label='ash_height < 20 km')
+    # ax.hist(above_20['thickness'].dropna(), bins=100, color='red', alpha=0.7, label='ash_height >= 20 km')
     ax.set_title(f'Year - {file.split("_")[0]}', fontsize=15)
     ax.set_xlabel('Ash Layer Thickness', fontsize=15)
     ax.set_ylabel('Frequency', fontsize=15)
@@ -66,9 +71,9 @@ for ax, file in zip(axs.flatten(), files):
     ax.set_xlim(0, 5.)
     ax.set_ylim(0, 35000)
     # y axis in log scale
-    #ax.set_yscale('log')
+    ax.set_yscale('log')
     ax.legend(fontsize=12)
 
 plt.tight_layout()
-plt.savefig(figure_save_location + '/' + 'all_thickness.png')
+plt.savefig(figure_save_location + '/' + 'all_thickness_lines.png')
 
