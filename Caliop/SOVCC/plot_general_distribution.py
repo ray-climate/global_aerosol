b1dce6727a2008c1d04a8db4d7726e258d3adca0
ruisong <rui.song@ucl.ac.uk>
2023-06-02 12:56:56 +0100

    plot

---
+++ Caliop/SOVCC/plot_general_distribution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    plot_general_distribution.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        02/06/2023 12:54
+
+import pandas as pd
+import numpy as np
+import sys
+import os
+
+# variable file location
+variable_file_location = './thickness_data_extraction'
+
+for file in os.listdir(variable_file_location):
+
+    print(file)
