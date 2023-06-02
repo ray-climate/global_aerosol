bd144a618cebee2eb06acc1f56b56ed20719b6b4
ruisong <rui.song@ucl.ac.uk>
2023-06-02 17:06:46 +0100

    plot

--- Caliop/SOVCC/plot_annual_distribution.py
+++ Caliop/SOVCC/plot_annual_distribution.py
@@ -57,13 +57,16 @@ for ax, file in zip(axs.flatten(), files):
     # Now we plot two histograms for the 'thickness' variable for this file,
     # one for ash_height < 20 and another for ash_height >= 20
     ax.hist(below_20['thickness'].dropna(), bins=100, color='blue', alpha=0.7, label='ash_height < 20 km')
-    ax.hist(above_20['thickness'].dropna(), bins=100, color='red', alpha=0.7, label='ash_height >= km')
+    ax.hist(above_20['thickness'].dropna(), bins=100, color='red', alpha=0.7, label='ash_height >= 20 km')
     ax.set_title(f'Year - {file.split("_")[0]}', fontsize=15)
-    ax.set_xlabel('Thickness', fontsize=15)
+    ax.set_xlabel('Ash Layer Thickness', fontsize=15)
     ax.set_ylabel('Frequency', fontsize=15)
     ax.tick_params(axis='both', which='major', labelsize=12)
     ax.grid(True)
     ax.set_xlim(0, 5.)
+    ax.set_ylim(0, 35000)
+    # y axis in log scale
+    ax.set_yscale('log')
     ax.legend(fontsize=12)
 
 plt.tight_layout()
