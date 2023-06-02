679ad2febf49852a5250a6a32258a7c08a08f017
ruisong <rui.song@ucl.ac.uk>
2023-06-02 18:04:23 +0100

    plot

--- Caliop/SOVCC/plot_thickness_height_dependence.py
+++ Caliop/SOVCC/plot_thickness_height_dependence.py
@@ -5,8 +5,14 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        02/06/2023 17:42
 
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    plot_thickness_height_dependence.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        02/06/2023 17:42
+
 import matplotlib.pyplot as plt
-from matplotlib import colors
 import pandas as pd
 import numpy as np
 import sys
@@ -24,9 +30,8 @@ except:
 
 files = [file for file in os.listdir(variable_file_location) if file.endswith('.csv')]
 
-# Initiate empty lists to store all thickness and ash_height data
-all_thickness = []
-all_ash_height = []
+# Initiate empty DataFrame to store all data
+all_data = pd.DataFrame(columns=['thickness', 'ash_height'])
 
 for file in files:
 
@@ -50,29 +55,28 @@ for file in files:
         for i in range(modified.shape[1]):
             data[f"{column}_{i + 1}"] = pd.to_numeric(data[f"{column}_{i + 1}"], errors='coerce')
 
-    # Append thickness and ash_height data to the lists
-    all_thickness.append(data['thickness'])
-    all_ash_height.append(data['ash_height'])
+    # Append thickness and ash_height data to the DataFrame
+    all_data = all_data.append(data[['thickness', 'ash_height']], ignore_index=True)
 
-# Concatenate all data into one Series
-all_thickness = pd.concat(all_thickness)
-all_ash_height = pd.concat(all_ash_height)
+# Remove rows with any NaN values
+all_data = all_data.dropna()
 
-# Remove NaN values
-all_thickness = all_thickness.dropna()
-all_ash_height = all_ash_height.dropna()
+# Bin the 'ash_height' data into levels of 0.5 km from 8 to 30 km
+bins = np.arange(8, 30.5, 0.5)
+labels = bins[:-1] + 0.5/2  # Labels are the mid-point of each bin
+all_data['ash_height_bin'] = pd.cut(all_data['ash_height'], bins=bins, labels=labels)
 
-# Now we plot a 2D histogram (density map) for the 'thickness' and 'ash_height' variables
+# Group by 'ash_height_bin' and calculate the mean 'thickness' for each group
+grouped = all_data.groupby('ash_height_bin').mean()
+
+# Now we plot the relation between mean thickness and ash_height
 plt.figure(figsize=(10, 10))
-plt.hist2d(all_thickness, all_ash_height, bins=[300, 300], cmap='RdYlGn_r', norm = colors.LogNorm())
+plt.plot(grouped['thickness'], grouped.index, marker='o')
 
-plt.colorbar(label='Density')
-plt.title('Ash Layer Thickness - Layer Mean Height', fontsize=20)
-plt.xlabel('Thickness', fontsize=15)
-plt.ylabel('Ash Height', fontsize=15)
-plt.tick_params(axis='both', which='major', labelsize=12)
+plt.title('Mean Thickness vs. Ash Height', fontsize=20)
+plt.xlabel('Mean Thickness', fontsize=15)
+plt.ylabel('Ash Height (km)', fontsize=15)
 plt.grid(True)
-plt.xlim(0, 6.)
-plt.ylim(7, 30.)
 
-plt.savefig(figure_save_location + '/' + 'density_map_thickness_ashheight.png')
+plt.savefig(figure_save_location + '/' + 'mean_thickness_vs_ash_height.png')
+
