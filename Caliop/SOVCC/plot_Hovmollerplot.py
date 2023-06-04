e03eaf363556c583f3afec0546361c25b86522de
ruisong <rui.song@ucl.ac.uk>
2023-06-04 19:29:29 +0100

    plot

--- Caliop/SOVCC/plot_Hovmollerplot.py
+++ Caliop/SOVCC/plot_Hovmollerplot.py
@@ -32,13 +32,12 @@ for file in files:
     for column in ['utc_time', 'thickness', 'latitude']:
         if column == 'utc_time':
             # Convert utc_time to datetime format
-            data[column] = pd.to_datetime(data[column])
+            data[column] = pd.to_datetime(data[column], format='%Y-%m-%dT%H-%M-%S')
         else:
             data[column] = pd.to_numeric(data[column], errors='coerce')
 
     all_data = all_data.append(data[['utc_time', 'thickness', 'latitude']],
                                ignore_index=True)
-
 
 # Remove rows with any NaN values
 all_data = all_data.dropna()
@@ -47,33 +46,21 @@ all_data = all_data.dropna()
 lat_bins = np.arange(-90, 91, 1)
 
 # Bin the latitude data
-all_data['latitude_bin'] = pd.cut(all_data['latitude'], bins=lat_bins)
+all_data['latitude_bin'] = pd.cut(all_data['latitude'], bins=lat_bins, labels=(lat_bins[:-1] + 1/2))
 
-# Convert utc_time to datetime format
-all_data['utc_time'] = pd.to_datetime(all_data['utc_time'])
-# Set utc_time as the index
-all_data.set_index('utc_time', inplace=True)
+# Group the utc_time to every 5 days
+all_data['utc_time_bin'] = all_data['utc_time'].dt.floor('5D')
 
-# Convert utc_time to date only
-all_data['date'] = all_data.index.date
+# Now we can group by 'utc_time_bin' and 'latitude_bin' and calculate the mean
+grouped_data = all_data.groupby(['utc_time_bin', 'latitude_bin']).mean().reset_index()
 
-# Create a date range with 5-day frequency
-date_range = pd.date_range(start=all_data.date.min(), end=all_data.date.max(), freq='5D')
-
-# Map each date in the data to the closest date in the date range
-all_data['date_bin'] = all_data['date'].map(lambda x: date_range[date_range.get_loc(x, method='nearest')])
-
-# Now we can group by 'date_bin' and 'latitude_bin' and calculate the mean
-grouped_data = all_data.groupby(['date_bin', 'latitude_bin']).mean().reset_index()
-
-# Pivot the data so that date_bin and latitude_bin are the index and columns
-pivoted_data = grouped_data.pivot(index='date_bin', columns='latitude_bin', values='thickness')
+# Pivot the data so that utc_time_bin and latitude_bin are the index and columns
+pivoted_data = grouped_data.pivot(index='utc_time_bin', columns='latitude_bin', values='thickness')
 
 fig, ax = plt.subplots(figsize=(14, 10))
 
 # Plot the pivoted data
-c = ax.imshow(pivoted_data, aspect='auto', cmap='rainbow', origin='lower',
-              extent=[-90, 90, 0, all_data.index.max()], vmin=0, vmax=4.)
+c = ax.pcolormesh(pivoted_data.columns, pivoted_data.index, pivoted_data.values, cmap='rainbow')
 
 ax.set_xlabel('Latitude', fontsize=18)
 ax.set_ylabel('Time', fontsize=18)
