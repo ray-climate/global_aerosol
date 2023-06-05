2833f78eba8e0e73572a33c6f5f66621956a1e89
ruisong <rui.song@ucl.ac.uk>
2023-06-06 00:26:52 +0100

    plot

--- Caliop/SOVCC/eruption_evolution.py
+++ Caliop/SOVCC/eruption_evolution.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
+import matplotlib.dates as mdates
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
 
