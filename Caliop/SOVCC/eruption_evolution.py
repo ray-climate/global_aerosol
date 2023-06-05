794a115214d953128fe465d589941b470bfae60a
ruisong <rui.song@ucl.ac.uk>
2023-06-05 23:41:21 +0100

    plot

--- Caliop/SOVCC/eruption_evolution.py
+++ Caliop/SOVCC/eruption_evolution.py
@@ -5,11 +5,12 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        05/06/2023 17:11
 
-# -*- coding:utf-8 -*-
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
 
 # variable file location
 variable_file_location = './thickness_data_extraction'
@@ -23,9 +24,7 @@ lat_top = 0
 lat_bottom = -80
 
 # create save_location folder if not exist
-try:
-    os.stat(figure_save_location)
-except:
+if not os.path.exists(figure_save_location):
     os.mkdir(figure_save_location)
 
 files = [file for file in os.listdir(variable_file_location) if file.endswith('.csv')]
@@ -40,12 +39,11 @@ for file in files:
     for column in ['utc_time', 'thickness', 'latitude', 'ash_height']:
         if column == 'utc_time':
             # Convert utc_time to datetime format
-            data[column] = pd.to_datetime(data[column], format='%Y-%m-%dT%H-%M-%S')
+            data[column] = pd.to_datetime(data[column], format='%Y-%m-%dT%H:%M:%S')
         else:
             data[column] = pd.to_numeric(data[column], errors='coerce')
 
-    all_data = all_data.append(data[['utc_time', 'thickness', 'latitude', 'ash_height']],
-                               ignore_index=True)
+    all_data = all_data.append(data[['utc_time', 'thickness', 'latitude', 'ash_height']], ignore_index=True)
 
 # Remove rows with any NaN values
 all_data = all_data.dropna()
@@ -54,23 +52,17 @@ all_data = all_data.dropna()
 all_data = all_data[(all_data['utc_time'] >= start_time) & (all_data['utc_time'] <= end_time) &
                     (all_data['latitude'] >= lat_bottom) & (all_data['latitude'] <= lat_top)]
 
-# Iterate over the rows to check for latitude criterion
-for i, row in all_data.iterrows():
-    nearby_records = all_data[(np.abs(all_data['latitude'] - row['latitude']) <= 1) &
-                              (all_data['utc_time'] == row['utc_time'])]
-    if nearby_records.shape[0] < 5:
-        all_data.drop(i, inplace=True)
-    if i % 1000 == 0:  # Print progress for every 1000 rows
-        print(f"Processed {i} rows")
+# Group the data by each day and calculate the mean and standard deviation of thickness
+grouped_data = all_data.groupby(pd.Grouper(key='utc_time', freq='D'))['thickness'].agg(['mean', 'std', 'count'])
 
-# Average the thickness for same utc_time
-all_data = all_data.groupby('utc_time').mean().reset_index()
+# Set up colormap
+cmap = plt.get_cmap("viridis")
+norm = Normalize(vmin=grouped_data['count'].min(), vmax=grouped_data['count'].max())
 
-grouped_data = all_data.groupby(pd.Grouper(key='utc_time', freq='D'))['thickness'].agg(['mean', 'std'])
-
-plt.figure(figsize=(10, 6))  # Set the plot size
-plt.plot(all_data['utc_time'], all_data['thickness'], 'ro')
-plt.errorbar(grouped_data.index, grouped_data['mean'], yerr=grouped_data['std'], fmt='o')
+fig, ax = plt.subplots(figsize=(10, 6))  # Set the plot size
+sc = ax.scatter(grouped_data.index, grouped_data['mean'], c=grouped_data['count'], cmap=cmap, norm=norm)
+plt.errorbar(grouped_data.index, grouped_data['mean'], yerr=grouped_data['std'], fmt='o', color='black')
+plt.colorbar(ScalarMappable(norm=norm, cmap=cmap), ax=ax, label='Count')
 plt.xlabel('Time', fontsize=18)
 plt.ylabel('Average Thickness', fontsize=18)
 plt.grid(True)
@@ -79,17 +71,6 @@ plt.xticks(rotation=45)  # Rotate x-axis labels for better visibility
 plt.ylim(0, 5.)
 plt.tight_layout()  # Adjust subplot parameters to give specified padding
 plt.savefig(figure_save_location + '/' + name + '_average_thickness_vs_time.png')
-
-# Ash Height Plot
-plt.figure(figsize=(10, 6))  # Set the plot size
-plt.plot(all_data['utc_time'], all_data['ash_height'], marker='o')
-plt.xlabel('Time', fontsize=18)
-plt.ylabel('Average Ash Height', fontsize=18)
-plt.grid(True)
-plt.title('Average Ash Height Over Time', fontsize=20)
-plt.xticks(rotation=45)  # Rotate x-axis labels for better visibility
-plt.tight_layout()  # Adjust subplot parameters to give specified padding
-plt.savefig(figure_save_location + '/' + name + '_average_ash_height_vs_time.png')
 
 
 
