8482ac411faf1f3c62af50fa1e497f351faa6ab8
ruisong <rui.song@ucl.ac.uk>
2023-06-06 16:12:07 +0100

    plot

--- Caliop/SOVCC/box_caulle.py
+++ Caliop/SOVCC/box_caulle.py
@@ -18,7 +18,7 @@ import numpy as np
 import os
 
 # variable file location
-variable_file_location = './thickness_data_extraction'
+variable_file_location = './thickness_data_extraction_extinction'
 figure_save_location = './figures'
 
 # Define time and latitude range
@@ -118,14 +118,14 @@ ax[1].set_xticks(positions[::5])  # add this
 ax[1].set_xticklabels(x_labels[::5])  # add this
 ax[1].set_xlabel('Days Since T0 (' + formatted_start_time + ')', fontsize=18)
 
-# ax[2].boxplot([data['extinction'] for data in box_plot_data.values()], positions=positions, widths=0.6)  # add this
-# ax[2].set_ylabel('Extinction [km$^{-1}$]', fontsize=18)  # you might want to adjust this label
-# ax[2].tick_params(axis='both', labelsize=18)
-# ax[2].set_ylim(0, 6)  # Set the appropriate y limits for your extinction data
-# ax[2].set_title(f"{name}", fontsize=20)
-# ax[2].set_xticks(positions[::5])  # add this
-# ax[2].set_xticklabels(x_labels[::5])  # add this
-# ax[2].set_xlabel('Days Since T0 (' + formatted_start_time + ')', fontsize=18)
+ax[2].boxplot([data['extinction'] for data in box_plot_data.values()], positions=positions, widths=0.6)  # add this
+ax[2].set_ylabel('Extinction [km$^{-1}$]', fontsize=18)  # you might want to adjust this label
+ax[2].tick_params(axis='both', labelsize=18)
+ax[2].set_ylim(0, 6)  # Set the appropriate y limits for your extinction data
+ax[2].set_title(f"{name}", fontsize=20)
+ax[2].set_xticks(positions[::5])  # add this
+ax[2].set_xticklabels(x_labels[::5])  # add this
+ax[2].set_xlabel('Days Since T0 (' + formatted_start_time + ')', fontsize=18)
 
 plt.savefig(figure_save_location + '/' + name + '_box.png')
 
