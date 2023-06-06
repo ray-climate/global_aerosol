b4773e13a4c67a208b4f1ffb5604b9e7f005ee66
ruisong <rui.song@ucl.ac.uk>
2023-06-06 11:15:27 +0100

    plot

--- Caliop/SOVCC/plot_time_caulle.py
+++ Caliop/SOVCC/plot_time_caulle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @Filename:    eruption_evolution.py
+# @Filename:    plot_time_caulle.py
 # @Author:      Dr. Rui Song
 # @Email:       rui.song@physics.ox.ac.uk
-# @Time:        05/06/2023 17:11
+# @Time:        06/06/2023 11:12
 
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from matplotlib.cm import ScalarMappable
@@ -16,6 +16,7 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import os
+
 # variable file location
 variable_file_location = './thickness_data_extraction'
 figure_save_location = './figures'
@@ -23,7 +24,7 @@ figure_save_location = './figures'
 # Define time and latitude range
 name = 'Puyehue-Cordón Caulle'
 start_time = '2011-06-15'
-end_time = '2011-07-25'
+end_time = '2011-06-25'
 lat_top = 0
 lat_bottom = -80
 
@@ -69,58 +70,54 @@ for i, row in all_data.iterrows():
         print(f"Processed {i} rows")
 
 # Group the data by each utc_time and calculate the mean and count of thickness
-grouped_data_utc = all_data.groupby('utc_time').agg({'thickness': 'mean', 'count': 'first'})
+grouped_data_utc = all_data.groupby('utc_time').agg({'thickness': 'mean', 'ash_height': 'mean', 'count': 'first'})
 
 start_date = pd.to_datetime(start_time)
-grouped_data_day = all_data.groupby(pd.Grouper(key='utc_time', freq='D')).agg({'thickness': ['mean', 'std']})
-grouped_data_day.columns = ['thickness_mean', 'thickness_std']
+grouped_data_day = all_data.groupby(pd.Grouper(key='utc_time', freq='D')).agg({'thickness': ['mean', 'std'], 'ash_height': ['mean', 'std']})
+grouped_data_day.columns = ['thickness_mean', 'thickness_std', 'ash_height_mean', 'ash_height_std']
 
 grouped_data_day_days = (grouped_data_day.index - start_date).days  # new Series representing days since start
 grouped_data_utc_days = (grouped_data_utc.index - start_date).total_seconds() / (24 * 60 * 60)
+
 
 # Set up colormap
 cmap = plt.get_cmap("jet")
 norm = Normalize(vmin=grouped_data_utc['count'].min(), vmax=grouped_data_utc['count'].max())
 
-fig, ax1 = plt.subplots(figsize=(8, 8))  # Set the plot size
+fig, ax = plt.subplots(2, 1, figsize=(8, 16))  # Set the plot size and create 2 subplots
 
-# Scatter plot with time-based x-axis
+# First subplot for thickness
+sc = ax[0].scatter(grouped_data_utc_days, grouped_data_utc['thickness'], c=grouped_data_utc['count'], cmap=cmap, norm=norm, alpha=0.35, s=5*grouped_data_utc['count'])
+ax[0].set_ylabel('Ash layer thickness [km]', fontsize=18)
+ax[0].set_ylim(0, 4)  # set ylim correctly
+ax[0].grid(True)
+ax[0].set_title(f"{name}", fontsize=20)
 
-sc = ax1.scatter(grouped_data_utc_days, grouped_data_utc['thickness'], c=grouped_data_utc['count'], cmap=cmap, norm=norm, alpha=0.35, s=5*grouped_data_utc['count'])
-ax1.set_ylabel('Ash layer thickness [km]', fontsize=18)
-ax1.set_ylim(0, 4)  # set ylim correctly
-ax1.grid(True)
-
-# create an inset_axes instance with a width of "5%" and a height of "50%"
-axins = inset_axes(ax1,
-                   width="50%",  # width = 5% of parent_bbox width
-                   height="5%",  # height : 50%
-                   loc='upper left',
-                   bbox_to_anchor=(0.05, 0.55, 0.4, 0.4),
-                   bbox_transform=ax1.transAxes,
-                   borderpad=0
-                   )
-ax1.set_xlim(0, 50)
-plt.colorbar(sc, cax=axins, orientation='horizontal', label='Counts')
-
-ax1.tick_params(axis='both', which='major', labelsize=18)
-ax1.set_xticklabels([])  # Hide ax1 xticklabels
-ax1.set_title(f"{name}", fontsize=20)
-ax2 = ax1.twiny()  # Create a twin x-axis sharing the y-axis
+ax2 = ax[0].twiny()  # Create a twin x-axis sharing the y-axis
 ax2.xaxis.tick_bottom()  # Move ax2 xticks to bottom
 ax2.xaxis.set_label_position('bottom')  # Move ax2 xlabel to bottom
 
 # Error bar plot with day-based x-axis
 ax2.errorbar(grouped_data_day_days, grouped_data_day['thickness_mean'], yerr=grouped_data_day['thickness_std'], fmt='x', color='black', markeredgecolor='black', capsize=3, elinewidth=2.4)
-start_time_dt = datetime.strptime(start_time, '%Y-%m-%d')
-formatted_start_time = start_time_dt.strftime('%d/%m/%Y')
-ax2.xaxis.set_major_locator(mticker.MaxNLocator(integer=True))  # Ensure the ticks are integers
-ax2.set_xlabel('Days Since T0 (' + formatted_start_time + ')', fontsize=18)
-ax2.tick_params(axis='both', which='major', labelsize=18)
-ax2.set_xlim(0, 50)
-fig.tight_layout()
 
-plt.savefig(figure_save_location + '/' + name + '_thickness_for_each_utc_time.png')
+# Second subplot for ash_height
+sc = ax[1].scatter(grouped_data_utc_days, grouped_data_utc['ash_height'], c=grouped_data_utc['count'], cmap=cmap, norm=norm, alpha=0.35, s=5*grouped_data_utc['count'])
+ax[1].set_ylabel('Ash height [km]', fontsize=18)
+ax[1].set_ylim(0, 4)  # set ylim correctly
+ax[1].grid(True)
+
+ax2 = ax[1].twiny()  # Create a twin x-axis sharing the y-axis
+ax2.xaxis.tick_bottom()  # Move ax2 xticks to bottom
+ax2.xaxis.set_label_position('bottom')  # Move ax2 xlabel to bottom
+
+# Error bar plot with day-based x-axis
+ax2.errorbar(grouped_data_day_days, grouped_data_day['ash_height_mean'], yerr=grouped_data_day['ash_height_std'], fmt='x', color='black', markeredgecolor='black', capsize=3, elinewidth=2.4)
+
+# ...
+
+plt.savefig(figure_save_location + '/' + name + '_thickness_and_ash_height_for_each_utc_time.png')
+
+
 
 
 
