c5622fa9d87abe2fae6d38ffe63abc2c0a85a3ac
ruisong <rui.song@ucl.ac.uk>
2023-06-06 14:39:09 +0100

    plot

--- Caliop/SOVCC/box_caulle.py
+++ Caliop/SOVCC/box_caulle.py
@@ -24,7 +24,7 @@ figure_save_location = './figures'
 # Define time and latitude range
 name = 'Puyehue-Cordón Caulle'
 start_time = '2011-06-15'
-end_time = '2011-06-20'
+end_time = '2011-08-20'
 lat_top = 0
 lat_bottom = -80
 
