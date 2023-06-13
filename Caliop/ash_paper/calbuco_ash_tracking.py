2ad49831fe99d08eddbfa0051bbe03a0e6e4c456
ruisong <rui.song@ucl.ac.uk>
2023-06-13 16:39:38 +0100

    plot

--- Caliop/ash_paper/calbuco_ash_tracking.py
+++ Caliop/ash_paper/calbuco_ash_tracking.py
@@ -18,7 +18,7 @@ import numpy as np
 import os
 
 # variable file location
-variable_file_location = './SOVCC/thickness_data_extraction_extinction'
+variable_file_location = '../SOVCC/thickness_data_extraction_extinction'
 figure_save_location = './figures'
 
 # Define time and latitude range
