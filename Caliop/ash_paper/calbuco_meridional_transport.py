a300b8368eecc0be4059cecd4e6e2c15591165ad
ruisong <rui.song@ucl.ac.uk>
2023-06-14 00:01:16 +0100

    plot

--- Caliop/ash_paper/calbuco_meridional_transport.py
+++ Caliop/ash_paper/calbuco_meridional_transport.py
@@ -103,21 +103,27 @@ plt.show()
 
 # Create a colormap
 cmap = plt.cm.get_cmap('viridis')  # use 'viridis' colormap, change as needed
-norm = Normalize(vmin=grouped_data['utc_time'].dt.date.min(), vmax=grouped_data['utc_time'].dt.date.max())
+
+# Convert the dates into integers (number of days from the earliest date)
+min_date = grouped_data['utc_time'].dt.date.min()
+grouped_data['date_num'] = (grouped_data['utc_time'].dt.date - min_date).dt.days
+
+# Normalize the date_num for color mapping
+norm = Normalize(vmin=grouped_data['date_num'].min(), vmax=grouped_data['date_num'].max())
 
 # Create a new figure
 fig, ax = plt.subplots(figsize=(10,6))
 
 # Group by each day, sort by longitude and plot ash_height over longitude
-for name, group in grouped_data.groupby(grouped_data['utc_time'].dt.date):
+for name, group in grouped_data.groupby('utc_time'):
     group = group.sort_values('longitude')
-    ax.plot(group['longitude'], group['ash_height'], marker='o', linestyle='-', color=cmap(norm(name)))
+    ax.plot(group['longitude'], group['ash_height'], marker='o', linestyle='-', color=cmap(norm(group['date_num'].mean())))
 
 # Set title, x and y labels
 ax.set_title('Ash height over longitude')
 ax.set_xlabel('Longitude')
 ax.set_ylabel('Ash height')
-ax.set_xlim(-80, 30)
+
 # Add colorbar
 sm = ScalarMappable(cmap=cmap, norm=norm)
 sm.set_array([])
@@ -128,5 +134,9 @@ plt.xticks(rotation=45)
 
 # Save figure
 plt.savefig(figure_save_location + '/' + 'ash_height_over_longitude.png', dpi=300)
+
+# Show the plot
+plt.show()
+
 
 
