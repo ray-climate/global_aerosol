5428c63c6d77f7a988f594345098c4af5cef9f93
ruisong <rui.song@ucl.ac.uk>
2023-06-15 15:13:54 +0100

    plot

---
+++ Caliop/SOVCC/caliop_extraction_v2.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    caliop_extraction_v2.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        15/06/2023 14:55
+
+import sys
+sys.path.append('../../')
+
+from Caliop.caliop import Caliop_hdf_reader
+import numpy as np
+import sys
+import os
+
+# set year from command line arugment
+year = sys.argv[1]
+
+# caliop location on CEDA
+caliop_location = '/gws/nopw/j04/eo_shared_data_vol1/satellite/calipso/APro5km'
+# location to save ash only data
+save_location = './caliop_ash_data_extraction_extended_v1'
+
+# create save_location folder if not exist
+try:
+    os.stat(save_location)
+except:
+    os.mkdir(save_location)
+
+# loop through all the sub year folder in caliop_location
+for caliop_sub_folder in os.listdir(caliop_location + '/' + year):
+
+    try:
+        os.stat(save_location + '/' + year)
+    except:
+        os.mkdir(save_location + '/' + year)
+
+    for files in os.listdir(caliop_location + '/' + year + '/' + caliop_sub_folder):
+        if files.endswith('.hdf'):
+            print('---------> Reading caliop file: %s' %files)
+            # read caliop hdf file
+            caliop_file = caliop_location + '/' + year + '/' + caliop_sub_folder + '/' + files
+            # extract ash layer only
+            request = Caliop_hdf_reader()
+            #
+            (caliop_v4_aerosol_type, feature_type) = request._get_feature_classification(filename=caliop_file, variable='Atmospheric_Volume_Description')
+            extinction = request._get_calipso_data(filename=caliop_file, variable='Extinction_Coefficient_532')
+            backscatter = request._get_calipso_data(filename=caliop_file, variable='Total_Backscatter_Coefficient_532')
+            depolarisation = request._get_calipso_data(filename=caliop_file, variable='Particulate_Depolarization_Ratio_Profile_532')
+
+            orbit_l2_altitude = request.get_altitudes(filename=caliop_file)
+            orbit_l2_latitude = request._get_latitude(filename=caliop_file)
+            orbit_l2_longitude = request._get_longitude(filename=caliop_file)
+            orbit_l2_tropopause_height = request._get_tropopause_height(filename=caliop_file)
+            #
+            save_sub_location = save_location + '/' + year + '/' + caliop_sub_folder + '/'
+            try:
+                os.stat(save_sub_location)
+            except:
+                os.mkdir(save_sub_location)
+
+            # save all the variables as a numpy file
+            save_file_name = save_sub_location + files[:-4]
+            np.savez(save_file_name,
+                     caliop_v4_aerosol_type=caliop_v4_aerosol_type,
+                     feature_type=feature_type,
+                     extinction=extinction,
+                     backscatter=backscatter,
+                     depolarisation=depolarisation,
+                     orbit_l2_altitude=orbit_l2_altitude,
+                     orbit_l2_latitude=orbit_l2_latitude,
+                     orbit_l2_longitude=orbit_l2_longitude,
+                     orbit_l2_tropopause_height=orbit_l2_tropopause_height)
+
+            # for i in range(len(orbit_l2_latitude)):
+            #     # print(len(orbit_l2_latitude))
+            #     print(i, orbit_l2_latitude[i], orbit_l2_longitude[i])
+            # quit()
+        else:
+            pass
---
+++ Caliop/SOVCC/caliop_parameter_extraction.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    caliop_parameter_extraction.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        15/06/2023 14:54
+
+import sys
+sys.path.append('../../')
+
+from Caliop.caliop import Caliop_hdf_reader
+import pandas as pd
+import numpy as np
+import sys
+import os
+
+# set year from command line arugment
+year = sys.argv[1]
+
+# caliop location on CEDA
+caliop_extracted_location = './caliop_ash_data_extraction'
+save_location = './thickness_data_extraction_extinction'
+
+# create save_location folder if not exist
+try:
+    os.stat(save_location)
+except:
+    os.mkdir(save_location)
+
+def calculate_ash_mask_thickness(ash_mask, altitude, extinction):
+    """
+    Calculates thickness of ash mask based on altitude.
+
+    Args:
+        ash_mask: List of int
+        altitude: List of float
+
+    Returns:
+        thicknesses: List of float
+    """
+    thicknesses = []  # Initialize thicknesses list
+    weighted_extinctions = []  # Initialize weighted extinction list
+    mean_heights = []
+    # Convert ash_mask and altitude to numpy arrays
+    ash_mask = np.array(ash_mask)
+    altitude = np.array(altitude)
+    extinction = np.array(extinction)
+
+    # Find indices of 1s
+    one_indices = np.where(ash_mask == 1)[0]
+
+    if len(one_indices) > 0:
+        # Find all sequences of 1s
+        sequences = np.split(one_indices, np.where(np.diff(one_indices) != 1)[0] + 1)
+
+        for seq in sequences:
+            # If the sequence has more than one element
+            if len(seq) > 1:
+                # Calculate thickness based on corresponding altitude
+                min_index = seq[0]
+                max_index = seq[-1]
+
+                if min_index > 0:
+                    max_altitude = altitude[min_index] + 0.5 * (altitude[min_index - 1] - altitude[min_index])
+                else:
+                    max_altitude = altitude[min_index]
+
+                if max_index < len(altitude) - 1:
+                    min_altitude = altitude[max_index] - 0.5 * (altitude[max_index] - altitude[max_index + 1])
+                else:
+                    min_altitude = altitude[max_index]
+
+                thickness = max_altitude - min_altitude
+                thicknesses.append(thickness)
+                extinction_mask = extinction[seq]
+                altitude_mask = altitude[seq]
+                altitude_diff = -np.diff(altitude_mask)
+                altitude_diff = np.append(altitude_diff, altitude_diff[-1])
+
+                weighted_extinctions.append(np.sum(altitude_diff * extinction_mask) / thickness)
+                mean_heights.append(np.mean([max_altitude, min_altitude]))
+
+    return thicknesses, mean_heights, weighted_extinctions
+
+
+# loop through all the sub year folder in caliop_location
+latitude_all = []
+longitude_all = []
+thickness_all = []
+ash_height_all = []
+troppause_altitude_all = []
+utc_time_all = []
+extinction_all = []
+
+for caliop_sub_folder in os.listdir(caliop_extracted_location + '/' + year):
+
+    print('---------> Reading caliop date: %s' %caliop_sub_folder)
+
+    for file in os.listdir(caliop_extracted_location + '/' + year + '/' + caliop_sub_folder):
+        # if file.endswith('.npz') & file.starts('CAL_LID_L2_05kmAPro-Standard-V4-20.2011-06'):
+        if file.endswith('.npz'):
+            print('---------> Reading caliop file: %s' %file)
+
+            dataset = np.load(caliop_extracted_location + '/' + year + '/' + caliop_sub_folder + '/' + file)
+
+            aerosol_type = dataset['caliop_v4_aerosol_type']
+            feature_type = dataset['feature_type']
+            altitude = dataset['orbit_l2_altitude']
+            latitude = dataset['orbit_l2_latitude']
+            longitude = dataset['orbit_l2_longitude']
+            tropopause_altitude = dataset['orbit_l2_tropopause_height']
+            extinction = dataset['extinction']
+            utc_time = file[35:54]
+
+            ash_mask = np.zeros((aerosol_type.shape))
+            ash_mask[(feature_type == 4) & (aerosol_type == 2)] = 1
+
+            for i in range(ash_mask.shape[1]):
+                if np.sum(ash_mask[:, i]) > 1:
+                    thickness_i, ash_height_i, extinction_i = calculate_ash_mask_thickness(ash_mask[:, i], altitude, extinction[:, i])
+                    latitude_i = latitude[i]
+                    longitude_i = longitude[i]
+                    tropopause_altitude_i = tropopause_altitude[i]
+
+                    utc_time_all.append(utc_time)
+                    latitude_all.append(latitude_i)
+                    longitude_all.append(longitude_i)
+                    thickness_all.append(thickness_i)
+                    ash_height_all.append(ash_height_i)
+                    troppause_altitude_all.append(tropopause_altitude_i)
+                    extinction_all.append(extinction_i)
+
+                    print(latitude_i, longitude_i, thickness_i, ash_height_i, extinction_i)
+
+df = pd.DataFrame({
+    'utc_time': utc_time_all,  # 'yyyy-mm-ddThh:mm:ssZ'
+    'latitude': latitude_all,
+    'longitude': longitude_all,
+    'thickness': [','.join(map(str, t)) for t in thickness_all],
+    'ash_height': [','.join(map(str, t)) for t in ash_height_all],
+    'tropopause_altitude': troppause_altitude_all,
+    'extinction': [','.join(map(str, t)) for t in extinction_all]
+})
+
+df.to_csv(save_location + '/' + year + '_thickness.csv', index=False)
+
+
+
