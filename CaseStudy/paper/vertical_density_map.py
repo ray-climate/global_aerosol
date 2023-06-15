aef26104a48584a8191772673977c84cee69c59f
ruisong <rui.song@ucl.ac.uk>
2023-06-15 15:48:09 +0100

    plot

--- CaseStudy/paper/vertical_density_map.py
+++ CaseStudy/paper/vertical_density_map.py
@@ -76,7 +76,7 @@ for npz_file in os.listdir(input_path):
             alpha_aeolus_all = np.copy(alpha)
 
 beta_aeolus_mask = np.zeros((beta_aeolus_all.shape))
-beta_aeolus_mask[beta_aeolus_mask > 0.0] = 1.0
+beta_aeolus_mask[beta_aeolus_all > 0.0] = 1.0
 
 alt_aeolus_mean = np.nanmean(alt_aeolus_all, axis=0)
 alt_aeolus_mean = (alt_aeolus_mean[1:] + alt_aeolus_mean[:-1]) / 2.0
