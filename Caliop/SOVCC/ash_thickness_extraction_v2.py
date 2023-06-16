3dca5ed17c2cf2b6424ae5640e91593daa83b033
ruisong <rui.song@ucl.ac.uk>
2023-06-16 12:15:08 +0100

    plot

--- Caliop/SOVCC/ash_thickness_extraction_v2.py
+++ Caliop/SOVCC/ash_thickness_extraction_v2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @Filename:    caliop_parameter_extraction.py
+# @Filename:    ash_thickness_extraction_v2.py
 # @Author:      Dr. Rui Song
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        15/06/2023 14:54
@@ -18,8 +18,8 @@ import os
 year = sys.argv[1]
 
 # caliop location on CEDA
-caliop_extracted_location = './caliop_ash_data_extraction'
-save_location = './thickness_data_extraction_extinction'
+caliop_extracted_location = './caliop_ash_data_extraction_extended_v1'
+save_location = './ash_thickness_data_extraction_v1'
 
 # create save_location folder if not exist
 try:
@@ -84,13 +84,22 @@ def calculate_ash_mask_thickness(ash_mask, altitude, extinction):
 
 
 # loop through all the sub year folder in caliop_location
+# location parameter
+utc_time_all = []
 latitude_all = []
 longitude_all = []
+altitude_all = []
+# layer parameters
 thickness_all = []
 ash_height_all = []
 troppause_altitude_all = []
-utc_time_all = []
+# optical parameters
 extinction_all = []
+backscatter_all = []
+depolarisation_all = []
+# type parameters
+aerosol_type_all = []
+feature_type_all = []
 
 for caliop_sub_folder in os.listdir(caliop_extracted_location + '/' + year):
 
@@ -105,44 +114,61 @@ for caliop_sub_folder in os.listdir(caliop_extracted_location + '/' + year):
 
             aerosol_type = dataset['caliop_v4_aerosol_type']
             feature_type = dataset['feature_type']
+            # location parameter
+            utc_time = file[35:54]
             altitude = dataset['orbit_l2_altitude']
             latitude = dataset['orbit_l2_latitude']
             longitude = dataset['orbit_l2_longitude']
+            # layer parameters
             tropopause_altitude = dataset['orbit_l2_tropopause_height']
+            # optical parameters
             extinction = dataset['extinction']
-            utc_time = file[35:54]
+            backscatter = dataset['backscatter']
+            depolarisation = dataset['depolarisation']
 
             ash_mask = np.zeros((aerosol_type.shape))
             ash_mask[(feature_type == 4) & (aerosol_type == 2)] = 1
 
             for i in range(ash_mask.shape[1]):
                 if np.sum(ash_mask[:, i]) > 1:
+
                     thickness_i, ash_height_i, extinction_i = calculate_ash_mask_thickness(ash_mask[:, i], altitude, extinction[:, i])
+                    utc_time_all.append(utc_time)
                     latitude_i = latitude[i]
                     longitude_i = longitude[i]
+                    altitude_i = np.copy(altitude)
                     tropopause_altitude_i = tropopause_altitude[i]
 
-                    utc_time_all.append(utc_time)
+
                     latitude_all.append(latitude_i)
                     longitude_all.append(longitude_i)
+                    altitude_all.append(altitude_i)
                     thickness_all.append(thickness_i)
                     ash_height_all.append(ash_height_i)
                     troppause_altitude_all.append(tropopause_altitude_i)
-                    extinction_all.append(extinction_i)
+                    extinction_all.append(extinction[:, i])
+                    backscatter_all.append(backscatter[:, i])
+                    depolarisation_all.append(depolarisation[:, i])
+                    aerosol_type_all.append(aerosol_type[:, i])
+                    feature_type_all.append(feature_type[:, i])
 
-                    print(latitude_i, longitude_i, thickness_i, ash_height_i, extinction_i)
 
 df = pd.DataFrame({
     'utc_time': utc_time_all,  # 'yyyy-mm-ddThh:mm:ssZ'
     'latitude': latitude_all,
     'longitude': longitude_all,
-    'thickness': [','.join(map(str, t)) for t in thickness_all],
+    'altitude': altitude_all,
+    'ash_thickness': [','.join(map(str, t)) for t in thickness_all],
     'ash_height': [','.join(map(str, t)) for t in ash_height_all],
     'tropopause_altitude': troppause_altitude_all,
-    'extinction': [','.join(map(str, t)) for t in extinction_all]
+    'extinction': [','.join(map(str, t)) for t in extinction_all],
+    'backscatter': [','.join(map(str, t)) for t in backscatter_all],
+    'depolarisation': [','.join(map(str, t)) for t in depolarisation_all],
+    'aerosol_type': [','.join(map(str, t)) for t in aerosol_type_all],
+    'feature_type': [','.join(map(str, t)) for t in feature_type_all]
 })
 
-df.to_csv(save_location + '/' + year + '_thickness.csv', index=False)
+df.to_csv(save_location + '/' + year + '_ash_thickness.csv', index=False)
 
 
 
