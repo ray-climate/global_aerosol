63ed542123306d5d22be799d769cd8d6a5acc753
ruisong <rui.song@ucl.ac.uk>
2023-06-28 15:33:14 +0100

    plot

--- CaseStudy/paper/vertical_density_map.py
+++ CaseStudy/paper/vertical_density_map.py
@@ -140,6 +140,7 @@ beta_aeolus_mean = np.nanmean(beta_aeolus_all, axis=0)
 conversion_factor = (np.nanmean(dp_caliop_mean) * 0.82 * 2) / (1. - np.nanmean(dp_caliop_mean) * 0.82)
 conversion_factor = 1 / (1. + conversion_factor)
 print('mean depolarisation ratio: ', np.nanmean(dp_caliop_mean))
+print('std depolarisation ratio: ', np.nanstd(dp_caliop_mean))
 print('delta circ 355 is: ', conversion_factor)
 
 ################## plot depolarisation ratio
