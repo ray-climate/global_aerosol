deb60e13453c7c8a9e584f37396bac8f26639b4a
ruisong <rui.song@ucl.ac.uk>
2023-06-30 12:20:39 +0100

    plot

--- CaseStudy/paper/AOD_caliop_June18-19.py
+++ CaseStudy/paper/AOD_caliop_June18-19.py
@@ -19,7 +19,6 @@ input_path = '../Sahara2020Summer/aeolus_caliop_sahara2020_extraction_output/'
 script_name = os.path.splitext(os.path.basename(os.path.abspath(__file__)))[0]
 save_path = f'./figures/{script_name}_output/'
 
-quit()
 for npz_file in os.listdir(input_path):
     if npz_file.endswith('.npz') & ('caliop_dbd_ascending_202006181612' in npz_file):
 
@@ -29,3 +28,16 @@ for npz_file in os.listdir(input_path):
         alpha_caliop = np.load(input_path + npz_file, allow_pickle=True)['alpha']
         dp_caliop = np.load(input_path + npz_file, allow_pickle=True)['dp']
         aod_caliop = np.load(input_path + npz_file, allow_pickle=True)['aod']
+
+fontsize = 12
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.plot(lat_caliop, aod_caliop, 'g.--',lw=2, markersize=5, label='CALIOP')
+ax.set_xlabel('Latitude', fontsize=fontsize)
+ax.set_ylabel('Extinction [km$^{-1}$]', fontsize=fontsize)
+ax.set_xlim(5.5, 23.)
+ax.set_ylim(1e-3, 5e0)
+# ax.set_title(f'layer between {layer[0]:.1f} km - {layer[1]:.1f} km', fontsize=fontsize, loc='left')
+ax.tick_params(axis='both', labelsize=fontsize)
+ax.legend(loc='best', fontsize=fontsize)
+ax.set_yscale('log')
+plt.savefig(save_path + f'CALIOP_AOD_June18.png', dpi=300)
