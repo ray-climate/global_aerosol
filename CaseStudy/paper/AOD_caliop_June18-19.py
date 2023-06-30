31ab8a5ec2aa0359b29500af1be15f60bfe2f5cf
ruisong <rui.song@ucl.ac.uk>
2023-06-30 12:10:14 +0100

    plot

--- CaseStudy/paper/AOD_caliop_June18-19.py
+++ CaseStudy/paper/AOD_caliop_June18-19.py
@@ -18,7 +18,6 @@ import os
 input_path = '../Sahara2020Summer/aeolus_caliop_sahara2020_extraction_output/'
 script_name = os.path.splitext(os.path.basename(os.path.abspath(__file__)))[0]
 save_path = f'./figures/{script_name}_output/'
-print(save_path)
 
 quit()
 for npz_file in os.listdir(input_path):
