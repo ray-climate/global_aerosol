8497a069a23104167a450ff2e6c35d0cfe82a376
ruisong <rui.song@ucl.ac.uk>
2023-07-07 15:22:20 +0100

    plot

--- CaseStudy/paper/plot_aod_June19.py
+++ CaseStudy/paper/plot_aod_June19.py
@@ -5,6 +5,9 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        07/07/2023 14:33
 
+import sys
+sys.path.append('../../')
+from Caliop.caliop import Caliop_hdf_reader
 import matplotlib.ticker as ticker
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -15,7 +18,67 @@ import sys
 import csv
 import os
 
-# yes
+lat_up = 40. # degree
+lat_down = 0. # degree
+meridional_boundary = [-60., 30.] # degree
+lon_left = meridional_boundary[0]
+lon_right = meridional_boundary[1]
+
+caliop_file = '/gws/nopw/j04/eo_shared_data_vol1/satellite/calipso/APro5km/2020/2020_06_18/CAL_LID_L2_05kmAPro-Standard-V4-20.2020-06-18T15-45-38ZD.hdf'
+
+def read_caliop_data(caliop_file_path, lat_down, lat_up, lon_left, lon_right):
+
+    caliop_request = Caliop_hdf_reader()
+
+    # Read data from Caliop file
+    # caliop_request.get_variable_names(caliop_file_path)
+    caliop_utc = np.asarray(caliop_request._get_profile_UTC(caliop_file_path))
+    caliop_latitude = np.asarray(caliop_request._get_latitude(caliop_file_path))
+    caliop_longitude = np.asarray(caliop_request._get_longitude(caliop_file_path))
+    caliop_altitude = np.asarray(caliop_request.get_altitudes(caliop_file_path))
+    caliop_AOD_532_trop = np.asarray(caliop_request._get_aod(caliop_file_path, 'Column_Optical_Depth_Tropospheric_Aerosols_532'))
+    caliop_AOD_532_stra = np.asarray(caliop_request._get_aod(caliop_file_path, 'Column_Optical_Depth_Stratospheric_Aerosols_532'))
+    caliop_AOD_532_total = caliop_AOD_532_trop + caliop_AOD_532_stra
+
+    caliop_beta = np.asarray(
+        caliop_request._get_calipso_data(filename=caliop_file_path, variable='Total_Backscatter_Coefficient_532'))
+    caliop_alpha = np.asarray(
+        caliop_request._get_calipso_data(filename=caliop_file_path, variable='Extinction_Coefficient_532'))
+    (caliop_aerosol_type, caliop_feature_type) = caliop_request._get_feature_classification(filename=caliop_file_path,
+                                                                                            variable='Atmospheric_Volume_Description')
+
+    caliop_aerosol_type_mask = np.copy(caliop_aerosol_type)
+    caliop_aerosol_type_mask[caliop_feature_type != 3] = -1.
+    caliop_Depolarization_Ratio = np.asarray(caliop_request._get_calipso_data(filename=caliop_file_path,
+                                                                              variable='Particulate_Depolarization_Ratio_Profile_532'))
+
+    # Apply spatial mask
+    spatial_mask = np.where((caliop_latitude > lat_down) & (caliop_latitude < lat_up) &
+                            (caliop_longitude > lon_left) & (caliop_longitude < lon_right))[0]
+
+    if len(spatial_mask) > 0:
+
+        # logger.info('Data found within the spatial window: %s', caliop_file_path)
+        print('Data found within the spatial window: ', caliop_file_path)
+
+        return caliop_utc[spatial_mask], caliop_latitude[spatial_mask], \
+               caliop_longitude[spatial_mask], caliop_altitude, caliop_beta[:, spatial_mask], caliop_alpha[:, spatial_mask], \
+               caliop_aerosol_type[:, spatial_mask], caliop_feature_type[:, spatial_mask], caliop_Depolarization_Ratio[:, spatial_mask], caliop_AOD_532_total[spatial_mask]
+    else:
+        return None
+
+caliop_data = read_caliop_data(caliop_file, lat_down, lat_up, lon_left, lon_right)
+caliop_utc, caliop_latitude, caliop_longitude, caliop_altitude, caliop_beta, caliop_alpha, caliop_aerosol_type, caliop_feature_type, caliop_Depolarization_Ratio, caliop_AOD_532_total = caliop_data
+
+spatial_mask = np.where((caliop_latitude > lat_down) & (caliop_latitude < lat_up) & (caliop_longitude > lon_left) & (caliop_longitude < lon_right))[0]
+
+print(caliop_latitude.shape)
+print(caliop_alpha.shape)
+quit()
+
+
+
+
 aod_file = './output_aod_file.npz'
 script_name = os.path.splitext(os.path.basename(os.path.abspath(__file__)))[0]
 save_path = f'./figures/{script_name}_output/'
