55348ac56939f35a096e0b924cbda92a1f098655
ruisong <rui.song@ucl.ac.uk>
2023-07-10 15:20:22 +0100

    pl

--- CaseStudy/paper/lidar_ratio_both.py
+++ CaseStudy/paper/lidar_ratio_both.py
@@ -121,11 +121,8 @@ lr_aeolus_std = np.nanstd(lr_aeolus_all, axis=0)
 alt_aeolus_mean = np.nanmean(alt_aeolus_all, axis=0)[0:-1]
 
 print(len(lr_aeolus_mean))
-alt_aeolus_mean = np.nanmean(alt_aeolus_all, axis=0)
 print(len(alt_aeolus_mean))
-print(alt_aeolus_mean)
-alt_aeolus_mean = (alt_aeolus_mean[1:] + alt_aeolus_mean[:-1]) / 2.0
-print(alt_aeolus_mean)
+
 # Set font parameters
 font = {'family': 'serif',
         'weight': 'normal',
