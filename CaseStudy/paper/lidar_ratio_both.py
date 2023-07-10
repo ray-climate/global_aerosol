01eacb5917121a4f1315ca6c3b0efd8fbd844df6
ruisong <rui.song@ucl.ac.uk>
2023-07-10 14:55:48 +0100

    plot

--- CaseStudy/paper/lidar_ratio_both.py
+++ CaseStudy/paper/lidar_ratio_both.py
@@ -58,9 +58,67 @@ for npz_file in os.listdir(input_path):
             lr_caliop_all = np.copy(lr)
 
 lr_caliop_all[lr_caliop_all <= 0.] = np.nan
-
 lr_caliop_mean = np.nanmean(lr_caliop_all, axis=1)
 lr_caliop_std = np.nanstd(lr_caliop_all, axis=1)
+
+
+beta_aeolus_all = []
+alpha_aeolus_all = []
+alt_aeolus_all = []
+lr_aeolus_all = []
+
+# convert qc_aeolus to bits and check the quality of the data
+def qc_to_bits(qc_array):
+    qc_uint8 = qc_array.astype(np.uint8)
+    qc_bits = np.unpackbits(qc_uint8, axis=1)
+    qc_bits = qc_bits.reshape(*qc_array.shape, -1)
+    return qc_bits
+
+font = {'family': 'serif',
+        'weight': 'normal',
+        'size': 14}
+plt.rc('font', **font)
+
+conversion_factor = 0.564
+
+for npz_file in os.listdir(input_path):
+    if npz_file.endswith('.npz') & (('aeolus_qc_descending_20200619' in  npz_file) | ('aeolus_qc_descending_20200618' in  npz_file)):
+        # print the file name and variables in the file
+        print(npz_file)
+        alt = np.load(input_path + npz_file, allow_pickle=True)['alt']
+        beta = np.load(input_path + npz_file, allow_pickle=True)['beta']
+        alpha = np.load(input_path + npz_file, allow_pickle=True)['alpha']
+        qc_aeolus = np.load(input_path + npz_file, allow_pickle=True)['qc']
+
+        alpha[alpha <= 0.] = np.nan
+        beta[beta <= 0.] = np.nan
+
+        qc_bits = qc_to_bits(qc_aeolus)
+        first_bit = qc_bits[:, :, -1]
+        second_bit = qc_bits[:, :, -2]
+
+        # Create a boolean mask where the second bit equals 1 (valid data)
+        valid_mask_extinction = first_bit == 1
+        valid_mask_backscatter = second_bit == 1
+        # set invalid data to nan
+        alpha_aeolus_qc = np.where(valid_mask_extinction, alpha, np.nan)
+        beta_aeolus_qc = np.where(valid_mask_backscatter, beta, np.nan)
+
+        lr_aeolus_qc = np.nanmean(alpha_aeolus_qc, axis=0) / (np.nanmean(beta_aeolus_qc, axis=0) / conversion_factor)
+        alt_mean = np.nanmean(alt, axis=0)
+        # print('lidar ratio: ', lr_aeolus_qc)
+        lr_aeolus_qc[lr_aeolus_qc <= 25.] = np.nan
+        lr_aeolus_qc[lr_aeolus_qc > 100.] = np.nan
+
+        lr_aeolus_all.append(lr_aeolus_qc)
+        alt_aeolus_all.append(alt_mean)
+
+lr_aeolus_all = np.array(lr_aeolus_all)
+alt_aeolus_all = np.array(alt_aeolus_all)
+
+lr_aeolus_mean = np.nanmean(lr_aeolus_all, axis=0)
+lr_aeolus_std = np.nanstd(lr_aeolus_all, axis=0)
+alt_aeolus_mean = np.nanmean(alt_aeolus_all, axis=0)[0:-1]
 
 # Set font parameters
 font = {'family': 'serif',
@@ -70,6 +128,7 @@ plt.rc('font', **font)
 
 plt.figure(figsize=(8, 12))
 plt.errorbar(lr_caliop_mean, alt_caliop, xerr=lr_caliop_std, fmt='o', color='green', ecolor='lightgreen', elinewidth=3, capsize=0)
+plt.errorbar(lr_aeolus_mean, alt_aeolus_mean, xerr=lr_aeolus_std, fmt='o', color='blue', ecolor='lightblue', elinewidth=3, capsize=0)
 plt.xlabel('Lidar Ratio')
 plt.ylabel('Altitude')
 plt.xlim(0, 100)
