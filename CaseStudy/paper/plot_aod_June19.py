5aafcc01282a818b799ccc0b7cd4d62ad88797f3
ruisong <rui.song@ucl.ac.uk>
2023-07-20 14:19:50 +0100

    pl

--- CaseStudy/paper/plot_aod_June19.py
+++ CaseStudy/paper/plot_aod_June19.py
@@ -91,6 +91,7 @@ modis_aod_all = np.load(aod_file, allow_pickle=True)['modis_aod_all']
 caliop_aod_trap = np.zeros((caliop_alpha.shape[1]))
 for i in range(caliop_alpha.shape[1]):
     alpha_i = caliop_alpha[:, i]
+    alpha_i[alpha_i < 0] = np.nan
     alpha_i[np.isnan(alpha_i)] = 0
     caliop_aod_trap[i] = np.trapz(alpha_i[::-1], caliop_altitude[::-1])
     print(caliop_aod_trap[i])
