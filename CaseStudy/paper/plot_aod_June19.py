9485b47d43ef68b4e1f6705a11b9a36d8ea3a31e
ruisong <rui.song@ucl.ac.uk>
2023-07-20 13:34:09 +0100

    pl

--- CaseStudy/paper/plot_aod_June19.py
+++ CaseStudy/paper/plot_aod_June19.py
@@ -144,6 +144,7 @@ for i in range(caliop_latitude.shape[0]):
     aerosol_layer_height = caliop_altitude * aersol_mask[:, i]
     top_plume_height_top[i] = np.max(aerosol_layer_height)
     top_plume_height_bottom[i] = np.min(aerosol_layer_height)
+    print(top_plume_height_top[i], top_plume_height_bottom[i])
     if (top_plume_height_top[i] == 0) | (top_plume_height_bottom[i] == 0):
         top_plume_height_top[i] = np.nan
         top_plume_height_bottom[i] = np.nan
