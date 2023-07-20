8191a4525ca8b2940b96d5eb33410545fc80bc35
ruisong <rui.song@ucl.ac.uk>
2023-07-20 15:48:33 +0100

    pl

--- CaseStudy/paper/plot_aod_June19.py
+++ CaseStudy/paper/plot_aod_June19.py
@@ -171,7 +171,7 @@ plt.savefig(save_path + f'dust_plume_height.png', dpi=300)
 caliop_aod_trap_filter = np.copy(caliop_aod_trap)
 caliop_aod_trap_corr_filter = np.copy(caliop_aod_trap_corr)
 
-caliop_aod_trap_filter[plum_diff_index <1.] = np.nan
+# caliop_aod_trap_filter[plum_diff_index <1.] = np.nan
 # caliop_aod_trap_corr_filter[plum_diff_index <1.] = np.nan
 
 fig, ax = plt.subplots(figsize=(11, 5))
