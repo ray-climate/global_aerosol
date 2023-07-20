70ee8be6dd4b6532424dd0638650657cdda2166c
ruisong <rui.song@ucl.ac.uk>
2023-07-20 15:08:57 +0100

    pl

--- CaseStudy/paper/plot_aod_June19.py
+++ CaseStudy/paper/plot_aod_June19.py
@@ -96,9 +96,10 @@ for i in range(caliop_alpha.shape[1]):
     mask_i = aersol_mask[:, i]
     alpha_i[alpha_i < 0] = np.nan
     alpha_i[np.isnan(alpha_i)] = 0
-    print(lat_caliop[i], np.trapz(alpha_i[::-1], caliop_altitude[::-1]))
+
     alpha_i[(caliop_altitude > 7.)] = 0.
-    print(lat_caliop[i], np.trapz(alpha_i[::-1], caliop_altitude[::-1]))
+    mask_i[(caliop_altitude > 7.)] = 0.
+
     caliop_aod_trap[i] = np.trapz(alpha_i[::-1], caliop_altitude[::-1])
     plume_thickness[i] = np.trapz(mask_i[::-1], caliop_altitude[::-1])
 
