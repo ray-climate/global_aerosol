035d9f4910505ca3d74a4464f36c15b5f335013c
ruisong <rui.song@ucl.ac.uk>
2023-07-20 14:24:50 +0100

    pl

--- CaseStudy/paper/plot_aod_June19.py
+++ CaseStudy/paper/plot_aod_June19.py
@@ -164,8 +164,8 @@ fig, ax = plt.subplots(figsize=(11, 5))
 
 ax.plot(caliop_latitude[mask_less_equal], top_plume_height_top[mask_less_equal], 'r-*', lw=3, markersize=10, label='dust top')
 ax.plot(caliop_latitude[mask_less_equal], top_plume_height_bottom[mask_less_equal], 'b-*', lw=3, markersize=10, label='dust bottom')
-ax.plot(caliop_latitude, top_plume_height_bottom, 'k-.', lw=3, markersize=10, label='dust bottom nonfilter')
-ax.plot(caliop_latitude, plume_thickness, 'g-.', lw=3, markersize=10, label='dust thickness')
+# ax.plot(caliop_latitude, top_plume_height_bottom, 'k-.', lw=3, markersize=10, label='dust bottom nonfilter')
+ax.plot(caliop_latitude[mask_less_equal], plume_thickness[mask_less_equal], 'g-*', lw=3, markersize=10, label='dust thickness')
 
 ax.set_xlabel('Latitude [$^{\circ}$]', fontsize=fontsize)
 ax.set_ylabel('Altitude [km]', fontsize=fontsize)
