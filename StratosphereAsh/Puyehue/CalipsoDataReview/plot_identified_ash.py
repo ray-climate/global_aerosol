cbd5983b05be74e43b05b0303c769212c0d46429
ruisong <rui.song@ucl.ac.uk>
2023-10-11 12:46:48 +0100

    plot identified ash

--- StratosphereAsh/Puyehue/CalipsoDataReview/plot_identified_ash.py
+++ StratosphereAsh/Puyehue/CalipsoDataReview/plot_identified_ash.py
@@ -5,11 +5,23 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        11/10/2023 11:22
 
+import pandas as pd
 import os
 
+variable_file_location = '../../../Caliop/SOVCC/filtered_data_continuous_10/'
 figure_save_location = './figures'
 
 # create save_location folder if not exist
 if not os.path.exists(figure_save_location):
     os.mkdir(figure_save_location)
+
+
+files = [file for file in os.listdir(variable_file_location) if file.endswith('.csv')]
+
+# Initiate empty DataFrame to store all data
+all_data = pd.DataFrame(columns=['utc_time', 'thickness', 'latitude', 'longitude', 'ash_height'])  # add ash_height column
+
+for file in files:
+    data = pd.read_csv(variable_file_location + '/' + file)
+    print(f"Processing file {file}")
 
