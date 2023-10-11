c19f9af47b74ac8b60d84b870285422208f8bad4
ruisong <rui.song@ucl.ac.uk>
2023-10-11 12:52:12 +0100

    plot identified ash

--- StratosphereAsh/Puyehue/CalipsoDataReview/plot_identified_ash.py
+++ StratosphereAsh/Puyehue/CalipsoDataReview/plot_identified_ash.py
@@ -15,6 +15,10 @@ figure_save_location = './figures'
 if not os.path.exists(figure_save_location):
     os.mkdir(figure_save_location)
 
+start_date = '2011-06-13'
+end_date = '2011-08-20'
+lat_north = 0
+lat_south = -80
 
 files = [file for file in os.listdir(variable_file_location) if file.endswith('.csv')]
 
@@ -24,4 +28,23 @@ all_data = pd.DataFrame(columns=['utc_time', 'thickness', 'latitude', 'longitude
 for file in files:
     data = pd.read_csv(variable_file_location + '/' + file)
     print(f"Processing file {file}")
+
+    for column in ['utc_time', 'thickness', 'latitude', 'longitude', 'ash_height']:  # include 'extinction'
+        if column == 'utc_time':
+            # Convert utc_time to datetime format
+            data[column] = pd.to_datetime(data[column], format='%Y-%m-%d %H:%M:%S')
+        else:
+            data[column] = pd.to_numeric(data[column], errors='coerce')
+
+    all_data = all_data.append(data[['utc_time', 'thickness', 'latitude', 'longitude', 'ash_height']], ignore_index=True)  # include 'extinction' and 'AOD'
+
+# Remove rows with any NaN values
+all_data = all_data.dropna()
+
+# Filter data based on defined start_time, end_time, lat_top, and lat_bottom
+all_data = all_data[(all_data['utc_time'] >= start_date) & (all_data['utc_time'] <= end_date) &
+                    (all_data['latitude'] >= lat_south) & (all_data['latitude'] <= lat_north)]
+
+print(all_data['utc_time'])
+print(all_data.shape)
 
