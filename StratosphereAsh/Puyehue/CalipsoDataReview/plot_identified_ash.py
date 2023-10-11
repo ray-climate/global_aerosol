4ea59cc4499d57b3d4497b9c2536679e49857e9c
ruisong <rui.song@ucl.ac.uk>
2023-10-11 11:23:20 +0100

    pl

---
+++ StratosphereAsh/Puyehue/CalipsoDataReview/plot_identified_ash.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    plot_identified_ash.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        11/10/2023 11:22
+
+import os
+
+figure_save_location = './figures'
+
+# create save_location folder if not exist
+if not os.path.exists(figure_save_location):
+    os.mkdir(figure_save_location)
+
