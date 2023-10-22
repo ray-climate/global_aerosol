3be4b10db6559f8f032f9fe8abdd42caba888ef9
ruisong <rui.song@ucl.ac.uk>
2023-10-22 19:10:13 +0100

    download caliop

--- StratosphereAsh/Puyehue/CalipsoDataReview/plot_identified_ash_update.py
+++ StratosphereAsh/Puyehue/CalipsoDataReview/plot_identified_ash_update.py
@@ -294,7 +294,9 @@ def main():
         index_ticks_l1 = np.linspace(start_index_l1, end_index_l1, num_xticks).astype(int)
         # determine the MAX_ALTITUDE index
         index_max_altitude_l1 = np.abs(alt_caliop_l1 - MAX_ALTITUDE).argmin()
-
+        print(index_max_altitude_l1)
+        print(alt_caliop_l1[index_max_altitude_l1])
+        quit()
         # Set x-ticks and x-tick labels
         ax2.set_xticks(index_ticks_l1)
         ax2.set_xticklabels(["{:.1f}".format(val) for val in footprint_lat_caliop_l1[index_ticks_l1]])
