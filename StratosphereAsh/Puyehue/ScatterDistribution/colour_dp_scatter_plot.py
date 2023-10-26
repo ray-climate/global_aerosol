aebb1b634aa446a69061d4923e9f79a06c8c2dbf
ray <rui.song90@gmail.com>
2023-10-26 16:12:43 +0100

    download caliop

--- StratosphereAsh/Puyehue/ScatterDistribution/colour_dp_scatter_plot.py
+++ StratosphereAsh/Puyehue/ScatterDistribution/colour_dp_scatter_plot.py
@@ -5,8 +5,10 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        26/10/2023 14:33
 
+from scipy.stats import gaussian_kde
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import os
 
 INPUT_DIR = './csv'
@@ -35,15 +37,32 @@ for file in os.listdir(INPUT_DIR):
                 except:
                     continue
 
-# Plotting the data
+# Compute point densities
+data = np.vstack([depolarization_ratio, color_ratio])
+kde = gaussian_kde(data)
+densities = kde(data)
+
+# Plot
 plt.figure(figsize=(10, 7))
-hb = plt.hexbin(depolarization_ratio, color_ratio, gridsize=50, cmap='inferno')
-cb = plt.colorbar(hb)
-cb.set_label('Counts')
+scatter = plt.scatter(depolarization_ratio, color_ratio, c=densities, s=50, edgecolor='', cmap='inferno')
+plt.colorbar(scatter, label='Density')
 plt.xlabel('Depolarization Ratio')
 plt.ylabel('Color Ratio')
-plt.title('2D Histogram of Depolarization Ratio vs. Color Ratio')
+plt.title('Scatter Plot with Density Coloring')
 plt.xlim(0, 0.6)
 plt.ylim(0, 1.)
 plt.tight_layout()
+#
+# # Plotting the data
+# plt.figure(figsize=(10, 7))
+# hb = plt.hexbin(depolarization_ratio, color_ratio, gridsize=50, cmap='inferno')
+# cb = plt.colorbar(hb)
+# cb.set_label('Counts')
+# plt.xlabel('Depolarization Ratio')
+# plt.ylabel('Color Ratio')
+# plt.title('2D Histogram of Depolarization Ratio vs. Color Ratio')
+# plt.xlim(0, 0.6)
+# plt.ylim(0, 1.)
+# plt.tight_layout()
+
 plt.savefig(FIG_DIR + '/2D_Histogram_of_Depolarization_Ratio_vs_Color_Ratio.png', dpi=300)
