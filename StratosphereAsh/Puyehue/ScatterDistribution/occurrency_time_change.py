c19b4e85edd22b4543a5d8324b0acc060ae724c8
ray <rui.song90@gmail.com>
2023-10-27 21:24:52 +0100

    download caliop

--- StratosphereAsh/Puyehue/ScatterDistribution/occurrency_time_change.py
+++ StratosphereAsh/Puyehue/ScatterDistribution/occurrency_time_change.py
@@ -15,11 +15,26 @@ import os
 INPUT_DIR = './csv'
 FIG_DIR = './plots_time'
 
+dates_all = []
+lats_all = []
+
 for file in os.listdir(INPUT_DIR):
-    if file.endswith('.csv'):
+    if file.endswith('.csv') & ('2011-06-16' in file):
         print('Reading file: %s' % file)
 
         # derive dates from file name
-        date = file.split('.')[1][0:12]
-        print(date)
-        quit()
+        date = file.split('.')[1][0:10]
+
+        with open(INPUT_DIR + '/' + file, 'r') as f:
+            lines = f.readlines()
+            for line in lines[1:]:
+                try:
+                    if (float(line.split(',')[4]) > 0) &(float(line.split(',')[5]) > 0) & (float(line.split(',')[6]) >= 2.) & (float(line.split(',')[6]) <= 4.):
+                        dates_all.append(date)
+                        lats_all.append(float(line.split(',')[0]))
+                except:
+                    continue
+
+print(dates_all)
+print(lats_all)
+
