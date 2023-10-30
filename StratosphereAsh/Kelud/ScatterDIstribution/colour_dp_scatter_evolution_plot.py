e6262af5dd2b59fd29a27cc2ee05eecf16ae1894
ray <rui.song90@gmail.com>
2023-10-30 16:02:36 +0000

    download caliop

---
+++ StratosphereAsh/Kelud/ScatterDIstribution/colour_dp_scatter_evolution_plot.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    colour_dp_scatter_evolution_plot.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        30/10/2023 15:56
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import argparse
+import os
+
+INPUT_DIR = './csv'
+FIG_DIR = './plots_days'
+
+try:
+    os.stat(FIG_DIR)
+except:
+    os.mkdir(FIG_DIR)
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Script to process data at specific date.")
+parser.add_argument("DATE_SEARCH", type=str, help="Date in the format YYYY-MM-DD.")
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Use the parsed arguments
+DATE_SEARCH = args.DATE_SEARCH
+
+color_ratio = []
+depolarization_ratio = []
+Aerosol_type = []
+
+for file in os.listdir(INPUT_DIR):
+    if file.endswith('.csv') & ('.%sT'%DATE_SEARCH in file):
+        print('Reading file: %s' % file)
+
+        # start to read the csv file and load variables of 'Color_Ratio' and 'Depolarization_Ratio'
+
+        with open(INPUT_DIR + '/' + file, 'r') as f:
+            lines = f.readlines()
+            for line in lines[1:]:
+                try:
+                    if (float(line.split(',')[4]) > 0) &(float(line.split(',')[5]) > 0) & (float(line.split(',')[6]) >= 2.) & (float(line.split(',')[6]) <= 4.):
+                        color_ratio.append(float(line.split(',')[4]))
+                        depolarization_ratio.append(float(line.split(',')[5]))
+                        Aerosol_type.append(float(line.split(',')[6]))
+
+                except:
+                    continue
+
+color_ratio = np.array(color_ratio)
+depolarization_ratio = np.array(depolarization_ratio)
+Aerosol_type = np.array(Aerosol_type)
+
+# Organize data into a pandas DataFrame
+df = pd.DataFrame({
+    'Depolarization Ratio': depolarization_ratio,
+    'Color Ratio': color_ratio,
+    'Aerosol Type': Aerosol_type
+})
+
+# Replace values in 'Aerosol Type' column
+aerosol_name_mapping = {2.: 'ash', 3.: 'sulfate', 4.: 'smoke'}
+df['Aerosol Type'] = df['Aerosol Type'].map(aerosol_name_mapping)
+# Update palette
+palette = {'ash': 'red', 'sulfate': 'green', 'smoke': 'black'}
+
+
+# add a figure
+plt.figure(figsize=(8, 8))
+# Generate the 2D scatter plot using seaborn's jointplot
+g = sns.jointplot(data=df, x="Depolarization Ratio", y="Color Ratio", marker="+", s=30, color='red')  # `s` inside scatter_kws defines the size
+g.set_axis_labels('Depolarization Ratio', 'Color Ratio')
+plt.xlim(0, 0.6)
+plt.ylim(0, 1.)
+plt.title('%s' % DATE_SEARCH)
+
+plt.tight_layout()
+plt.savefig(FIG_DIR + '/joint_distribution_%s.png'%DATE_SEARCH, dpi=300)
+
+
---
+++ StratosphereAsh/Kelud/ScatterDIstribution/screen_all_orbits.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    screen_all_orbits.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        30/10/2023 15:58
+
+import os
+import sys
+import csv
+import logging
+import datetime
+import pandas as pd
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from matplotlib.colors import ListedColormap, BoundaryNorm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.gridspec import GridSpec
+import matplotlib as mpl
+
+# Append the custom path to system path
+sys.path.append('../../../')
+from getColocationData.get_caliop import *
+
+# Constants
+LOG_EXTENSION = ".log"
+NORTHERN_LATITUDE = 20.
+SOUTHERN_LATITUDE = -20.
+MIN_ALTITUDE = 0
+MAX_ALTITUDE = 26
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Script to process data at specific date.")
+parser.add_argument("DATE_SEARCH", type=str, help="Date in the format YYYY-MM-DD.")
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Use the parsed arguments
+DATE_SEARCH = args.DATE_SEARCH
+
+# Directory paths and locations
+CALIPSO_DATA_PATH = "/network/group/aopp/eodg/calipso/asdc.larc.nasa.gov/data/CALIPSO/LID_L2_05kmALay-Standard-V4-51/"
+FIGURE_OUTPUT_PATH = './figures'
+CSV_OUTPUT_PATH = './csv'
+
+# Initialize Logging
+script_base_name, _ = os.path.splitext(sys.modules['__main__'].__file__)
+log_file_name = script_base_name + LOG_EXTENSION
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', filemode='w', filename=log_file_name, level=logging.INFO)
+logger = logging.getLogger()
+
+def main():
+
+    """Main function to screen CALIPSO Level-2 data"""
+    # Create figure saving directory if not present
+    if not os.path.exists(FIGURE_OUTPUT_PATH):
+        os.mkdir(FIGURE_OUTPUT_PATH)
+
+    # Create csv saving directory if not present
+    if not os.path.exists(CSV_OUTPUT_PATH):
+        os.mkdir(CSV_OUTPUT_PATH)
+
+    # search all data at CALIPSO_DATA_PATH/year/month/
+    year = DATE_SEARCH.split('-')[0]
+    month = DATE_SEARCH.split('-')[1]
+    day = DATE_SEARCH.split('-')[2]
+    data_path = os.path.join(CALIPSO_DATA_PATH, year, month)
+    file_list = os.listdir(data_path)
+    # only keep files that contains year-month-day in the full file name
+    file_list = [file for file in file_list if DATE_SEARCH in file]
+
+    # iterate through all files
+    for file in file_list:
+        # print(data_path + file)
+        try:
+            (footprint_lat_caliop, footprint_lon_caliop,
+             caliop_Integrated_Attenuated_Total_Color_Ratio,
+             caliop_Integrated_Particulate_Depolarization_Ratio,
+             caliop_aerosol_type, caliop_feature_type,
+             caliop_Layer_Top_Altitude, caliop_Layer_Base_Altitude)\
+                = extract_variables_from_caliop_ALay(data_path + '/' + file, logger)
+            print('Processing file: {}'.format(file))
+        except:
+            print('Cannot process file: {}'.format(file))
+            continue
+
+        caliop_aerosol_type = caliop_aerosol_type[:, (footprint_lat_caliop > SOUTHERN_LATITUDE) & (footprint_lat_caliop < NORTHERN_LATITUDE)]
+        caliop_feature_type = caliop_feature_type[:, (footprint_lat_caliop > SOUTHERN_LATITUDE) & (footprint_lat_caliop < NORTHERN_LATITUDE)]
+        caliop_dp = caliop_Integrated_Particulate_Depolarization_Ratio[:, (footprint_lat_caliop > SOUTHERN_LATITUDE) & (footprint_lat_caliop < NORTHERN_LATITUDE)]
+        caliop_color = caliop_Integrated_Attenuated_Total_Color_Ratio[:, (footprint_lat_caliop > SOUTHERN_LATITUDE) & (footprint_lat_caliop < NORTHERN_LATITUDE)]
+        caliop_Layer_Top = caliop_Layer_Top_Altitude[:, (footprint_lat_caliop > SOUTHERN_LATITUDE) & (footprint_lat_caliop < NORTHERN_LATITUDE)]
+        caliop_Layer_Base = caliop_Layer_Base_Altitude[:, (footprint_lat_caliop > SOUTHERN_LATITUDE) & (footprint_lat_caliop < NORTHERN_LATITUDE)]
+        caliop_lat = footprint_lat_caliop[(footprint_lat_caliop > SOUTHERN_LATITUDE) & (footprint_lat_caliop < NORTHERN_LATITUDE)]
+        caliop_lon = footprint_lon_caliop[(footprint_lat_caliop > SOUTHERN_LATITUDE) & (footprint_lat_caliop < NORTHERN_LATITUDE)]
+
+        # get index of caliop_feature_type == 4
+        caliop_feature_type_4_index = np.where(caliop_feature_type == 4)
+
+        # aerosol subtype
+        # 0 = invalid
+        # 1 = polar stratospheric aerosol
+        # 2 = volcanic ash
+        # 3 = sulfate
+        # 4 = elevated smoke
+        # 5 = unclassified
+        # 6 = spare
+        # 7 = spare
+
+        ash_mask = np.zeros((caliop_feature_type.shape))
+        ash_mask[(caliop_feature_type == 4) & (caliop_aerosol_type == 2)] = 1
+        number_of_ash_layer = np.sum(ash_mask)
+        print('Number of ash layer: {}'.format(number_of_ash_layer))
+        if number_of_ash_layer < 5:
+            # if number of ash layer < 5, skip this file
+            continue
+
+        # save all detected feature type 4 into a csv file, iterative to write each row
+        with open(CSV_OUTPUT_PATH + '/' + file.replace('.hdf', '.csv'), 'w') as csvfile:
+            # first row to write name of parameters
+
+            writer = csv.writer(csvfile, lineterminator='\n')
+            writer.writerow(('Latitude', 'Longitude', 'Altitude_Base', 'Altitude_Top', 'Color_Ratio',
+                             'Depolarization_Ratio', 'Aerosol_type'))
+
+            for i in range(len(caliop_feature_type_4_index[0])):
+
+                index_row = caliop_feature_type_4_index[0][i]
+                index_col = caliop_feature_type_4_index[1][i]
+
+                # start to write every parameter into the new row
+                writer.writerow((caliop_lat[index_col],
+                                 caliop_lon[index_col],
+                                 caliop_Layer_Base[index_row, index_col],
+                                 caliop_Layer_Top[index_row, index_col],
+                                 caliop_color[index_row, index_col],
+                                 caliop_dp[index_row, index_col],
+                                 caliop_aerosol_type[index_row, index_col]))
+
+        print('Finished processing file: {}'.format(file))
+
+if __name__ == "__main__":
+    main()
