3f6223e07a20cd9a06d26a93314b9cdc1d4ef494
ray <rui.song90@gmail.com>
2023-11-02 20:57:09 +0000

    download caliop

--- StratosphereAsh/Puyehue/IceClouds/screen_ice_clouds_save_csv.py
+++ StratosphereAsh/Puyehue/IceClouds/screen_ice_clouds_save_csv.py
@@ -121,7 +121,7 @@ def main():
 
                 index_row = caliop_cloud_index[0][i]
                 index_col = caliop_cloud_index[1][i]
-
+                print(index_row, index_col)
                 if caliop_dp[index_row, index_col] > 0.:
 
                     print(caliop_lat[index_col], caliop_lon[index_col], alt_caliop[index_row], caliop_dp[index_row, index_col])
