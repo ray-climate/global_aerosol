1a09721dd4965c9e0da8de9807c3399481bdbaa0
ray <rui.song90@gmail.com>
2023-11-03 23:53:03 +0000

    download caliop

--- StratosphereAsh/Puyehue/Aerosol/plot_aerosol_observation_number.py
+++ StratosphereAsh/Puyehue/Aerosol/plot_aerosol_observation_number.py
@@ -7,92 +7,77 @@
 
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
-from matplotlib.patches import Patch
+from datetime import datetime
 from collections import defaultdict
-import matplotlib.ticker as ticker
-import numpy as np
-import csv
 import os
+import csv
 
+# Constants
 NORTHERN_LATITUDE = -20
 SOUTHERN_LATITUDE = -80
-MIN_ALTITUDE = 9
-MAX_ALTITUDE = 16.
-
 INPUT_PATH = './csv_new'
 FIGURE_OUTPUT_PATH = './figures'
 
-# Create csv saving directory if not present
+# Create the output directory if it does not exist
 if not os.path.exists(FIGURE_OUTPUT_PATH):
     os.mkdir(FIGURE_OUTPUT_PATH)
 
-date_all = []
-lat_all = []
-alt_base_all = []
-alt_top_all = []
-depolarization_all = []
-aerosol_type_all = []
-CAD_all = []
 
-# Dictionary to hold the count of valid depolarization values per day
-valid_depolarization_count = defaultdict(int)
+# Dictionary to hold the count of valid depolarization values for each latitude range
+valid_depolarization_counts = {
+    (-20, -40): defaultdict(int),
+    (-40, -60): defaultdict(int),
+    (-60, -80): defaultdict(int)
+}
 
+# Process files
 for file in os.listdir(INPUT_PATH):
     if file.endswith('.csv'):
         with open(os.path.join(INPUT_PATH, file), 'r') as f:
             reader = csv.reader(f)
-            next(reader)  # Skip the header
+            next(reader)  # Skip header
             for row in reader:
-                try:
-                    latitude = float(row[0])
-                    alt_base = float(row[2])
-                    alt_top = float(row[3])
-                    depolarization = float(row[5])
-                    aerosol_type = float(row[6])
-                    CAD = float(row[7])
-                    file_date = datetime.strptime(file.split('.')[1][0:10], '%Y-%m-%d')
+                latitude = float(row[0])
+                alt_base = float(row[2])
+                alt_top = float(row[3])
+                depolarization = float(row[5])
+                aerosol_type = float(row[6])
+                CAD = float(row[7])
+                date = datetime.strptime(file.split('.')[1][0:10], '%Y-%m-%d')
 
-                    if (SOUTHERN_LATITUDE <= latitude <= NORTHERN_LATITUDE) & (MIN_ALTITUDE <= alt_base <= MAX_ALTITUDE) & (MIN_ALTITUDE <= alt_top <= MAX_ALTITUDE) & (2. <= aerosol_type <= 4.):
-                        valid_depolarization_count[file_date] += 1
-                except:
-                    pass
+                if (SOUTHERN_LATITUDE <= latitude <= NORTHERN_LATITUDE) & (MIN_ALTITUDE <= alt_base <= MAX_ALTITUDE) & (MIN_ALTITUDE <= alt_top <= MAX_ALTITUDE) & (2. <= aerosol_type <= 4.) & (depolarization > 0):
 
-# Sort the dictionary by date
-sorted_valid_depolarization_count = dict(sorted(valid_depolarization_count.items()))
+                    # Assign counts to appropriate latitude range
+                    for lat_range in valid_depolarization_counts:
+                        if lat_range[0] <= latitude <= lat_range[1]:
+                            valid_depolarization_counts[lat_range][date] += 1
+                            break
 
-# Extract dates and counts for plotting
-dates = list(sorted_valid_depolarization_count.keys())
-counts = list(sorted_valid_depolarization_count.values())
+# Define the figure and subplots
+fig, axs = plt.subplots(3, 1, figsize=(10, 15), sharex=True)
 
-# Plotting
-# Plotting
-plt.figure(figsize=(10, 5))
-
-# Force x-axis limits
+# Define date range for xlim
 start_date = datetime(2011, 4, 1)
 end_date = datetime(2011, 10, 1)
-plt.xlim(start_date, end_date)
 
-# Plot the data
-plt.plot(dates, counts, marker='o')
+# Plot data for each latitude range
+for i, lat_range in enumerate(valid_depolarization_counts):
+    # Sort data by date for the latitude range
+    dates, counts = zip(*sorted(valid_depolarization_counts[lat_range].items()))
 
-# Set x-axis major ticks to the first day of each month
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(bymonthday=1))
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    # Plot on the respective subplot
+    axs[i].plot(dates, counts, marker='o')
+    axs[i].set_xlim(start_date, end_date)
+    axs[i].xaxis.set_major_locator(mdates.MonthLocator(bymonthday=1))
+    axs[i].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    axs[i].set_ylabel(f"Counts for Lat {lat_range[0]} to {lat_range[1]}")
+    axs[i].grid(True)
 
-# Set x-axis minor ticks to invisible, if necessary
-plt.gca().xaxis.set_minor_locator(mdates.MonthLocator())
-plt.gca().xaxis.set_minor_formatter(ticker.NullFormatter())
+# Set the xlabel for the bottom subplot
+axs[-1].set_xlabel('Date')
 
-# Rotate date labels automatically
-plt.gcf().autofmt_xdate()
-
-plt.xlabel('Date')
-plt.ylabel('Number of Valid Depolarization Values')
-plt.title('Valid Depolarization Values per Day')
-plt.grid(True)
+# Automatically adjust the subplot params for a nice fit
 plt.tight_layout()
 
 # Save the figure
-plt.savefig(os.path.join(FIGURE_OUTPUT_PATH, 'valid_depolarization_values_per_day.png'))
+plt.savefig(os.path.join(FIGURE_OUTPUT_PATH, 'valid_depolarization_values_by_latitude.png'))
