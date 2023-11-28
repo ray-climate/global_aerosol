bc928a637c1a55958c67e759b58ac3d5d5c12b4c
ray <rui.song90@gmail.com>
2023-11-28 17:21:55 +0000

    download caliop

--- StratosphereAsh/Global/plot_global_ash.py
+++ StratosphereAsh/Global/plot_global_ash.py
@@ -151,7 +151,7 @@ hist, xedges, yedges = np.histogram2d(mdates.date2num(caliop_times), all_caliop_
 fig, ax = plt.subplots(figsize=(18, 7))
 X, Y = np.meshgrid(xedges, yedges)
 # mesh = ax.pcolormesh(X, Y, hist.T, shading='auto', cmap='plasma', norm=LogNorm())
-mesh = ax.pcolormesh(X, Y, hist.T, shading='auto', cmap='plasma', vmin=20, vmax=300)
+mesh = ax.pcolormesh(X, Y, hist.T, shading='auto', cmap='plasma', vmin=20, vmax=100)
 # Format the time axis
 ax.xaxis_date()
 date_format = mdates.DateFormatter('%Y-%m-%d')
@@ -161,7 +161,7 @@ plt.yticks(fontsize=16)
 plt.ylim([-80., 80.])
 plt.xlabel('Time', fontsize=18)
 plt.ylabel('Latitude', fontsize=18)
-plt.title('Occurrence of Stratopsheric Aerosol Layers from CALIOP [Night]', fontsize=18)
+plt.title('Occurrence of Stratospheric Aerosol Layers from CALIOP [Night]', fontsize=18)
 
 # Add colorbar with reference to the mesh
 cbar = plt.colorbar(mesh, extend='both', shrink=0.7)
