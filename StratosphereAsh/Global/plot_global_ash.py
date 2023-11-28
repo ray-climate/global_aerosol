b37304d69ece74455a7756ccc5309bf7ad7a52f1
ray <rui.song90@gmail.com>
2023-11-28 17:13:59 +0000

    download caliop

--- StratosphereAsh/Global/plot_global_ash.py
+++ StratosphereAsh/Global/plot_global_ash.py
@@ -150,8 +150,8 @@ hist, xedges, yedges = np.histogram2d(mdates.date2num(caliop_times), all_caliop_
 # Plot
 fig, ax = plt.subplots(figsize=(18, 7))
 X, Y = np.meshgrid(xedges, yedges)
-mesh = ax.pcolormesh(X, Y, hist.T, shading='auto', cmap='plasma', norm=LogNorm())
-
+# mesh = ax.pcolormesh(X, Y, hist.T, shading='auto', cmap='plasma', norm=LogNorm())
+mesh = ax.pcolormesh(X, Y, hist.T, shading='auto', cmap='plasma')
 # Format the time axis
 ax.xaxis_date()
 date_format = mdates.DateFormatter('%Y-%m-%d')
