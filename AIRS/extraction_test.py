15dbefcbbd119fff91f841fcb0158d0f809462ed
ray <rui.song90@gmail.com>
2023-12-19 17:05:28 +0000

    download caliop

--- AIRS/extraction_test.py
+++ AIRS/extraction_test.py
@@ -7,23 +7,20 @@
 
 from osgeo import gdal
 
-file_name = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/AIRS/AIRS.2011.06.21.001.L1B.AIRS_Rad.v5.0.0.0.G11172125737.hdf'
+def read_airs_radiance(filename):
 
-# Open the HDF4 file
-hdf_file = gdal.Open(file_name)
+    # Open the HDF4 file
+    hdf_file = gdal.Open(file_name)
+    # Get the subdataset name for "radiances"
+    subdataset_name = "HDF4_EOS:EOS_SWATH:\"{file}\":L1B_AIRS_Science:radiances".format(file=file_name)
+    # Open the subdataset for "radiances"
+    radiances_ds = gdal.Open(subdataset_name)
+    # Read the "radiances" data
+    radiances = radiances_ds.ReadAsArray()
 
-# Get the subdataset name for "radiances"
-subdataset_name = "HDF4_EOS:EOS_SWATH:\"{file}\":L1B_AIRS_Science:radiances".format(file=file_name)
+    # Now "radiances" contains the extracted data
+    print(radiances.shape)
 
-# Open the subdataset for "radiances"
-radiances_ds = gdal.Open(subdataset_name)
-
-# Read the "radiances" data
-radiances = radiances_ds.ReadAsArray()
-
-# Close the datasets
-radiances_ds = None
-hdf_file = None
-
-# Now "radiances" contains the extracted data
-print(radiances.shape)
+if __name__ == '__main__':
+    file_name = '/gws/pw/j07/nceo_aerosolfire/rsong/project/global_aerosol/AIRS/AIRS.2011.06.21.001.L1B.AIRS_Rad.v5.0.0.0.G11172125737.hdf'
+    read_airs_radiance(file_name)
