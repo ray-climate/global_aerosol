8b622d2c40c4c048b6900546aaa1e1d9ee9ef6ce
ray <rui.song90@gmail.com>
2024-01-08 00:26:18 +0000

    download caliop

--- CaseStudy/paper/vertical_density_map.py
+++ CaseStudy/paper/vertical_density_map.py
@@ -248,7 +248,7 @@ if True:
     # Plot the KDE density plot and the curve plot for aeolus
     plt.figure(figsize=(8, 12))
     kde = sns.kdeplot(data=long_form_data_aeolus_beta, x='beta_aeolus_log', y='Altitude', cmap='Blues', fill=True, cbar=True,
-                      cbar_kws={'label': 'Density', 'shrink': 0.6, 'orientation': 'vertical'})
+                      cbar_kws={'label': 'Density', 'shrink': 0.4, 'orientation': 'vertical'})
 
     for i in range(len(beta_aeolus_mean)-1):
         plt.plot([np.log10(beta_aeolus_mean[i] / conversion_factor), np.log10(beta_aeolus_mean[i] / conversion_factor)], [alt_aeolus_mean[i], alt_aeolus_mean[i+1]], 'r')
