ce2068528f884ec557ff100c8ef83c0385c32f17
ray <rui.song90@gmail.com>
2024-01-12 23:39:43 +0000

    download caliop

--- CaseStudy/paper/vertical_density_map.py
+++ CaseStudy/paper/vertical_density_map.py
@@ -266,7 +266,7 @@ if True:
 
     sm = ScalarMappable(cmap='Blues', norm=norm)
     sm.set_array([])  # You need to set_array for ScalarMappable
-    cbar = plt.colorbar(sm, ax=ax_main, orientation='vertical', pad=-0.25, shrink=0.3, extend='both')
+    cbar = plt.colorbar(sm, ax=ax_main, orientation='vertical', pad=-0.3, shrink=0.3, extend='both')
     cbar.set_label('Density', fontsize=15)
     cbar.ax.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1, decimals=1))
     cbar.ax.yaxis.set_major_locator(ticker.MaxNLocator(6))
