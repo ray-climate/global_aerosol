40080c848cd971e864c7b440a687eb209a1bb95b
ray <rui.song90@gmail.com>
2024-01-14 11:10:54 +0000

    download caliop

--- CaseStudy/paper/vertical_density_map.py
+++ CaseStudy/paper/vertical_density_map.py
@@ -252,7 +252,7 @@ from matplotlib.cm import ScalarMappable
 #
 if True:
     # Plot the KDE density plot and the curve plot for aeolus
-    plt.figure(figsize=(8, 12))
+    plt.figure(figsize=(10, 12))
 
     vmin, vmax = 0.008, 0.048
     norm = Normalize(vmin=vmin, vmax=vmax)
